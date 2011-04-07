9fa13ee77297efb2b91b4ddefcd16a470acf28f5
Michael S. Hansen <michael.hansen@nih.gov>
2011-04-07 18:22:45 -0400

    Added copy constructors and assignment operators to cuNDArray. Added squeeze function to NDArray

--- toolboxes/gpundarray/cuNDArray.h
+++ toolboxes/gpundarray/cuNDArray.h
@@ -137,57 +137,15 @@ template <class T> class cuNDArray : public NDArray<T>
   virtual int get_device() {return device_;}
 
   //Copy constructor
-  cuNDArray(const cuNDArray<T>& a) {
-    this->data_ = 0;
-    this->dimensions_ = a.dimensions_;
-    if (allocate_memory() == 0) {
-      if (a.device_ == this->device_) {
-	if (cudaMemcpy(this->data_, a.data_, this->elements_*sizeof(T), cudaMemcpyDeviceToDevice) !=
-	    cudaSuccess) {
-	  std::cerr << "cuNDArray: Unable to copy data in copy constructor" << std::endl;
-	}
-      } else {
-	//This memory is on a different device, we must move it.
-	//TODO: fix bug in here
-	hoNDArray<T> tmp;
-	if (!tmp.create(a.dimensions_)) {
-	  std::cerr << "cuNDArray: Unable to allocate temporary memory in copy constructor" << std::endl;
-	  deallocate_memory();
-	  this->dimensions_.clear();
-	}
-	
-	if (cudaMemcpy(tmp.get_data_ptr(), a.data_, this->elements_*sizeof(T), cudaMemcpyDeviceToHost) !=
-	    cudaSuccess) {
-	  deallocate_memory();
-	  this->data_ = 0;
-	  this->dimensions_.clear();
-	}
+  cuNDArray(const cuNDArray<T>& a);
 
-	if (cudaMemcpy(this->data_, tmp.get_data_ptr(), this->elements_*sizeof(T), cudaMemcpyHostToDevice) !=
-	    cudaSuccess) {
-	  deallocate_memory();
-	  this->data_ = 0;
-	  this->dimensions_.clear();
-	}
-      }
-    }
-  }
+  //Assignment operator
+  cuNDArray& operator=(const cuNDArray<T>& rhs);
+
+  cuNDArray(hoNDArray<T>& a);
+
   
-  cuNDArray(hoNDArray<T>& a) {
-    this->data_ = 0;
-    this->dimensions_ = a.get_dimensions();
-    if (allocate_memory() == 0) {
-      if (cudaMemcpy(this->data_, a.get_data_ptr(), this->elements_*sizeof(T), cudaMemcpyHostToDevice) !=
-	  cudaSuccess) {
-	deallocate_memory();
-	this->data_ = 0;
-	this->dimensions_.clear();
-      }
-    }
-  }
-  
-  
-  hoNDArray<T> to_host() {
+  hoNDArray<T> to_host() const {
     hoNDArray<T> ret;
     if (ret.create(this->dimensions_)) {
       if (cudaMemcpy(ret.get_data_ptr(), this->data_, this->elements_*sizeof(T), cudaMemcpyDeviceToHost) !=
@@ -201,63 +159,7 @@ template <class T> class cuNDArray : public NDArray<T>
 
   virtual int permute(std::vector<unsigned int>& dim_order, 
 		      NDArray<T>* out = 0,
-		      int shift_mode = 0)
-  {
-
-    cuNDArray<T>* out_int = 0;
-
-    //Check ordering array
-    if (dim_order.size() > this->dimensions_.size()) {
-      std::cerr << "hoNDArray::permute - Invalid length of dimension ordering array" << std::endl;
-      return -1;
-    }
-
-    std::vector<unsigned int> dim_count(this->dimensions_.size(),0);
-    for (unsigned int i = 0; i < dim_order.size(); i++) {
-      if (dim_order[i] >= this->dimensions_.size()) {
-	std::cerr << "hoNDArray::permute - Invalid dimension order array" << std::endl;
-	return -1;
-      }
-      dim_count[dim_order[i]]++;
-    }
-
-    //Create an internal array to store the dimensions
-    std::vector<unsigned int> dim_order_int;
-
-    //Check that there are no duplicate dimensions
-    for (unsigned int i = 0; i < dim_order.size(); i++) {
-      if (dim_count[dim_order[i]] != 1) {
-	std::cerr << "hoNDArray::permute - Invalid dimension order array (duplicates)" << std::endl;
-	return -1;
-      }
-      dim_order_int.push_back(dim_order[i]);
-    }
-
-    //Pad dimension order array with dimension not mentioned in order array
-    if (dim_order_int.size() < this->dimensions_.size()) {
-      for (unsigned int i = 0; i < dim_count.size(); i++) {
-	if (dim_count[i] == 0) {
-	  dim_order_int.push_back(i);
-	}
-      }
-    }
-
-    if (out) {
-      out_int = dynamic_cast< cuNDArray<T>* >(out);
-      if (!out_int) {
-	std::cerr << "cuNDArray::permute: failed to dynamic cast out array pointer" << std::endl;
-	return -1;
-      }
-      for (unsigned int i = 0; i < dim_order_int.size(); i++) {
-	if (this->dimensions_[dim_order_int[i]] != out_int->get_size(i)) {
-	  std::cerr << "cuNDArray::permute: Dimensions of output array do not match the input array" << std::endl;
-	  return -1;
-	}
-      }
-    }
-
-    return cuNDArray_permute(this, out_int, dim_order_int, shift_mode);
-  }
+		      int shift_mode = 0);
 
  protected:
   int device_;
