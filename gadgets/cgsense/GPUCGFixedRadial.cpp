5176f0a4ddec7d5a0f8d193c176731a7fc040164
Thomas Sangild Sorensen <sangild@cs.au.dk>
2011-12-02 11:39:57 +0100

    Added FixedRadial cgSense files, not yet working
    Former-commit-id: 037f8f6af3e60bc3e43e638643bae6e8de8c1aea

---
+++ gadgets/cgsense/GPUCGFixedRadial.cpp
@@ -0,0 +1,36 @@
+#include "GPUCGGoldenRadial.h"
+#include "radial_utilities.h"
+#include "Gadgetron.h"
+
+boost::shared_ptr< cuNDArray<floatd2::Type> > 
+GPUCGGoldenRadialGadget::calculate_trajectory()
+{
+	// Define trajectories
+	boost::shared_ptr< cuNDArray<floatd2::Type> > traj = compute_radial_trajectory_golden_ratio_2d<float>
+		( samples_per_profile_, profiles_per_frame_, 1, current_profile_offset_ );
+
+  if (!traj.get()) {
+    GADGET_DEBUG1("Failed to allocate trajectory");
+    return boost::shared_ptr< cuNDArray<floatd2::Type> >();
+  }
+
+  return traj;
+}
+
+boost::shared_ptr< cuNDArray<float> >
+GPUCGGoldenRadialGadget::calculate_density_compensation()
+{
+
+	boost::shared_ptr< cuNDArray<float> > dcw = compute_radial_dcw_golden_ratio_2d
+    ( samples_per_profile_, profiles_per_frame_, (float)matrix_size_os_.vec[0]/(float)matrix_size_.vec[0], 
+      1.0f/((float)samples_per_profile_/(float)std::max(matrix_size_.vec[0],matrix_size_.vec[1])) );
+  
+  if (!dcw.get()) {
+    GADGET_DEBUG1("Failed to calculate density compensation weights\n");
+    return boost::shared_ptr< cuNDArray<float> >();
+  }
+
+  return dcw;
+}
+
+GADGET_FACTORY_DECLARE(GPUCGGoldenRadialGadget)
---
+++ gadgets/cgsense/GPUCGFixedRadial.h
@@ -0,0 +1,19 @@
+#ifndef GPUCGGOLDENRADIALGADGET_H
+#define GPUCGGOLDENRADIALGADGET_H
+#pragma once
+
+#include "GPUCGGadget.h"
+
+class EXPORTGADGETSCGSENSE GPUCGGoldenRadialGadget : public GPUCGGadget
+{
+
+ public:
+  GADGET_DECLARE(GPUCGGoldenRadialGadget);
+
+ protected:
+  virtual boost::shared_ptr< cuNDArray<floatd2::Type> > calculate_trajectory();
+  virtual boost::shared_ptr< cuNDArray<float> > calculate_density_compensation();
+};
+
+
+#endif //GPUCGGOLDENRADIALGADGET_H
