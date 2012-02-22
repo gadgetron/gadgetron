0d3410bec4a01156b3bafaefc92dd44162d94ee8
Thomas Sangild Sørensen <sangild@sangild-linux.(none)>
2012-02-22 11:58:27 +0100

    minor ndarray utility update
    Former-commit-id: a3bbd4a9dca4e60d2d5fda28016344da62854867

--- toolboxes/gpucore/vector_td_utilities.h
+++ toolboxes/gpucore/vector_td_utilities.h
@@ -168,10 +168,10 @@ template<unsigned int D>
 typename uintd<D>::Type vector_to_uintd( std::vector<unsigned int> _vector )
 {
   typename uintd<D>::Type out;
-  if( _vector.size() < D ){
+  /*  if( _vector.size() < D ){
     std::cout << "Warning :: cannot convert vector to typename uintd<D>, dimensionality is less D" << std::endl;
     std::cout << "Filling in ones..." << std::endl;
-  }
+    }*/
   
   for( unsigned int i=0; i<D; i++ ){
     if( i<_vector.size() )
