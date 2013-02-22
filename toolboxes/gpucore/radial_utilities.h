f49ed1af8982e2e2a0940134fa9de7d7e070f0ec
Michael S. Hansen <michael.hansen@nih.gov>
2013-02-22 11:49:09 -0500

    Some fixes to fixed angle radial CG SENSE
    Former-commit-id: 63e15b85495399faffb260880fd62f7de7669016

--- toolboxes/gpucore/radial_utilities.h
+++ toolboxes/gpucore/radial_utilities.h
@@ -8,7 +8,7 @@
 
 // Compute fixed angle radial trajectory in the normalized range [-1/2;1/2]
 template<class REAL> EXPORTGPUCORE boost::shared_ptr< cuNDArray< typename reald<REAL,2>::Type > >
-compute_radial_trajectory_fixed_angle_2d( unsigned int num_samples_per_profile, unsigned int num_profiles_per_frame, unsigned int num_frames );
+compute_radial_trajectory_fixed_angle_2d( unsigned int num_samples_per_profile, unsigned int num_profiles_per_frame, unsigned int num_frames, REAL angular_offset = 0.0 );
 
 // Compute golden ratio radial trajectory in the normalized range [-1/2;1/2]
 template<class REAL> EXPORTGPUCORE boost::shared_ptr< cuNDArray< typename reald<REAL,2>::Type > >
@@ -16,7 +16,7 @@ compute_radial_trajectory_golden_ratio_2d( unsigned int num_samples_per_profile,
 
 // Compute fixed angle radial density compensation weights (a function of the chose reconstruction settings: matrix_size and oversampling factor)
 template<class REAL> EXPORTGPUCORE boost::shared_ptr< cuNDArray<REAL> >
-compute_radial_dcw_fixed_angle_2d( unsigned int num_samples_per_profile, unsigned int num_profiles, REAL alpha, REAL one_over_radial_oversampling_factor );
+compute_radial_dcw_fixed_angle_2d( unsigned int num_samples_per_profile, unsigned int num_profiles, REAL alpha, REAL one_over_radial_oversampling_factor);
 
 // Compute golden ratio radial density compensation weights (a function of the chose reconstruction settings: matrix_size and oversampling factor)
 template<class REAL> EXPORTGPUCORE boost::shared_ptr< cuNDArray<REAL> >
