aae36fdcc243530e57a7ef65b7104a96d25867a8
David C Hansen <dch@cs.au.dk>
2013-03-06 13:22:17 -0500

    Added missed filed
    Former-commit-id: 23300bf5d430562260b102a6a06b61827b7bd25e

---
+++ toolboxes/gpucore/hoCuNDArray_utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "hoNDArray_utils.h"
+#include "complext.h"
+
+namespace Gadgetron{
+/**
+ * @brief Calculates the elementwise absolute value of the array
+ * @param[in] data Input data
+ * @return A new array containing the elementwise absolute value of data
+ */
+template<class T>
+boost::shared_ptr<hoCuNDArray<typename realType<T>::type> > abs(hoCuNDArray<T> *data){
+	return boost::static_pointer_cast<hoCuNDArray<typename realType<T>::type> >(abs(static_cast<hoNDArray<T>* >(data)));
+}
+}
