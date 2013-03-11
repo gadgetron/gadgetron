5d42b80bfe8c3c1aaae805f0bfceda62ae02d9f6
Joseph Naegele <joseph.naegele@gmail.com>
2013-03-11 12:24:06 -0400

    initial Matlab gadget code
    Former-commit-id: c7a2ff8e46d5f2a155cd1ef1fd818bc11544c64a

--- cmake/FindGMatlab.cmake
+++ cmake/FindGMatlab.cmake
@@ -20,108 +20,75 @@
 #  License text for the above reference.)
 
 set(MATLAB_FOUND 0)
+
 if(WIN32)
-  if(${CMAKE_GENERATOR} MATCHES "Visual Studio 6")
-    set(MATLAB_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.0;MATLABROOT]/extern/lib/win32/microsoft/msvc60")
-  else()
-    if(${CMAKE_GENERATOR} MATCHES "Visual Studio 7")
-      # Assume people are generally using 7.1,
-      # if using 7.0 need to link to: ../extern/lib/win32/microsoft/msvc70
-      set(MATLAB_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.0;MATLABROOT]/extern/lib/win32/microsoft/msvc71")
+    set(MATLAB_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.0;MATLABROOT]")
+    if(${CMAKE_GENERATOR} MATCHES "Visual Studio 6")
+        set(MATLAB_LIB_ROOT "${MATLAB_ROOT}/extern/lib/win32/microsoft/msvc60")
     else()
-      if(${CMAKE_GENERATOR} MATCHES "Borland")
-        # Same here, there are also: bcc50 and bcc51 directories
-        set(MATLAB_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.0;MATLABROOT]/extern/lib/win32/microsoft/bcc54")
-      else()
-        if(MATLAB_FIND_REQUIRED)
-          message(FATAL_ERROR "Generator not compatible: ${CMAKE_GENERATOR}")
+        if(${CMAKE_GENERATOR} MATCHES "Visual Studio 7")
+            # Assume people are generally using 7.1,
+            # if using 7.0 need to link to: ../extern/lib/win32/microsoft/msvc70
+            set(MATLAB_LIB_ROOT "${MATLAB_ROOT}/extern/lib/win32/microsoft/msvc71")
+        else()
+            if(${CMAKE_GENERATOR} MATCHES "Borland")
+                # Same here, there are also: bcc50 and bcc51 directories
+                set(MATLAB_LIB_ROOT "${MATLAB_ROOT}/extern/lib/win32/microsoft/bcc54")
+            else()
+                if(MATLAB_FIND_REQUIRED)
+                    message(FATAL_ERROR "Generator not compatible: ${CMAKE_GENERATOR}")
+                endif()
+            endif()
         endif()
-      endif()
     endif()
-  endif()
-  find_library(MATLAB_MEX_LIBRARY
-    libmex
-    ${MATLAB_ROOT}
+    find_path(
+        MATLAB_INCLUDE_DIR
+        "mex.h"
+        HINTS ${MATLAB_ROOT}
+        PATH_SUFFIXES extern/include
     )
-  find_library(MATLAB_MX_LIBRARY
-    libmx
-    ${MATLAB_ROOT}
+else(WIN32)
+    find_path(
+        MATLAB_ROOT extern/include/mex.h
+        HINTS $ENV{MATLAB_HOME} $ENV{MATLAB_ROOT}
+        PATHS /usr /usr/local /opt
+        PATH_SUFFIXES MATLAB
     )
-  find_library(MATLAB_ENG_LIBRARY
-    libeng
-    ${MATLAB_ROOT}
+    find_path(
+        MATLAB_INCLUDE_DIR
+        "mex.h"
+        HINTS ${MATLAB_ROOT}
+        PATH_SUFFIXES extern/include
     )
+endif(WIN32)
 
-  find_path(MATLAB_INCLUDE_DIR
-    "mex.h"
-    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.0;MATLABROOT]/extern/include"
+# find each library
+# give it it's own cmake variable
+# add it to the list of libraries
+foreach(lib mat ut mex mx eng)
+    string(TOUPPER ${lib} LIB)
+    find_library(MATLAB_${LIB}_LIBRARY
+        ${lib}
+        HINTS ${MATLAB_ROOT} ${MATLAB_LIB_ROOT}
+        PATH_SUFFIXES lib bin bin/maci64 bin/glnax64 bin/glnax86
     )
-else()
-
-  find_library(MATLAB_MEX_LIBRARY
-    mex
-    ${MATLAB_ROOT}/bin/glnxa64
-    ${MATLAB_ROOT}/bin/glnx86
-    ${MATLAB_ROOT}/bin/maci64
-    $ENV{MATLAB_ROOT}/bin/glnxa64
-    $ENV{MATLAB_ROOT}/bin/glnx86
-    $ENV{MATLAB_ROOT}/bin/maci64
-    NO_DEFAULT_PATH
-    NO_SYSTEM_ENVIRONMENT_PATH
-    )
-  find_library(MATLAB_MX_LIBRARY
-    mx
-    ${MATLAB_ROOT}/bin/glnxa64
-    ${MATLAB_ROOT}/bin/glnx86
-    ${MATLAB_ROOT}/bin/maci64
-    $ENV{MATLAB_ROOT}/bin/glnxa64
-    $ENV{MATLAB_ROOT}/bin/glnx86
-    $ENV{MATLAB_ROOT}/bin/maci64
-    NO_DEFAULT_PATH
-    NO_SYSTEM_ENVIRONMENT_PATH
-    NO_CMAKE_PATH
-    NO_CMAKE_ENVIRONMENT_PATH
-    )
-    MESSAGE("MATLAB_MX_LIBRARY: ${MATLAB_MX_LIBRARY}")
-  find_library(MATLAB_ENG_LIBRARY
-    eng
-    ${MATLAB_ROOT}/bin/glnxa64
-    ${MATLAB_ROOT}/bin/glnx86
-    ${MATLAB_ROOT}/bin/maci64
-    $ENV{MATLAB_ROOT}/bin/glnxa64
-    $ENV{MATLAB_ROOT}/bin/glnx86
-    $ENV{MATLAB_ROOT}/bin/maci64
-    )
-  find_path(MATLAB_INCLUDE_DIR
-    "mex.h"
-    "${MATLAB_ROOT}/extern/include"
-    "$ENV{MATLAB_ROOT}/extern/include"
-    "/usr/local/matlab-7sp1/extern/include/"
-    "/opt/matlab-7sp1/extern/include/"
-    "$ENV{HOME}/matlab-7sp1/extern/include/"
-    "$ENV{HOME}/redhat-matlab/extern/include/"
-    )
-
-endif()
-
-# This is common to UNIX and Win32:
-set(MATLAB_LIBRARIES
-  ${MATLAB_MEX_LIBRARY}
-  ${MATLAB_MX_LIBRARY}
-  ${MATLAB_ENG_LIBRARY}
-)
+    if(MATLAB_${LIB}_LIBRARY)
+        list(APPEND MATLAB_LIBRARIES ${MATLAB_${LIB}_LIBRARY})
+    endif()
+    mark_as_advanced(MATLAB_${LIB}_LIBRARY)
+endforeach()
 
 if(MATLAB_INCLUDE_DIR AND MATLAB_LIBRARIES)
-  set(MATLAB_FOUND 1)
+    set(MATLAB_FOUND 1)
 endif()
 
+include("FindPackageHandleStandardArgs")
+FIND_PACKAGE_HANDLE_STANDARD_ARGS("Matlab" DEFAULT_MSG MATLAB_ROOT MATLAB_INCLUDE_DIR MATLAB_LIBRARIES)
+
 mark_as_advanced(
-  MATLAB_LIBRARIES
-  MATLAB_MEX_LIBRARY
-  MATLAB_MX_LIBRARY
-  MATLAB_ENG_LIBRARY
-  MATLAB_INCLUDE_DIR
-  MATLAB_FOUND
-  MATLAB_ROOT
+    MATLAB_LIBRARIES
+    MATLAB_INCLUDE_DIR
+    MATLAB_FOUND
+    MATLAB_ROOT
 )
 
--- gadgets/matlab/MatlabGadget.cpp
+++ gadgets/matlab/MatlabGadget.cpp
@@ -1,31 +1,172 @@
 #include "MatlabGadget.h"
 
+#define NUMBER_OF_FIELDS    3 //23
+
+const char *ismrmrd_acq_field_names[] = {
+    "version",
+    "flags",
+    "measurement_uid",
+    /*
+    "scan_counter",
+    "acquisition_time_stamp",
+    "physiology_time_stamp",
+    "number_of_samples",
+    "available_channels",
+    "active_channels",
+    "channel_mask",
+    "discard_pre",
+    "discard_post",
+    "center_sample",
+    "encoding_space_ref",
+    "trajectory_dimensions",
+    "sample_time_us",
+    "position",
+    "read_dir",
+    "phase_dir",
+    "slice_dir",
+    "patient_table_position",
+ //   "EncodingCounters   idx",
+    "user_int",
+    "user_float"
+    */
+};
 
 int AcquisitionMatlabGadget::process(GadgetContainerMessage<ISMRMRD::AcquisitionHeader>* m1,
-		GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2)
+        GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2)
 {
+    if (this->next()->putq(m1) < 0) {
+        return GADGET_FAIL;
+    }
 
-	//GADGET_DEBUG1("Data passing MATLAB Gadget\n");
+    ISMRMRD::AcquisitionHeader *acq = m1->getObjectPtr();
+    hoNDArray< std::complex<float> > *data = m2->getObjectPtr();
 
-	if (this->next()->putq(m1) < 0) {
-		return GADGET_FAIL;
-	}
+    // Copy the Acquisition Header into Matlab memory
+    /*
+    ISMRMRD::AcquisitionHeader *macq = (ISMRMRD::AcquisitionHeader *)mxMalloc(sizeof(*acq));
+    if (!macq) {
+        GADGET_DEBUG1("Failed to allocate Matlabe memory for Acquisition Header\n");
+        return GADGET_FAIL;
+    }
+    memcpy(macq, acq, sizeof(*macq));
+    */
 
+    std::cout << "Preparing Matlab struct array for Acquisition header" << std::endl;
 
+    // Create struct array for storing a single ISMRMRD Acquisition Header
+    mwSize dims[2] = {1, 1};
+    mxArray *acqhdr = mxCreateStructArray(2, dims, NUMBER_OF_FIELDS, ismrmrd_acq_field_names);
 
-	return GADGET_OK;
+    std::cout << "Created struct array" << std::endl;
+
+    mxArray *acqhdr_values[NUMBER_OF_FIELDS];
+
+    acqhdr_values[0] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    *mxGetPr(acqhdr_values[0]) = acq->version;
+
+    acqhdr_values[1] = mxCreateNumericMatrix(1, 1, mxUINT64_CLASS, mxREAL);
+    *mxGetPr(acqhdr_values[1]) = acq->flags;
+
+    acqhdr_values[2] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
+    *mxGetPr(acqhdr_values[2]) = acq->measurement_uid;
+
+    std::cout << "Set 3 array values" << std::endl;
+    fflush(stdout);
+/*
+    acqhdr_values[3] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
+    mxSetData(acqhdr_values[3], &macq->scan_counter);
+
+    acqhdr_values[4] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
+    mxSetData(acqhdr_values[4], &macq->acquisition_time_stamp);
+
+    acqhdr_values[5] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
+    mxSetData(acqhdr_values[5], macq->physiology_time_stamp);
+
+    acqhdr_values[6] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[6], &macq->number_of_samples);
+
+    acqhdr_values[7] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[7], &macq->available_channels);
+
+    acqhdr_values[8] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[8], &macq->active_channels);
+
+    acqhdr_values[9] = mxCreateNumericMatrix(ISMRMRD_CHANNEL_MASKS, 1, mxUINT64_CLASS, mxREAL);
+    mxSetData(acqhdr_values[9], macq->channel_mask);
+
+    acqhdr_values[10] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[10], &macq->discard_pre);
+
+    acqhdr_values[11] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[11], &macq->discard_post);
+
+    acqhdr_values[12] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[12], &macq->center_sample);
+
+    acqhdr_values[13] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[13], &macq->encoding_space_ref);
+
+    acqhdr_values[14] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+    mxSetData(acqhdr_values[14], &macq->trajectory_dimensions);
+
+    acqhdr_values[15] = mxCreateNumericMatrix(1, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[15], &macq->sample_time_us);
+
+    acqhdr_values[16] = mxCreateNumericMatrix(ISMRMRD_POSITION_LENGTH, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[16], macq->position);
+
+    acqhdr_values[17] = mxCreateNumericMatrix(ISMRMRD_DIRECTION_LENGTH, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[17], macq->read_dir);
+
+    acqhdr_values[18] = mxCreateNumericMatrix(ISMRMRD_DIRECTION_LENGTH, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[18], macq->phase_dir);
+
+    acqhdr_values[19] = mxCreateNumericMatrix(ISMRMRD_DIRECTION_LENGTH, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[19], macq->slice_dir);
+
+    acqhdr_values[20] = mxCreateNumericMatrix(ISMRMRD_POSITION_LENGTH, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[20], macq->patient_table_position);
+
+    //acqhdr_values[21] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
+    acqhdr_values[21] = mxCreateNumericMatrix(ISMRMRD_USER_INTS, 1, mxINT32_CLASS, mxREAL);
+    mxSetData(acqhdr_values[21], macq->user_int);
+
+    acqhdr_values[22] = mxCreateNumericMatrix(ISMRMRD_USER_FLOATS, 1, mxSINGLE_CLASS, mxREAL);
+    mxSetData(acqhdr_values[22], macq->user_float);
+*/
+    // Set the fields in the ISMRMRD Acquisition Header struct mxArray
+    mwIndex i;
+    for (i = 0; i < NUMBER_OF_FIELDS; i++) {
+        mxSetFieldByNumber(acqhdr, 0, i, acqhdr_values[i]);
+    }
+
+    engPutVariable(engine_, "acqhdr", acqhdr);
+
+    char buffer[2049] = "\0";
+    engOutputBuffer(engine_, buffer, 2048);
+
+    engEvalString(engine_, "acqhdr");
+
+    printf("%s", buffer);
+
+    mxDestroyArray(acqhdr);
+
+    return GADGET_OK;
 }
 
+
 int ImageMatlabGadget::process(GadgetContainerMessage<ISMRMRD::ImageHeader>* m1,
-		GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2)
+        GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2)
 {
 
-	if (this->next()->putq(m1) < 0) {
-		return GADGET_FAIL;
-	}
+    if (this->next()->putq(m1) < 0) {
+        return GADGET_FAIL;
+    }
 
+    ISMRMRD::ImageHeader *img = m1->getObjectPtr();
+    hoNDArray< std::complex<float> > *data = m2->getObjectPtr();
 
-	return GADGET_OK;
+    return GADGET_OK;
 }
 
 
