8f2638b550a9d4762ecb7070ae90fb43c46fecb6
Michael S. Hansen <michael.hansen@nih.gov>
2013-04-21 19:07:43 -0400

    Improvements to Gadgetron web monitor
    Former-commit-id: ada11673c2523090927dc7bc471b4bac59cb496d

--- apps/gadgetron/webapp/gadgetron_web_app.py
+++ apps/gadgetron/webapp/gadgetron_web_app.py
@@ -8,6 +8,18 @@ import ConfigParser
 import os
 import platform
 import threading 
+import signal
+import psutil
+import inspect
+
+run_gadgetron_check = True
+
+def ctrlcsignal(signal, frame):
+    global reactor
+    global run_gadgetron_check
+    print "Shutting down server"
+    run_gadgetron_check = False
+    reactor.stop()
 
 def isGadgetronAlive(port,environment):
     process = subprocess.Popen(["gt_alive","localhost",str(port)], env=environment)
@@ -33,6 +45,8 @@ class GadgetronResource(resource.Resource):
     environment = 0;
     gadgetron_port = 9002
     check_thread = 0
+    run_gadgetron_check = True
+    process_lock = threading.Lock()
 
     def __init__(self, cfgfilename):
         config = ConfigParser.RawConfigParser()
@@ -52,68 +66,80 @@ class GadgetronResource(resource.Resource):
         elif (platform.system() == 'Darwin'):
             self.environment["DYLD_LIBRARY_PATH"]="/usr/local/cuda/lib64:/usr/local/cula/lib64:" +  self.environment["GADGETRON_HOME"] + "/lib:" + ismrmrd_home + "/lib:/opt/local/lib"  
 
+        #self.process_lock.acquire()
         self.gadgetron_process = subprocess.Popen(["gadgetron","-p",self.gadgetron_port], env=self.environment,stdout=gf,stderr=gf)
+        #self.process_lock.release()
         resource.Resource.__init__(self)
         
         self.check_thread = threading.Thread(target=self.check_gadgetron)
         self.check_thread.start()
 
     def __del__(self):
-        self.check_thread.stop()
+        self.run_gadgetron_check = False
+        self.check_thread.join()
         self.gadgetron_process.terminate()
 
     def restart_gadgetron(self):
+        self.process_lock.acquire()
         s = self.gadgetron_process.poll()
         if (s == None):
             self.gadgetron_process.kill()
+            time.sleep(2)
         gf = open(self.gadgetron_log_filename,"w")
         self.gadgetron_process = subprocess.Popen(["gadgetron"], env=self.environment,stdout=gf,stderr=gf)
+        time.sleep(2)
+        self.process_lock.release()
 
     def check_gadgetron(self):
-        while (True):
+        global run_gadgetron_check
+        while (run_gadgetron_check):
+            self.process_lock.acquire()
             s = self.gadgetron_process.poll()
+            self.process_lock.release()
             if (s != None):
                 self.restart_gadgetron()
             time.sleep(3)
         
 
-    def render_GET(self, request):
-        gadgetron_restarted = False
-        print_log_file = False
-        
-        if 'command' in request.args:
-            if request.args['command'] == ['restart']:
-                self.restart_gadgetron()
-                time.sleep(2)
-                gadgetron_restarted = True
-
-            if request.args['command'] == ['log']:
-                print_log_file = True
-
+    def render_page(self):
         doc = "<html>\n<body>\n"
         doc += "<h1>Gadgetron Monitor</h1>\n"
 
         alive = (isGadgetronAlive(self.gadgetron_port,self.environment) == 0)
 
-        if gadgetron_restarted:
-            doc += "<div><h2>Gadgetron Restarted</h2></div>"
-    
         doc += "<div>Gadgetron Status: "
 
         if (alive):
-            doc += "<span style=\"color: green;\">[OK]</span>"
+            doc += "<span style=\"color: green;\">[OK]</span></div>"
         else:
-            doc += "<span style=\"color: red;\">[UNRESPONSIVE]</span>"
+            doc += "<span style=\"color: red;\">[UNRESPONSIVE]</span></div>"
             
-        doc += "</div>"
+        doc += "<div><p><span><form method=\"POST\"><input type=\"submit\" value=\"RESTART\"><input type=\"hidden\" name=\"command\" value=\"restart\"></form></span></div>"
+        doc += "<div><p><span><form method=\"POST\"><input type=\"submit\" value=\"REFRESH\"><input type=\"hidden\" name=\"command\" value=\"refresh\"></form></span></div>"
+        if (alive):
+            p = psutil.Process(self.gadgetron_process.pid)
+            doc += "<div><ul>"
+            doc += "<li>Process ID: " + str(self.gadgetron_process.pid) + "</li>"
+            doc += "<li>CPU Percent: " + str(round(p.get_cpu_percent(),2)) + "</li>"
+            doc += "<li>Memory Percent: " + str(round(p.get_memory_percent(),2)) + "</li>"
+            doc += "</ul></div>"
 
-        doc += "<div><a href=\"/gadgetron/?command=log\">[LOG]</a> <a href=\"/gadgetron/?command=restart\">[RESTART]</a></div>\n"
-        
-        if print_log_file:
-            doc += "<iframe width=\"1024\" height=\"768\" src=\"/log\"></iframe>" 
+            doc += "<div><iframe width=\"1024\" height=\"768\" src=\"/log\"></iframe></div>" 
         
         doc += "</body>\n</html>"
         return doc
+
+        
+    def render_GET(self, request):
+        return self.render_page()
+        
+    def render_POST(self, request):
+        if 'command' in request.args:
+            if request.args['command'] == ['restart']:
+                print "Restarting Gadgetron"
+                self.restart_gadgetron()
+
+        return self.render_page()
 
 class GadgetronLogResource(resource.Resource):
     filename = ""
@@ -135,6 +161,8 @@ port = int(config.get('WEBSERVER','port'))
 root = resource.Resource()
 root.putChild('gadgetron',GadgetronResource(sys.argv[1]))
 root.putChild('log', GadgetronLogResource(config.get('GADGETRON','logfile')))
+
+signal.signal(signal.SIGINT, ctrlcsignal)
 
 reactor.listenTCP(port, server.Site(root))
 reactor.run()
