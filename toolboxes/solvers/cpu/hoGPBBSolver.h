184fa2e2ea2c5f3f0282070e68ffe263158a6641
David C Hansen <dch@cs.au.dk>
2013-05-02 12:36:36 +0200

    Removed solver_reciprocal clamp from the gpBBSolvers as it was not require. Fixed a C++ standard issue in opticalFlowSolver. Added missing dependency on libpthread for gadgetron. Fixed the numerous compile time warnings in the cuLaplaceOperator.
    Former-commit-id: 0d988943ab8f479448b66f54af05df5c57850ba5

--- toolboxes/solvers/cpu/hoGPBBSolver.h
+++ toolboxes/solvers/cpu/hoGPBBSolver.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "hoGTBLAS.h"
+#include "hoNDArray_blas.h"
 #include "gpBBSolver.h"
 #include "hoNDArray.h"
 #include "real_utilities.h"
@@ -23,15 +23,6 @@ public:
 		for (int i = 0; i != xdata->get_number_of_elements(); ++i)
 			if ( real(x[i]) <= REAL(0) && real(g[i]) > 0) g[i]=T(0);
 	}
-
-	virtual void solver_reciprocal_clamp( hoNDArray<T>* xdata,REAL threshold){
-		T* x = xdata->get_data_ptr();
-		for (int i = 0; i != xdata->get_number_of_elements(); ++i){
-			if (real(x[i]) < threshold) x[i]= T(0);
-			else x[i]= T(1)/x[i];
-		}
-	}
-
 
 
 
