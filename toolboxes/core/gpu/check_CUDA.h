7e058ce773b22742ebdea4b438f60b948e3849a1
Thomas Sangild <sangild@cs.au.dk>
2013-06-28 21:46:48 +0200

    still another boost exception throw was found
    Former-commit-id: c1a1257931ced1a60e022d43c8e3c6176797606b

--- toolboxes/core/gpu/check_CUDA.h
+++ toolboxes/core/gpu/check_CUDA.h
@@ -6,30 +6,33 @@
 
 #include "GadgetronCuException.h"
 
-#include <boost/exception/detail/attribute_noreturn.hpp>
+namespace Gadgetron {
 
-/**
- *  Should never be used in the code, use CHECK_FOR_CUDA_ERROR(); instead
- *  inspired by cutil.h: CUT_CHECK_ERROR
- */
-inline void CHECK_FOR_CUDA_ERROR(char const * cur_fun, const char* file, const int line) {
+  /**
+   *  Should never be used in the code, use CHECK_FOR_CUDA_ERROR(); instead
+   *  inspired by cutil.h: CUT_CHECK_ERROR
+   */
+  inline void CHECK_FOR_CUDA_ERROR(char const * cur_fun, const char* file, const int line) {
     cudaError_t errorCode = cudaGetLastError();
     if (errorCode != cudaSuccess) {
-    	boost::exception_detail::throw_exception_(Gadgetron::cuda_error(errorCode),cur_fun,file,line);
+      throw cuda_error(errorCode);
     }
 #ifdef DEBUG
     cudaThreadSynchronize();
     errorCode = cudaGetLastError();
     if (errorCode != cudaSuccess) {
-    	boost::exception_detail::throw_exception_(Gadgetron::cuda_error(errorCode),cur_fun,file,line);
+      throw cuda_error(errorCode);
     }
 #endif
+  }
 }
 
 /**
  *  Checks for CUDA errors and throws an exception if an error was detected.
  */
-
 #define CHECK_FOR_CUDA_ERROR(); CHECK_FOR_CUDA_ERROR(BOOST_CURRENT_FUNCTION,__FILE__,__LINE__);
 
-#define CUDA_CALL(res) {cudaError_t errorCode = res; if (errorCode != cudaSuccess) { boost::exception_detail::throw_exception_(cuda_error(errorCode),BOOST_CURRENT_FUNCTION,__FILE__,__LINE__); } }
+/**
+ *  Call "res", checks for CUDA errors and throws an exception if an error was detected.
+ */
+#define CUDA_CALL(res) {cudaError_t errorCode = res; if (errorCode != cudaSuccess) { throw cuda_error(errorCode); }}
