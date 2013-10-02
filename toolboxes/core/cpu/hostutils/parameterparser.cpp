565470f01a3b3d762766647e7d7f7df4241edca1
Thomas Sangild <sangild@cs.au.dk>
2013-10-02 08:58:12 +0200

    minor improvements to the parameterparser's output
    Former-commit-id: 7ad78aa82c5f9bd691d4ca4d68b717fae6dacd38

--- toolboxes/core/cpu/hostutils/parameterparser.cpp
+++ toolboxes/core/cpu/hostutils/parameterparser.cpp
@@ -16,14 +16,12 @@ namespace Gadgetron {
     m_is_required = required;
     m_is_set = false;
 
-    if (m_nr_values > 0)
-      {
+    if (m_nr_values > 0){
 	m_int_value = new int[m_nr_values];
 	m_float_value = new float[m_nr_values];
 	m_string_value = new std::string[m_nr_values];
       }
-    else
-      {
+    else{
 	m_int_value = 0;
 	m_float_value = 0;
 	m_string_value = 0;
@@ -39,36 +37,30 @@ namespace Gadgetron {
 
   const char* CommandLineParameter::get_string_value(unsigned int i)
   {
-    if (i < m_nr_values)
-      {
+    if (i < m_nr_values){
 	return m_string_value[i].c_str();
       }
-    else
-      {
+    else{
 	return 0;
       }
   }
 
   int CommandLineParameter::get_int_value(unsigned int i)
   {
-    if (i < m_nr_values)
-      {
+    if (i < m_nr_values){
 	return m_int_value[i];
       }
-    else
-      {
+    else{
 	return 0;
       }
   }
 
   float CommandLineParameter::get_float_value(unsigned int i)
   {
-    if (i < m_nr_values)
-      {
+    if (i < m_nr_values){
 	return m_float_value[i];
       }
-    else
-      {
+    else{
 	return 0.0f;
       }
   }
@@ -91,18 +83,15 @@ namespace Gadgetron {
   char** CommandLineParameter::set_value(char** argv)
   {
     int args = 0; 
-    for (unsigned int i = 0; i < m_nr_values;i++)
-      {
+    for (unsigned int i = 0; i < m_nr_values;i++){
 	m_string_value[i] = std::string(argv[i]);
-	if (m_type == COMMAND_LINE_FLOAT || m_type == COMMAND_LINE_INT)
-	  {
+	if (m_type == COMMAND_LINE_FLOAT || m_type == COMMAND_LINE_INT){
 	    std::stringstream ss (std::stringstream::in | std::stringstream::out);
 	    ss << m_string_value[i];
 	    ss >> m_float_value[i];
 	    m_int_value[i] = static_cast<int>(m_float_value[i]);
 	  }
-	else
-	  {
+	else{
 	    m_int_value[i] = 1;
 	    m_float_value[i] = 1.0f;
 	  }
@@ -148,8 +137,7 @@ namespace Gadgetron {
     int new_list_size = m_list_size + m_list_increment;
     CommandLineParameter **new_list = new CommandLineParameter*[new_list_size];
 
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
+    for (int i = 0; i < m_number_of_parameters; i++){
 	new_list[i] = m_parameter_list[i];
       }
 
@@ -159,8 +147,7 @@ namespace Gadgetron {
 
   void ParameterParser::delete_list()
   {
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
+    for (int i = 0; i < m_number_of_parameters; i++){
 	delete m_parameter_list[i];
       }
     delete [] m_parameter_list;
@@ -169,7 +156,6 @@ namespace Gadgetron {
   int ParameterParser::add_parameter(char com_switch,CommandLineParameterType type,  unsigned int nr_values, 
 				     const char* desc, bool required, const char* def)
   {
-
     char** argv = new char*[nr_values];
     std::string *arg_list = new std::string[nr_values];
 
@@ -179,8 +165,7 @@ namespace Gadgetron {
     ss << def;
 
     unsigned int args = 0; 
-    while (args < nr_values)
-      {
+    while (args < nr_values){
 	ss >> arg_list[args];
 	argv[args] = (char*)arg_list[args].c_str();
 	args++;
@@ -197,10 +182,8 @@ namespace Gadgetron {
   int ParameterParser::add_parameter(char com_switch,CommandLineParameterType type,  unsigned int nr_values, const char* desc, bool required)
   {
     CommandLineParameter *p = new CommandLineParameter(com_switch, type, nr_values, desc, required);
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
-	if (m_parameter_list[i]->is_switch_equal_to(com_switch))
-	  {
+    for (int i = 0; i < m_number_of_parameters; i++){
+	if (m_parameter_list[i]->is_switch_equal_to(com_switch)){
 	    std::cout << "ParameterParser: Attempt to parameter twice " << com_switch << std::endl;
 	    delete p;
 	    return -1;
@@ -208,12 +191,10 @@ namespace Gadgetron {
       }
     if (m_number_of_parameters >= m_list_size) expand_list();
     m_parameter_list[m_number_of_parameters++] = p;
-    if ((int)p->get_desc().length() > m_max_desc_length)
-      {
+    if ((int)p->get_desc().length() > m_max_desc_length){
 	m_max_desc_length = p->get_desc().length();
       }
-    if ((int)p->get_number_of_values() > m_max_number_values) 
-      {
+    if ((int)p->get_number_of_values() > m_max_number_values) {
 	m_max_number_values = p->get_number_of_values();
       }
     return 0;
@@ -222,54 +203,54 @@ namespace Gadgetron {
   int ParameterParser::parse_parameter_list(int argc, char** argv)
   {
     int a = 0;
+    int ret = 0;
     m_command_name = std::string(argv[a++]);
     bool argument_found;
-    while (a < argc)
-      {
-	if (argv[a][0] != '-')
-	  {
+    while (a < argc){
+	if (argv[a][0] != '-'){
 	    std::cout << "ParameterParser: malformed argument list" << std::endl;
 	    print_usage();
 	  }
 
 	argument_found = false;
-	for (int i = 0; i < m_number_of_parameters; i++)
-	  {
-	    if (m_parameter_list[i]->is_switch_equal_to(argv[a][1]))
-	      {
-		if (m_parameter_list[i]->get_number_of_values() <= argc-a-1)
-		  {
+	for (int i = 0; i < m_number_of_parameters; i++){
+	    if (m_parameter_list[i]->is_switch_equal_to(argv[a][1])){
+		if (m_parameter_list[i]->get_number_of_values() <= argc-a-1){
 		    m_parameter_list[i]->set_value((argv+a+1));
 		    a += m_parameter_list[i]->get_number_of_values()+1;
 		    argument_found = true;
 		    break;
-		  }
-		else
-		  {
-		    std::cout << "ParameterParser: malformed argument list" << std::endl;
-		    print_usage();
-		    return -1;
-		  }
-	      }
-	  }
-	if (!argument_found)
-	  {
-	    std::cout << "ParameterParser: unknown argument " << argv[a] << std::endl;
-	    print_usage();
-	    return -1;
-	  }
-      }
-    return 0;
-  }
+		}
+		else{
+		  std::cout << std::endl << "ParameterParser: malformed argument list: -" << argv[a][1] << std::endl;
+		  //print_usage();
+		  argument_found = true;
+		  a++;
+		  while (a < argc && argv[a][0] != '-') a++;
+		  ret = -1;
+		  break;
+		}
+	    }
+	}
 
+	if (!argument_found){
+	  std::cout << std::endl << "ParameterParser: unknown argument: -" << argv[a][1] << std::endl;
+	  //print_usage();
+	  ret = -1;
+	  a++;
+	  while (a < argc && argv[a][0] != '-') a++;
+	}
+    }
+    return ret;
+  }
+  
   void ParameterParser::print_usage()
   {
     int space_fill = 0;
 
     std::cout << "---------------------------------------------------- " << std::endl;
     std::cout << "Usage: " << m_command_name << " -[";
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
+    for (int i = 0; i < m_number_of_parameters; i++){
 	std::cout << m_parameter_list[i]->get_switch();
       }
     std::cout << "]" << std::endl;
@@ -277,24 +258,19 @@ namespace Gadgetron {
     for (int i = 0; i < m_number_of_parameters; i++)
       {
 	std::cout << " -" << m_parameter_list[i]->get_switch() << " ";
-	if (m_max_number_values > 1)
-	  {
-	    if (m_parameter_list[i]->get_number_of_values() > 1)
-	      {
+	if (m_max_number_values > 1){
+	    if (m_parameter_list[i]->get_number_of_values() > 1){
 		std::cout << m_parameter_list[i]->get_number_of_values() << "x "; 
 	      }
-	    else
-	      {
+	    else{
 		std::cout  << "   "; 
 	      }
 	  }
-	if (m_parameter_list[i]->get_number_of_values() > 0)
-	  {
+	if (m_parameter_list[i]->get_number_of_values() > 0){
 	    std::cout << "[" << m_parameter_list[i]->get_desc() << "]";
 	    space_fill = (m_max_desc_length - m_parameter_list[i]->get_desc().length())+2;
 	  }
-	else
-	  {
+	else{
 	    space_fill = m_max_desc_length+2+2;
 	  }
 	std::cout << std::endl;
@@ -305,31 +281,34 @@ namespace Gadgetron {
   void ParameterParser::print_parameter_list()
   {
     std::cout << "---------------------------------------------------- " << std::endl;
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
-	std::cout << "  ";
-	std::cout << std::setw(m_max_desc_length+2) << std::setiosflags(std::ios::left);
-	std::cout << m_parameter_list[i]->get_desc() << ": ";
-	if (m_parameter_list[i]->get_number_of_values() > 0)
-	  {
-	    for (int j = 0; j < m_parameter_list[i]->get_number_of_values(); j++)
-	      {
-		std::cout << m_parameter_list[i]->get_string_value(j) << " ";
-	      }
+    for (int i = 0; i < m_number_of_parameters; i++){
+      std::cout << "  ";
+      std::cout << std::setw(m_max_desc_length+2) << std::setiosflags(std::ios::left);
+      std::cout << m_parameter_list[i]->get_desc() << ": ";
+      if (m_parameter_list[i]->get_is_set()){
+	for (int j = 0; j < m_parameter_list[i]->get_number_of_values(); j++){
+	  std::cout << m_parameter_list[i]->get_string_value(j) << " ";
+	  if (!m_parameter_list[i]->get_is_required()){
+	    std::cout << "(optional)";
 	  }
-	else
-	  {
-	    std::cout << m_parameter_list[i]->get_is_set();
-	  }
-	std::cout << std::endl;
+	}
       }
+      else{
+	if (m_parameter_list[i]->get_is_required()){
+	  std::cout << "(missing)";
+	}
+	else{
+	  std::cout << "N/A (optional)";
+	}
+      }
+      std::cout << std::endl;
+    }
     std::cout << "---------------------------------------------------- " << std::endl;
   }
-
+  
   bool ParameterParser::all_required_parameters_set()
   {
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
+    for (int i = 0; i < m_number_of_parameters; i++){
 	if (!m_parameter_list[i]->get_is_set() && m_parameter_list[i]->get_is_required())
 	  return false;
       }
@@ -338,10 +317,8 @@ namespace Gadgetron {
 
   CommandLineParameter* ParameterParser::get_parameter(char com_switch)
   {
-    for (int i = 0; i < m_number_of_parameters; i++)
-      {
-	if (m_parameter_list[i]->is_switch_equal_to(com_switch))
-	  {
+    for (int i = 0; i < m_number_of_parameters; i++){
+	if (m_parameter_list[i]->is_switch_equal_to(com_switch)){
 	    return m_parameter_list[i];
 	  }
       }
