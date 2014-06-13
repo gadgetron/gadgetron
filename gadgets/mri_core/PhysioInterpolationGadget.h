04fa03bee77ad8feb16f265634022f0c0e9ce594
Hui Xue <hui.xue@nih.gov>
2014-06-13 09:48:56 -0400

    extend the phyiso interp gadget to handle multiple slices
    Former-commit-id: 8e3d21f50c5b890e1829bd2e45c3e0424a58a708

--- gadgets/mri_core/PhysioInterpolationGadget.h
+++ gadgets/mri_core/PhysioInterpolationGadget.h
@@ -11,33 +11,40 @@
 
 namespace Gadgetron{  
 
-    class EXPORTGADGETSMRICORE PhysioInterpolationGadget :
-        public Gadget2< ISMRMRD::ImageHeader, hoNDArray< std::complex<float> > >
+    class EXPORTGADGETSMRICORE PhysioInterpolationGadget : public Gadget2<ISMRMRD::ImageHeader, hoNDArray< std::complex<float> > >
     {
-
     public:
         GADGET_DECLARE(PhysioInterpolationGadget);
 
         PhysioInterpolationGadget();
         virtual ~PhysioInterpolationGadget();
 
-	inline unsigned short get_number_of_phases() { return phases_to_reconstruct_; }
+    inline unsigned short get_number_of_phases() { return phases_to_reconstruct_; }
 
     protected:
         virtual int process_config(ACE_Message_Block* mb);
 
-        virtual int process(GadgetContainerMessage< ISMRMRD::ImageHeader >* m1,
-            GadgetContainerMessage< hoNDArray< std::complex<float> > > * m2);
-	
-	virtual int close(unsigned long flags); //All the work is done here in this Gadget
+        virtual int process(GadgetContainerMessage< ISMRMRD::ImageHeader >* m1, GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2);
 
-	unsigned short phys_time_index_;
-	unsigned short phases_to_reconstruct_;
-	unsigned short mode_; //0=seperate series for each complete RR,
-	                      //1=First complete RR interval only	
+        virtual int close(unsigned long flags); //All the work is done here in this Gadget
+
+        unsigned short phys_time_index_;
+        unsigned short phases_to_reconstruct_;
+        unsigned short mode_; //0=seperate series for each complete RR,
+                              //1=First complete RR interval only
+
+        // true, if the first beat is on trigger
+        /// false, the first beat will be ignored
+        bool first_beat_on_trigger_;
+
     private:
-	ACE_Message_Queue<ACE_MT_SYNCH> buffer_;
-	std::vector<float> time_stamps_;
+
+        std::vector< boost::shared_ptr< ACE_Message_Queue<ACE_MT_SYNCH> > > buffer_;
+        std::vector< std::vector<float> > time_stamps_;
+
+        size_t slc_limit_;
+
+        bool image_with_attrib_;
     };
 }
 
