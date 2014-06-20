4894d5b127b992e05a751adbffd0a8fe26e9ab06
Joseph Naegele <joseph.naegele@gmail.com>
2014-06-20 14:11:49 -0400

    fixing integration test issue when no LD_LIBRARY_PATH set
    Former-commit-id: ff19058e24da02ec9372d6485a13bf434f9e7b58

--- test/integration/run_gadgetron_test.py
+++ test/integration/run_gadgetron_test.py
@@ -261,7 +261,8 @@ def main():
         myenv[libpath] += "/usr/local/cula/lib64:"
         myenv[libpath] += "/opt/intel/mkl/lib/intel64:"
         myenv[libpath] += "/opt/intel/lib/intel64:"
-        myenv[libpath] += os.environ[libpath]
+        if os.environ.get(libpath, None) is not None:
+            myenv[libpath] += os.environ[libpath]
 
     if platform.system() == "Windows":
         myenv["PATH"] = myenv[libpath]
