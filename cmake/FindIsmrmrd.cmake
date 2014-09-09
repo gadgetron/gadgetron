0db3991b467bed398ea39314683b3f4eb57598ba
Dusan Puletic <dusan.puletic@nih.gov>
2014-09-09 11:49:47 -0400

    CMake changes for simplifying Windows build scripts
    Former-commit-id: b0877563bd8d80d765f49f269ae82fe895ec0a45

--- cmake/FindIsmrmrd.cmake
+++ cmake/FindIsmrmrd.cmake
@@ -10,10 +10,10 @@ FIND_PATH( ISMRMRD_INCLUDE_DIR ismrmrd.h
 HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr PATH_SUFFIXES include ismrmrd ismrmrd/include)
 
 FIND_PATH( ISMRMRD_XSD_INCLUDE_DIR ismrmrd.hxx
-HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
+HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr $ENV{ISMRMRD_HOME}/../compiling/ismrmrd/src/xsd PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
 
 FIND_PATH( ISMRMRD_SCHEMA_DIR ismrmrd.xsd 
-HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
+HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr $ENV{ISMRMRD_HOME}/../compiling/ismrmrd/src/xsd PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
 
 FIND_LIBRARY( ISMRMRD_LIBRARY
               NAMES ismrmrd
@@ -27,8 +27,7 @@ SET(ISMRMRD_LIBRARIES ${ISMRMRD_LIBRARY} ${ISMRMRD_XML_LIBRARY})
 
 FIND_FILE( ISMRMRD_XSD_SOURCE
            NAMES "ismrmrd.cxx"
-           HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
-
+           HINTS $ENV{ISMRMRD_HOME} PATHS /usr/local /usr $ENV{ISMRMRD_HOME}/../compiling/ismrmrd/src/xsd PATH_SUFFIXES schema ismrmrd ismrmrd/schema)
 INCLUDE( "FindPackageHandleStandardArgs" )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( "Ismrmrd" DEFAULT_MSG ISMRMRD_INCLUDE_DIR ISMRMRD_LIBRARIES ISMRMRD_SCHEMA_DIR)
 
