0777dbe652ff57b6d12adaf93fb3eedb165bcd94
Michael S. Hansen <michael.hansen@nih.gov>
2014-09-27 12:28:14 -0400

    Implemented crossplatform function to get GADGETRON_HOME when not set
    Former-commit-id: 1bba8fd53c991d65610ffe3bca251ef36bf4ed33

--- apps/gadgetron/main.cpp
+++ apps/gadgetron/main.cpp
@@ -12,10 +12,14 @@
 #include <string>
 #include <fstream>
 #include <streambuf>
-
+#include <limits.h>
+#include <unistd.h>
 
 #ifdef _WIN32
     #include <windows.h>
+    #include <windows.h>
+    #include <Shlwapi.h>
+    #pragma comment(lib, "shlwapi.lib")
 #else
     #include <sys/types.h>
     #include <sys/stat.h>
@@ -27,8 +31,57 @@ using namespace boost::filesystem;
 using namespace Gadgetron;
 
 #define GT_WORKING_DIRECTORY "workingDirectory"
+#define MAX_GADGETRON_HOME_LENGTH 1024
 
 namespace Gadgetron {
+
+  std::string get_gadgetron_home() 
+  {
+
+#if defined  __APPLE_
+    char path[MAX_GADGETRON_HOME_LENGTH];
+    uint32_t size = sizeof(path);
+    if (_NSGetExecutablePath(path, &size) == 0) {
+      std::string s1(path);
+      return s1.substr(0, s1.find_last_of("\\/")) + std::string("/../");
+    } else {
+      std::cout << "Unable to determine GADGETRON_HOME" << std::endl;
+      return std::string("");
+    }
+#elif defined _WIN32 || _WIN64
+    // Full path to the executable (including the executable file)
+    char fullPath[MAX_GADGETRON_HOME_LENGTH];	
+    // Full path to the executable (without executable file)
+    char *rightPath;
+    // Will contain exe path
+    HMODULE hModule = GetModuleHandle(NULL);
+    if (hModule != NULL)
+      {
+	// When passing NULL to GetModuleHandle, it returns handle of exe itself
+	GetModuleFileName(hModule, fullPath, (sizeof(fullPath))); 
+	rightPath = fullPath;
+	PathRemoveFileSpec(rightPath);
+	std::string s1(rightPath);
+	return s1 + std::string("\\..\\");
+
+    }
+    else
+    {
+        std::cout << "The path to the executable is NULL" << std::endl;
+    }
+#else //Probably some NIX where readlink should work
+    char buff[MAX_GADGETRON_HOME_LENGTH];
+    ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff)-1);
+    if (len != -1) {
+      buff[len] = '\0';
+      std::string s1(buff);
+      return s1.substr(0, s1.find_last_of("\\/")) + std::string("/../");
+    } else {
+      std::cout << "Unable to determine GADGETRON_HOME" << std::endl;
+      return std::string("");
+    }
+#endif
+  }
 
 bool create_folder_with_all_permissions(const std::string& workingdirectory)
 {
@@ -80,6 +133,8 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
             ACE_Log_Msg::PROCESS);
 
     char * gadgetron_home = ACE_OS::getenv("GADGETRON_HOME");
+
+    std::cout << "GADGETRON_HOME determined from executable: " << get_gadgetron_home() << std::endl;
 
     if (!gadgetron_home || (std::string(gadgetron_home).size() == 0)) {
         ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("GADGETRON_HOME variable not set.\n")),-1);
