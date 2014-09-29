577727e95631741f3b3bb02c7cacf10363ab11a0
Hui Xue <hui.xue@nih.gov>
2014-09-29 19:34:31 -0400

    add linux install dependency script
    Former-commit-id: 4eeae5b6d709a4948b4b985b53dabb8ddefddc15

---
+++ cmake/InstallLinuxDependencies.cmake
@@ -0,0 +1,24 @@
+
+#install dependencies
+if (MKL_FOUND)
+    if (HAS_64_BIT)
+        set(MKL_REDIST_DIR ${MKLROOT_PATH}/mkl/lib/intel64)
+        set(MKL_COMPILER_REDIST_DIR ${MKLROOT_PATH}/lib/intel64)
+    else (HAS_64_BIT)
+        set(MKL_REDIST_DIR ${MKLROOT_PATH}/mkl/lib/ia32)
+        set(MKL_COMPILER_REDIST_DIR ${MKLROOT_PATH}/lib/ia32)
+    endif (HAS_64_BIT)
+
+    message("Install mkl libraries from ${MKL_REDIST_DIR} ")
+    FILE(GLOB MKL_DLL ${MKL_REDIST_DIR}/*.so)
+    foreach(fileName ${MKL_DLL})
+        message("Install ${fileName} ")
+        install( FILES ${fileName} DESTINATION lib )
+    endforeach(fileName)
+
+    FILE(GLOB MKL_COMPILER_DLL ${MKL_COMPILER_REDIST_DIR}/libiomp5*.so)
+    foreach(fileName ${MKL_COMPILER_DLL})
+        message("Install ${fileName} ")
+        install( FILES ${fileName} DESTINATION lib )
+    endforeach(fileName)
+endif (MKL_FOUND)
