681465ca28381402a40887abb72fc214f6125109
Hui Xue <hui.xue@nih.gov>
2014-10-02 17:41:13 -0400

    made the hoMatrix call changes
    Former-commit-id: 75b3abfb6efbc22c0a631b1e099699b91ce549a8

--- apps/clients/gadgetron_ismrmrd_client/gadgetron_ismrmrd_client.cpp
+++ apps/clients/gadgetron_ismrmrd_client/gadgetron_ismrmrd_client.cpp
@@ -162,7 +162,7 @@ public:
         //Read the image from the socket
         ISMRMRD::ImageHeader h;
         boost::asio::read(*stream, boost::asio::buffer(&h,sizeof(ISMRMRD::ImageHeader)));
-        ISMRMRD::Image im; 
+        ISMRMRD::Image<T> im; 
         im.setHead(h);
         boost::asio::read(*stream, boost::asio::buffer(im.getData(), im.getDataSize()));
         {
@@ -217,7 +217,7 @@ public:
         //Read the image headerfrom the socket
         ISMRMRD::ImageHeader h;
         boost::asio::read(*stream, boost::asio::buffer(&h,sizeof(ISMRMRD::ImageHeader)));
-        ISMRMRD::Image im;
+        ISMRMRD::Image<T> im;
         im.setHead(h);
 
         typedef unsigned long long size_t_type;
--- toolboxes/core/cpu/arma_math/hoMatrix_util.h
+++ toolboxes/core/cpu/arma_math/hoMatrix_util.h
@@ -4,83 +4,57 @@
 #include "cpucore_math_export.h"
 #include "hoMatrix.h"
 
-#ifdef USE_MKL
-    #include "mkl.h"
-#endif // USE_MKL
-
-#ifdef USE_ARMADILLO
-#include "hoArmadillo.h"
-#endif // USE_ARMADILLO
-
 #ifdef GT_Complex8
-#undef GT_Complex8
+    #undef GT_Complex8
 #endif // GT_Complex8
 typedef std::complex<float> GT_Complex8;
 
 #ifdef GT_Complex16
-#undef GT_Complex16
+    #undef GT_Complex16
 #endif // GT_Complex16
 typedef std::complex<double> GT_Complex16;
+
+#ifndef lapack_int
+    #ifdef USE_MKL
+        #ifdef MKL_ILP64
+            #define lapack_int __int64
+        #else
+            #define lapack_int int
+        #endif // MKL_ILP64
+    #else
+        #define lapack_int int
+    #endif // USE_MKL
+#endif // lapack_int
+
+/// ----------------------------------------------------------------------
+/// the fortran interface of lapack and blas functions are called
+/// if the MKL is used and MKL_ILP64 is defined,
+/// the 64bit functions are linked, meaning the size of single array can 
+/// be over 4GB
+/// ----------------------------------------------------------------------
 
 namespace Gadgetron
 {
 
-#if defined(USE_MKL) || defined(USE_ARMADILLO)
+#if defined(USE_MKL) || defined(USE_LAPACK) || defined(USE_ARMADILLO)
 
-template<typename T> EXPORTCPUCOREMATH
-bool EigenAnalysis_syev_heev2(hoMatrix<T>& A, hoMatrix<T>& eigenValue);
+    template<typename T> EXPORTCPUCOREMATH
+    bool EigenAnalysis_syev_heev2(hoMatrix<T>& A, hoMatrix<T>& eigenValue);
 
-template<typename T> EXPORTCPUCOREMATH
-bool SolveLinearSystem_Tikhonov(hoMatrix<T>& A, hoMatrix<T>& b, hoMatrix<T>& x, double lamda);
+    template<typename T> EXPORTCPUCOREMATH
+    bool SolveLinearSystem_Tikhonov(hoMatrix<T>& A, hoMatrix<T>& b, hoMatrix<T>& x, double lamda);
 
-#endif // defined(USE_MKL) || defined(USE_ARMADILLO)
+#endif // defined(USE_MKL) || defined(USE_LAPACK) || defined(USE_ARMADILLO)
 
-// following matrix computation calls MKL functions
-#ifdef USE_MKL
+// ----------------------------------------------------------------------------
 
-/// C = A*B for complex float
-EXPORTCPUCOREMATH bool GeneralMatrixProduct_gemm_CXFL(hoNDArray< std::complex<float> >& C, const hoNDArray< std::complex<float> >& A, const hoNDArray< std::complex<float> >& B);
+// following matrix computation calls lapacke functions
+#if defined(USE_MKL) || defined(USE_LAPACK)
 
-template<typename T> EXPORTCPUCOREMATH
-bool GeneralMatrixProduct_gemm(hoNDArray<T>& C, 
-                            const hoNDArray<T>& A, bool transA, 
-                            const hoNDArray<T>& B, bool transB);
+    /// C = A*B for complex float
+    EXPORTCPUCOREMATH bool GeneralMatrixProduct_gemm_CXFL(hoNDArray< std::complex<float> >& C, const hoNDArray< std::complex<float> >& A, const hoNDArray< std::complex<float> >& B);
 
-template<typename T> EXPORTCPUCOREMATH 
-bool GeneralMatrixProduct_gemm(hoMatrix<T>& C, 
-                            const hoMatrix<T>& A, bool transA, 
-                            const hoMatrix<T>& B, bool transB);
-
-template<typename T> EXPORTCPUCOREMATH 
-bool CholeskyHermitianPositiveDefinite_potrf(hoMatrix<T>& A, char uplo);
-
-template<typename T> EXPORTCPUCOREMATH 
-bool EigenAnalysis_syev_heev(hoMatrix<T>& A, hoMatrix<typename realType<T>::Type>& eigenValue);
-
-template<typename T> EXPORTCPUCOREMATH 
-bool SymmetricHermitianPositiveDefiniteInverse_potri(hoMatrix<T>& A);
-
-template<typename T> EXPORTCPUCOREMATH 
-bool TriangularInverse_trtri(hoMatrix<T>& A, char uplo);
-
-template<typename T> EXPORTCPUCOREMATH 
-bool SymmetricHermitianPositiveDefiniteLinearSystem_posv(hoMatrix<T>& A, hoMatrix<T>& b);
-
-/// Computes the LU factorization of a general m-by-n matrix
-/// this function is called by general matrix inversion
-template<typename T> EXPORTCPUCOREMATH 
-bool LUFactorizationGeneralMatrix_getrf(hoMatrix<T>& A, hoNDArray<lapack_int>& ipiv);
-
-/// Computes the inverse of an LU-factored general matrix
-template<typename T> EXPORTCPUCOREMATH 
-bool InverseGeneralMatrix_getri(hoMatrix<T>& A);
-
-#else
-
-    // matrix computation calls armadillo
-    #ifdef USE_ARMADILLO
-
-    template<typename T> EXPORTCPUCOREMATH 
+    template<typename T> EXPORTCPUCOREMATH
     bool GeneralMatrixProduct_gemm(hoNDArray<T>& C, 
                                 const hoNDArray<T>& A, bool transA, 
                                 const hoNDArray<T>& B, bool transB);
@@ -91,13 +65,13 @@ bool InverseGeneralMatrix_getri(hoMatrix<T>& A);
                                 const hoMatrix<T>& B, bool transB);
 
     template<typename T> EXPORTCPUCOREMATH 
-    bool SymmetricHermitianPositiveDefiniteInverse_potri(hoMatrix<T>& A);
-
-    template<typename T> EXPORTCPUCOREMATH 
     bool CholeskyHermitianPositiveDefinite_potrf(hoMatrix<T>& A, char uplo);
 
     template<typename T> EXPORTCPUCOREMATH 
     bool EigenAnalysis_syev_heev(hoMatrix<T>& A, hoMatrix<typename realType<T>::Type>& eigenValue);
+
+    template<typename T> EXPORTCPUCOREMATH 
+    bool SymmetricHermitianPositiveDefiniteInverse_potri(hoMatrix<T>& A);
 
     template<typename T> EXPORTCPUCOREMATH 
     bool TriangularInverse_trtri(hoMatrix<T>& A, char uplo);
@@ -105,11 +79,50 @@ bool InverseGeneralMatrix_getri(hoMatrix<T>& A);
     template<typename T> EXPORTCPUCOREMATH 
     bool SymmetricHermitianPositiveDefiniteLinearSystem_posv(hoMatrix<T>& A, hoMatrix<T>& b);
 
+    /// Computes the LU factorization of a general m-by-n matrix
+    /// this function is called by general matrix inversion
+    template<typename T> EXPORTCPUCOREMATH 
+    bool LUFactorizationGeneralMatrix_getrf(hoMatrix<T>& A, hoNDArray<lapack_int>& ipiv);
+
+    /// Computes the inverse of an LU-factored general matrix
     template<typename T> EXPORTCPUCOREMATH 
     bool InverseGeneralMatrix_getri(hoMatrix<T>& A);
 
+#else
+
+    // matrix computation calls armadillo
+    #ifdef USE_ARMADILLO
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool GeneralMatrixProduct_gemm(hoNDArray<T>& C, 
+                                    const hoNDArray<T>& A, bool transA, 
+                                    const hoNDArray<T>& B, bool transB);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool GeneralMatrixProduct_gemm(hoMatrix<T>& C, 
+                                    const hoMatrix<T>& A, bool transA, 
+                                    const hoMatrix<T>& B, bool transB);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool SymmetricHermitianPositiveDefiniteInverse_potri(hoMatrix<T>& A);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool CholeskyHermitianPositiveDefinite_potrf(hoMatrix<T>& A, char uplo);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool EigenAnalysis_syev_heev(hoMatrix<T>& A, hoMatrix<typename realType<T>::Type>& eigenValue);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool TriangularInverse_trtri(hoMatrix<T>& A, char uplo);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool SymmetricHermitianPositiveDefiniteLinearSystem_posv(hoMatrix<T>& A, hoMatrix<T>& b);
+
+        template<typename T> EXPORTCPUCOREMATH 
+        bool InverseGeneralMatrix_getri(hoMatrix<T>& A);
+
     #endif // USE_ARMADILLO
 
-#endif // USE_MKL
+#endif // defined(USE_MKL) || defined(USE_LAPACK)
 
 }
