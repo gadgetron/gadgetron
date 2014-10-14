7d146bd596684b27c316b3540972bb8523315ac3
Hui Xue <hui.xue@nih.gov>
2014-10-14 14:35:08 -0400

    speed up coil map estimatin
    Former-commit-id: 600489bccab62e41733cbd2303c2b9393d243629

--- toolboxes/gtplus/workflow/gtPlusISMRMRDReconUtil.hxx
+++ toolboxes/gtplus/workflow/gtPlusISMRMRDReconUtil.hxx
@@ -4648,8 +4648,16 @@ coilMap2DNIHInner(const hoNDArray<T>& data, hoNDArray<T>& coilMap, size_t ks, si
                         const value_type b = v.imag();
 
                         // pV1[cha] = phaseU1 * std::conj(pV1[cha]);
-                        pV1[cha].real(a*c+b*d);
-                        pV1[cha].imag(a*d-b*c);
+#ifdef WIN32
+                        /*pV1[cha].real(a*c+b*d);
+                        pV1[cha].imag(a*d-b*c);*/
+
+                        reinterpret_cast< value_type(&)[2] >(pV1[cha])[0] = a*c+b*d;
+                        reinterpret_cast< value_type(&)[2] >(pV1[cha])[1] = a*d-b*c;
+#else
+                        pV1[cha].real() = a*c+b*d;
+                        pV1[cha].imag() = a*d-b*c;
+#endif // WIN32
                     }
 
                     for ( cha=0; cha<CHA; cha++ )
