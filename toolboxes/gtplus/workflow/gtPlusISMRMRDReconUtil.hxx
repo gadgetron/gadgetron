0e94f63886f5c9476703e49f5357ba2ba1da1c76
Souheil Inati <souheil.inati@nih.gov>
2014-10-16 22:59:18 -0400

    Fixed compilation error on mac .real() can't be LHS
    Former-commit-id: 41416d63b9bd966a980bcc89f3166a99360e4c5c

--- toolboxes/gtplus/workflow/gtPlusISMRMRDReconUtil.hxx
+++ toolboxes/gtplus/workflow/gtPlusISMRMRDReconUtil.hxx
@@ -4647,17 +4647,8 @@ coilMap2DNIHInner(const hoNDArray<T>& data, hoNDArray<T>& coilMap, size_t ks, si
                         const value_type a = v.real();
                         const value_type b = v.imag();
 
-                        // pV1[cha] = phaseU1 * std::conj(pV1[cha]);
-#ifdef WIN32
-                        /*pV1[cha].real(a*c+b*d);
-                        pV1[cha].imag(a*d-b*c);*/
-
                         reinterpret_cast< value_type(&)[2] >(pV1[cha])[0] = a*c+b*d;
                         reinterpret_cast< value_type(&)[2] >(pV1[cha])[1] = a*d-b*c;
-#else
-                        pV1[cha].real() = a*c+b*d;
-                        pV1[cha].imag() = a*d-b*c;
-#endif // WIN32
                     }
 
                     for ( cha=0; cha<CHA; cha++ )
