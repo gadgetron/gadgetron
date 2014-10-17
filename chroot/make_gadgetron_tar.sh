c83d86be994198ea65a4aeb0070a1caa3ac7d7bf
Dusan Puletic <dusan.puletic@nih.gov>
2014-10-17 18:56:58 +0000

    chroot script updated
    Former-commit-id: 0f759ca863121906d4307ce3d20fa6aa0362dafb

--- chroot/make_gadgetron_tar.sh
+++ chroot/make_gadgetron_tar.sh
@@ -4,27 +4,28 @@ if [ $# -eq 3 ]; then
 
  sudo apt-get install debootstrap -y
 
- sudo debootstrap --variant=buildd --arch amd64 trusty "${2}" http://gb.archive.ubuntu.com/ubuntu/
+ sudo mkdir -p "${2}/gadgetron"
 
- "${3}/generate_gadgetron_root" "${1}" "${2}"
+ sudo debootstrap --variant=buildd --arch amd64 trusty "${2}/gadgetron" http://gb.archive.ubuntu.com/ubuntu/
+
+ "${3}/generate_gadgetron_root" "${1}" "${2}/gadgetron"
 
  # *** Uncomment if you have MKL compiled ***
 
- #sudo rm -rf "${2}/opt/intel/mkl/lib/intel64"
- #sudo rm -rf "${2}/opt/intel/lib/intel64"
- #sudo cp -rf /opt/intel/mkl/lib/intel64 "${2}/opt/intel/mkl/lib/intel64"
- #sudo cp -rf /opt/intel/lib/intel64 "${2}/opt/intel/lib/intel64"
+ #sudo rm -rf "${2}/gadgetron/opt/intel/mkl/lib/intel64"
+ #sudo rm -rf "${2}/gadgetron/opt/intel/lib/intel64"
+ #sudo cp -rf /opt/intel/mkl/lib/intel64 "${2}/gadgetron/opt/intel/mkl/lib/intel64"
+ #sudo cp -rf /opt/intel/lib/intel64 "${2}/gadgetron/opt/intel/lib/intel64"
 
- sudo cp "${3}/start-gadgetron.sh" "${2}"
- sudo cp "${3}/start.sh" "${2}/../"
- sudo cp "${3}/stop.sh" "${2}/../"
+ sudo cp "${3}/start-gadgetron.sh" "${2}/gadgetron"
+ sudo cp "${3}/start.sh" "${2}"
+ sudo cp "${3}/stop.sh" "${2}"
 
- sudo tar -zcvpf "${3}/gadgetron.tar.gz" --directory "${2}/../" --exclude="${2}/proc" --exclude="${2}/sys" --exclude="${2}/dev" "${2}/../"
-
+ sudo tar -zcvf "${3}/gadgetron_chroot.tar.gz" --directory "${2}" .
  exit 0
 
 else
- echo -e "\nUsage:  $0 (gadgetron_root) (chrootdir) (scripts dir)\n"
+ echo -e "\nUsage:      $0 (gadgetron_root) (chrootdir) (scripts dir)\nImportant:  Make sure that the chrootdir is empty!\n"
  exit 1
 fi
 
