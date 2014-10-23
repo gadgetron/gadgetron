e0c6040f1c661da24d0dfac37544b24f2183c475
Dusan Puletic <dusan.puletic@nih.gov>
2014-10-23 19:03:30 +0000

    chroot scripts updated
    Former-commit-id: 639bfad5094c4b8924ed80f1943a4c93a3506266

--- chroot/create_chroot.sh
+++ chroot/create_chroot.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-if [ $# -eq 3 ]; then
+if [ $# -eq 4 ]; then
 
- cd ${3} 
+ sudo rm -rf "${3}/chroot-root"
 
- sudo rm -rf "${2}/chroot-root"
+ mkdir -p "${3}/chroot-root"
 
- mkdir -p "${2}/chroot-root"
+ mkdir -p "${3}/chroot-root/gadgetron"
 
- mkdir -p "${2}/chroot-root/gadgetron"
-
- mkdir -p "${2}/chroot-backups"
+ mkdir -p "${3}/chroot-backups"
 
  sudo apt-get install debootstrap -y
 
- sudo debootstrap --variant=buildd --arch amd64 trusty "${2}/chroot-root/gadgetron" http://gb.archive.ubuntu.com/ubuntu/
+ sudo debootstrap --variant=buildd --arch amd64 trusty "${3}/chroot-root/gadgetron" http://gb.archive.ubuntu.com/ubuntu/
 
- sudo make install DESTDIR="${2}/chroot-root/gadgetron" -j8
+ cd ${4}
 
- "${2}/generate_gadgetron_root" ${1} "${2}/chroot-root/gadgetron"
+ sudo make install DESTDIR="${3}/chroot-root/gadgetron" -j8
 
- cp ${2}/start.sh "${2}/chroot-root"
- cp ${2}/stop.sh "${2}/chroot-root"
- sudo cp ${2}/start-gadgetron.sh "${2}/chroot-root/gadgetron" 
+ "${2}/generate_gadgetron_root" ${1} "${3}/chroot-root/gadgetron"
 
- sudo cp -n ${2}/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${2}/chroot-root/gadgetron${1}/config/gadgetron.xml
+ cp ${2}/start.sh "${3}/chroot-root"
+ cp ${2}/stop.sh "${3}/chroot-root"
+ cp ${2}/start-env.sh "${3}/chroot-root" 
+ 
+ sudo chmod +x ${3}/start-gadgetron.sh
+ sudo cp ${3}/start-gadgetron.sh "${3}/chroot-root/gadgetron" 
+ 
+ sudo chmod +x ${3}/enter-chroot-env.sh
+ sudo cp ${3}/enter-chroot-env.sh "${3}/chroot-root/gadgetron"
 
- tar -zcf "${2}/chroot-backups/gadgetron-chroot-`date '+%d-%B-%Y'`.tar.gz" --directory "${2}/chroot-root" --exclude=./gadgetron/etc --exclude=./gadgetron/var --exclude=./gadgetron/dev --exclude=./gadgetron/root .
+ sudo cp -n ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml
 
- sudo rm -rf "${2}/chroot-root"
+ tar -zcf "${3}/chroot-backups/gadgetron-chroot-`date '+%d-%B-%Y'`.tar.gz" --directory "${3}/chroot-root" --exclude=./gadgetron/etc --exclude=./gadgetron/var --exclude=./gadgetron/dev --exclude=./gadgetron/root .
+
+ sudo rm -rf "${3}/chroot-root"
  
  exit 0
 
 else
- echo -e "\nUsage:  $0 (gadgetron_root) (chroot scripts dir) (gadgetron build dir)\n"
+ echo -e "\nUsage:  $0 (gadgetron install dir) (chroot source dir) (chroot build dir) (gadgetron build dir)\n"
  exit 1
 fi
