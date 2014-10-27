ebba5e0a4dfd8de8a3160b5a763104886a25cf00
David C Hansen <dch@cs.au.dk>
2014-10-27 12:43:52 +0100

    Added partial derivative operators with non-cycle boundary conditions
    Former-commit-id: 5650972cbd1618ae283adbfea5bf3e3ff2808a9d

---
+++ toolboxes/operators/gpu/cuPartialDerivativeOperator2.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "linearOperator.h"
+#include "vector_td.h"
+#include "cuNDArray.h"
+
+
+namespace Gadgetron {
+template <class T, unsigned int D> class cuPartialDerivativeOperator2
+	: public linearOperator<cuNDArray<T> >
+{
+
+public:
+
+  cuPartialDerivativeOperator2() : linearOperator<cuNDArray<T> >() {}
+  virtual ~cuPartialDerivativeOperator2() {}
+
+  virtual void mult_M( cuNDArray<T> *in,cuNDArray<T> *out, bool accumulate = false );
+
+
+  virtual void mult_MH(cuNDArray<T> *in, cuNDArray<T> *out, bool accumulate = false );
+
+  virtual void mult_MH_M( cuNDArray<T> *in, cuNDArray<T>*out, bool accumulate = false );
+  virtual boost::shared_ptr< linearOperator<  cuNDArray< T > > > clone()
+    {
+      return linearOperator< cuNDArray<T> >::clone(this);
+    }
+
+};
+}
