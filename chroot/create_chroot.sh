029427185a690f098d65aabe431cb0f3f9741e25
Dusan Puletic <dusan.puletic@nih.gov>
2014-10-27 18:34:55 +0000

    chroot scripts updated (webapp added to chroot image)
    Former-commit-id: 4b1872c70fbd61cdedb06dff47140fce7389e07a

--- chroot/create_chroot.sh
+++ chroot/create_chroot.sh
@@ -1,44 +1,65 @@
 #!/bin/bash
 
-if [ $# -eq 4 ]; then
-
- sudo rm -rf "${3}/chroot-root"
-
- mkdir -p "${3}/chroot-root"
-
- mkdir -p "${3}/chroot-root/gadgetron"
-
- mkdir -p "${3}/chroot-backups"
-
- sudo apt-get install debootstrap -y
-
- sudo debootstrap --variant=buildd --arch amd64 trusty "${3}/chroot-root/gadgetron" http://gb.archive.ubuntu.com/ubuntu/
-
- cd ${4}
-
- sudo make install DESTDIR="${3}/chroot-root/gadgetron" -j8
-
- "${2}/generate_gadgetron_root" ${1} "${3}/chroot-root/gadgetron"
-
- cp ${2}/start.sh "${3}/chroot-root"
- cp ${2}/stop.sh "${3}/chroot-root"
- cp ${2}/start-env.sh "${3}/chroot-root" 
- 
- sudo chmod +x ${3}/start-gadgetron.sh
- sudo cp ${3}/start-gadgetron.sh "${3}/chroot-root/gadgetron" 
- 
- sudo chmod +x ${3}/enter-chroot-env.sh
- sudo cp ${3}/enter-chroot-env.sh "${3}/chroot-root/gadgetron"
-
- sudo cp -n ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml
-
- tar -zcf "${3}/chroot-backups/gadgetron-chroot-`date '+%d-%B-%Y'`.tar.gz" --directory "${3}/chroot-root" --exclude=./gadgetron/etc --exclude=./gadgetron/var --exclude=./gadgetron/dev --exclude=./gadgetron/root .
-
- sudo rm -rf "${3}/chroot-root"
- 
- exit 0
+#if [ "$EUID" -ne 0 ]]; then 
+if [ $(id -u) -ne 0 ]; then
+ echo -e "\nPlease start the script as a root or sudo!\n"
+ exit 1
 
 else
- echo -e "\nUsage:  $0 (gadgetron install dir) (chroot source dir) (chroot build dir) (gadgetron build dir)\n"
- exit 1
+ if [ $# -eq 5 ]; then
+
+  rm -rf ${3}/chroot-root
+
+  mkdir -p ${3}/chroot-root
+
+  mkdir -p ${3}/chroot-root/gadgetron
+
+  mkdir -p ${3}/chroot-root/gadgetron/webapp
+
+  mkdir -p ${3}/chroot-backups
+
+  apt-get install debootstrap -y
+
+  debootstrap --variant=buildd --arch amd64 trusty ${3}/chroot-root/gadgetron http://gb.archive.ubuntu.com/ubuntu/
+
+  cd ${4}
+
+  make install DESTDIR="${3}/chroot-root/gadgetron" -j8
+
+  ${2}/generate_gadgetron_root ${1} ${3}/chroot-root/gadgetron
+
+  cp ${2}/start.sh ${3}/chroot-root
+  cp ${2}/stop.sh ${3}/chroot-root
+  cp ${2}/start-env.sh ${3}/chroot-root 
+
+  chmod +x ${3}/start-gadgetron.sh
+  cp ${3}/start-gadgetron.sh ${3}/chroot-root/gadgetron 
+ 
+  chmod +x ${3}/enter-chroot-env.sh
+  cp ${3}/enter-chroot-env.sh ${3}/chroot-root/gadgetron
+
+  cp -n ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml
+
+  #cp ${5}/apps/gadgetron/webapp/* ${3}/chroot-root/gadgetron/webapp
+
+  cp ${3}/gadgetron_web_app.cfg ${3}/chroot-root/gadgetron/webapp
+  cp ${3}/gadgetron_web.conf ${3}/chroot-root/gadgetron/webapp
+  cp ${3}/gadgetron_web_ld.conf ${3}/chroot-root/gadgetron/webapp
+  cp ${5}/apps/gadgetron/webapp/gadgetron_web_app.py ${3}/chroot-root/gadgetron/webapp
+  cp ${5}/apps/gadgetron/webapp/gadgetron_web_app.py ${3}/chroot-root/gadgetron${1}/bin/gadgetron_web_app.py
+  cp ${3}/gadgetron_web_app.cfg ${3}/chroot-root/gadgetron${1}/config/
+
+  chroot ${3}/chroot-root/gadgetron apt-get install python-dev python-twisted python-psutil -y 
+
+  tar -zcf "${3}/chroot-backups/gadgetron-chroot-`date '+%d-%B-%Y'`.tar.gz" --directory "${3}/chroot-root" --exclude=./gadgetron/etc --exclude=./gadgetron/var --exclude=./gadgetron/dev --exclude=./gadgetron/root .
+
+  rm -rf "${3}/chroot-root"
+ 
+  exit 0
+
+ else
+  echo -e "\nUsage:  $0 (gadgetron install dir) (chroot source dir) (chroot build dir) (gadgetron build dir) (gadgetron source dir)\n"
+  exit 1
+ fi
+
 fi
