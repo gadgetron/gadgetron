8f9e32d7cbcbf3462803cc729e9846222e8d510d
Hui Xue <hui.xue@nih.gov>
2014-11-07 10:20:37 -0500

    add initial version of install chroot image script, working on the .tar.gz file
    Former-commit-id: 9f55b49b54d67d0a1a18a96a6ec4edb6be87b3fe

---
+++ chroot/install_chroot_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then 
+ echo -e "\nPlease start the script as a root or sudo!\n"
+ exit 1
+
+else
+ BASEDIR=$(dirname $0)
+
+ if [ $# -eq 2 ]; then
+
+  CHROOT_IMAGE_FILENAME=${1}
+  echo CHROOT_IMAGE_FILENAME=${CHROOT_IMAGE_FILENAME}
+
+  CHROOT_INSTALL_PATH=${2}
+  echo CHROOT_INSTALL_PATH=${CHROOT_INSTALL_PATH}
+
+  mkdir -p ${CHROOT_INSTALL_PATH}
+
+  cp -rf ${CHROOT_IMAGE_FILENAME} ${CHROOT_INSTALL_PATH}/
+
+  FILENAME_WITH_EXTENSION=${CHROOT_IMAGE_FILENAME##*/}
+  FILENAME=${FILENAME_WITH_EXTENSION%.*}
+  FILENAME=${FILENAME%.*}
+  echo ${FILENAME}
+
+  mkdir ${CHROOT_INSTALL_PATH}/${FILENAME}
+
+  echo untar ${CHROOT_INSTALL_PATH}/${FILENAME_WITH_EXTENSION} ... 
+
+  tar -xzf ${CHROOT_INSTALL_PATH}/${FILENAME_WITH_EXTENSION} --directory="${CHROOT_INSTALL_PATH}/${FILENAME}" .
+
+  rm -f ${CHROOT_INSTALL_PATH}/current
+
+  ln -s ${CHROOT_INSTALL_PATH}/${FILENAME} ${CHROOT_INSTALL_PATH}/current
+
+  exit 0
+ 
+ else
+  echo -e "\nUsage: $0 chroot_image_name chroot_install_path\n"
+  exit 1 
+ fi
+fi
