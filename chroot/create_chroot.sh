622c0188d311f57bd58d152cc850f47106569e56
Dusan Puletic <dusan.puletic@nih.gov>
2014-11-09 18:29:11 +0000

    Fixed bug related to libismrmrd not being copied. Decreased num of input vars to create_chroot.sh
    Former-commit-id: f3283774267204a7633fb488638457f769953e45

--- chroot/create_chroot.sh
+++ chroot/create_chroot.sh
@@ -5,79 +5,88 @@ if [ $(id -u) -ne 0 ]; then
  exit 1
 
 else
- if [ $# -eq 6 ]; then
+ if [ $# -eq 5 ]; then
 
-  rm -rf ${3}/chroot-root
+  # Add ISMRMRD_LIB_DIR to LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${5}
 
-  mkdir -p ${3}/chroot-root
+  rm -rf ${2}/chroot/chroot-root
 
-  touch ${3}/chroot-root/source-manifest.txt
+  mkdir -p ${2}/chroot/chroot-root
 
-  echo "gadgetron    ${6}" > ${3}/chroot-root/source-manifest.txt
+  touch ${2}/chroot/chroot-root/source-manifest.txt
 
-  mkdir -p ${3}/chroot-root/gadgetron
+  echo "gadgetron    ${4}" > ${2}/chroot/chroot-root/source-manifest.txt
 
-  mkdir -p ${3}/chroot-root/gadgetron/webapp
+  mkdir -p ${2}/chroot/chroot-root/gadgetron
 
-  mkdir -p ${3}/chroot-backups
+  mkdir -p ${2}/chroot/chroot-root/gadgetron/webapp
+
+  mkdir -p ${2}/chroot/chroot-backups
 
   apt-get install debootstrap -y
 
-  debootstrap --variant=buildd --arch amd64 trusty ${3}/chroot-root/gadgetron http://gb.archive.ubuntu.com/ubuntu/
+  debootstrap --variant=buildd --arch amd64 trusty ${2}/chroot/chroot-root/gadgetron http://gb.archive.ubuntu.com/ubuntu/
 
-  cd ${4}
+  cd ${2}
+  make install DESTDIR="${2}/chroot/chroot-root/gadgetron" -j8
 
-  make install DESTDIR="${3}/chroot-root/gadgetron" -j8
+  ${3}/chroot/generate_gadgetron_root ${1} ${2}/chroot/chroot-root/gadgetron
 
-  ${2}/generate_gadgetron_root ${1} ${3}/chroot-root/gadgetron
+  cp ${3}/chroot/start.sh ${2}/chroot/chroot-root
+  cp ${3}/chroot/stop.sh ${2}/chroot/chroot-root
+  cp ${3}/chroot/start-env.sh ${2}/chroot/chroot-root 
+  cp ${3}/chroot/start-webapp.sh ${2}/chroot/chroot-root 
+  cp ${3}/chroot/mount.sh ${2}/chroot/chroot-root
 
-  cp ${2}/start.sh ${3}/chroot-root
-  cp ${2}/stop.sh ${3}/chroot-root
-  cp ${2}/start-env.sh ${3}/chroot-root 
-  cp ${2}/start-webapp.sh ${3}/chroot-root 
-  cp ${2}/mount.sh ${3}/chroot-root
-
-  chmod +x ${3}/start-gadgetron.sh
-  cp ${3}/start-gadgetron.sh ${3}/chroot-root/gadgetron 
+  chmod +x ${2}/chroot/start-gadgetron.sh
+  cp ${2}/chroot/start-gadgetron.sh ${2}/chroot/chroot-root/gadgetron 
  
-  chmod +x ${3}/enter-chroot-env.sh
-  cp ${3}/enter-chroot-env.sh ${3}/chroot-root/gadgetron
+  chmod +x ${2}/chroot/enter-chroot-env.sh
+  cp ${2}/chroot/enter-chroot-env.sh ${2}/chroot/chroot-root/gadgetron
 
-  chmod +x ${3}/run-webapp.sh
-  cp ${3}/run-webapp.sh ${3}/chroot-root/gadgetron
+  chmod +x ${2}/chroot/run-webapp.sh
+  cp ${2}/chroot/run-webapp.sh ${2}/chroot/chroot-root/gadgetron
 
-  cp -n ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${3}/chroot-root/gadgetron${1}/config/gadgetron.xml
+  cp -n ${2}/chroot/chroot-root/gadgetron${1}/config/gadgetron.xml.example ${2}/chroot/chroot-root/gadgetron${1}/config/gadgetron.xml
 
-  cp ${3}/gadgetron_web_app.cfg ${3}/chroot-root/gadgetron/webapp
-  cp ${3}/gadgetron_web.conf ${3}/chroot-root/gadgetron/webapp
-  cp ${3}/gadgetron_web_ld.conf ${3}/chroot-root/gadgetron/webapp
-  cp ${5}/apps/gadgetron/webapp/gadgetron_web_app.py ${3}/chroot-root/gadgetron/webapp
-  cp ${5}/apps/gadgetron/webapp/gadgetron_web_app.py ${3}/chroot-root/gadgetron${1}/bin/gadgetron_web_app.py
-  cp ${3}/gadgetron_web_app.cfg ${3}/chroot-root/gadgetron${1}/config/
+  cp ${2}/chroot/gadgetron_web_app.cfg ${2}/chroot/chroot-root/gadgetron/webapp
+  cp ${2}/chroot/gadgetron_web.conf ${2}/chroot/chroot-root/gadgetron/webapp
+  cp ${2}/chroot/gadgetron_web_ld.conf ${2}/chroot/chroot-root/gadgetron/webapp
+  cp ${3}/apps/gadgetron/webapp/gadgetron_web_app.py ${2}/chroot/chroot-root/gadgetron/webapp
+  cp ${3}/apps/gadgetron/webapp/gadgetron_web_app.py ${2}/chroot/chroot-root/gadgetron${1}/bin/gadgetron_web_app.py
+  cp ${2}/chroot/gadgetron_web_app.cfg ${2}/chroot/chroot-root/gadgetron${1}/config/
 
-  chroot ${3}/chroot-root/gadgetron apt-get install python-dev python-twisted python-psutil -y 
+  chroot ${2}/chroot/chroot-root/gadgetron apt-get install python-dev python-twisted python-psutil -y 
   
-  TAR_FILE_NAME=gadgetron-`date '+%Y%m%d'`-${6:0:8}
-  IMAGE_FILE_NAME=${3}/chroot-backups/${TAR_FILE_NAME}.img
+  TAR_FILE_NAME=gadgetron-`date '+%Y%m%d'`-${4:0:8}
+  IMAGE_FILE_NAME=${2}/chroot/chroot-backups/${TAR_FILE_NAME}.img
 
-  tar -zcf "${3}/chroot-backups/${TAR_FILE_NAME}.tar.gz" --directory "${3}" --exclude=./chroot-root/gadgetron/etc --exclude=./chroot-root/gadgetron/var --exclude=./chroot-root/gadgetron/dev --exclude=./chrot-root/gadgetron/root ./chroot-root
+  tar -zcf "${2}/chroot/chroot-backups/${TAR_FILE_NAME}.tar.gz" --directory "${2}/chroot" --exclude=./chroot-root/gadgetron/etc --exclude=./chroot-root/gadgetron/var --exclude=./chroot-root/gadgetron/dev --exclude=./chrot-root/gadgetron/root ./chroot-root
 
   dd if=/dev/zero of=${IMAGE_FILE_NAME} bs=1024k seek=1024 count=0
   mke2fs -F -t ext3 ${IMAGE_FILE_NAME}
-  mkdir ${3}/gadgetron_root
-  mount -o loop ${IMAGE_FILE_NAME} ${3}/gadgetron_root
-  tar -xzf ${3}/chroot-backups/${TAR_FILE_NAME}.tar.gz -C ${3}/gadgetron_root/
+  mkdir ${2}/chroot/gadgetron_root
+  mount -o loop ${IMAGE_FILE_NAME} ${2}/chroot/gadgetron_root
+  tar -xzf ${2}/chroot/chroot-backups/${TAR_FILE_NAME}.tar.gz -C ${2}/chroot/gadgetron_root/
   sleep 3
-  umount ${3}/gadgetron_root
-  rmdir ${3}/gadgetron_root
+  umount ${2}/chroot/gadgetron_root
+  rmdir ${2}/chroot/gadgetron_root
 
-  rm -rf "${3}/chroot-root"
+  rm -rf "${2}/chroot/chroot-root"
  
   exit 0
 
  else
-  echo -e "\nUsage:  $0 (gadgetron install dir) (chroot source dir) (chroot build dir) (gadgetron build dir) (gadgetron source dir)\n"
+  echo -e "\nUsage:  $0 (gadgetron install prefix) (gadgetron binary dir) (gadgetron source dir) (GADGETRON_GIT_SHA1_HASH) (ISMRMRD_LIB_DIR)\n"
   exit 1
  fi
 
 fi
+
+# --ARGUMENTS--
+# 1. cmake install prefix:     /usr/local/gadgetron
+# 2. cmake binary dir:         /home/ubuntu/gadgetron/build
+# 3. cmake source dir:         /home/ubuntu/gadgetron
+# 4. HASH
+# 5. ISMRMRD_LIB_DIR           /usr/local
