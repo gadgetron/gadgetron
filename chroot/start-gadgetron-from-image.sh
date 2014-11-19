aeb0264bddc6ff32b873f2e11e751982db33744b
Dusan Puletic <dusan.puletic@nih.gov>
2014-11-19 20:28:44 +0000

    chroot: bug from previous commit fixed

--- chroot/start-gadgetron-from-image.sh
+++ chroot/start-gadgetron-from-image.sh
@@ -3,7 +3,7 @@
 start_gadgetron_image_job=0
 BASEDIR=$(dirname $0)
 
-trap '(($start_gadgetron_image_job == 0)) || ((`kill -0 $start_gadgetron_image_job`)) || kill $start_gadgetron_image_job & while kill -0 $start_gadgetron_image_job 2>/dev/null; do sleep 1; done & $BASEDIR/umount_image.sh' HUP TERM INT
+trap '(($start_gadgetron_image_job == 0)) || ((`kill -0 $start_gadgetron_image_job`)) || kill $start_gadgetron_image_job & while kill -0 $start_gadgetron_image_job 2>/dev/null; do sleep 1; done' HUP TERM INT
 
 if [ $(id -u) -ne 0 ]; then
   echo -e "\nPlease start the script as a root or sudo!\n"
@@ -16,7 +16,7 @@ else
     start_gadgetron_image_job=($!)
     wait $!
     sleep 1
-    $BASEDIR/umount_image.sh
+    $BASEDIR/umount_image.sh ${2}
     exit 0
   else
     echo -e "\nUsage: $0 <full path to img file> <mount point>\n"
--- chroot/umount_image.sh
+++ chroot/umount_image.sh
@@ -4,22 +4,22 @@ if [ $(id -u) -ne 0 ]; then
   echo -e "\nPlease start the script as a root or sudo!\n"
   exit 1
 else
-  if [ $# -eq 0 ]; then
-    if mountpoint -q /mnt/chroot; then
-      if mountpoint -q /mnt/chroot/chroot-root/gadgetron/proc; then
-        umount /mnt/chroot/chroot-root/gadgetron/proc
+  if [ $# -eq 1 ]; then
+    if mountpoint -q ${1}; then
+      if mountpoint -q ${1}/chroot-root/gadgetron/proc; then
+        umount ${1}/chroot-root/gadgetron/proc
       fi
-      if mountpoint -q /mnt/chroot/chroot-root/gadgetron/dev; then
-        umount /mnt/chroot/chroot-root/gadgetron/dev
+      if mountpoint -q ${1}/chroot-root/gadgetron/dev; then
+        umount ${1}/chroot-root/gadgetron/dev
       fi
-      if mountpoint -q /mnt/chroot/chroot-root/gadgetron/sys; then
-        umount /mnt/chroot/chroot-root/gadgetron/sys
+      if mountpoint -q ${1}/chroot-root/gadgetron/sys; then
+        umount ${1}/chroot-root/gadgetron/sys
       fi
-      umount /mnt/chroot
+      umount ${1}
       exit 0
     fi
   else
-    echo -e "\nUsage: $0\n"
+    echo -e "\nUsage: $0 <mount point>\n"
     exit 1
   fi
 fi
