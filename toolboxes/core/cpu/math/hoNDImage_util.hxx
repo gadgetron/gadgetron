530951897629b71a22b990da9f6822af3f34ff28
Hui Xue <hui.xue@nih.gov>
2014-12-18 21:57:45 -0500

    fix a bug in the hoNDImage_util filter1D function

--- toolboxes/core/cpu/math/hoNDImage_util.hxx
+++ toolboxes/core/cpu/math/hoNDImage_util.hxx
@@ -947,6 +947,8 @@ namespace Gadgetron
             typedef typename ArrayType::value_type T;
             typedef typename realType<T>::Type real_value_type;
 
+            typedef hoNDImage<T, 1> Array1DType;
+
             long long RO = (long long)img.get_size(0);
             long long num = (long long)(img.get_number_of_elements()/RO);
 
@@ -961,12 +963,12 @@ namespace Gadgetron
 
             #pragma omp parallel default(none) private(ii) shared(bh, num, RO, img, img_out, kerLen, kerHalfLen, pKer)
             {
-                hoNDBoundaryHandler<ArrayType>* pBH = NULL;
+                hoNDBoundaryHandler<Array1DType>* pBH = NULL;
 
-                hoNDBoundaryHandlerFixedValue<ArrayType> bhFixedValue;
-                hoNDBoundaryHandlerBorderValue<ArrayType> bhBorderValue;
-                hoNDBoundaryHandlerPeriodic<ArrayType> bhPeriodic;
-                hoNDBoundaryHandlerMirror<ArrayType> bhMirror;
+                hoNDBoundaryHandlerFixedValue<Array1DType> bhFixedValue;
+                hoNDBoundaryHandlerBorderValue<Array1DType> bhBorderValue;
+                hoNDBoundaryHandlerPeriodic<Array1DType> bhPeriodic;
+                hoNDBoundaryHandlerMirror<Array1DType> bhMirror;
 
                 pBH = &bhBorderValue;
 
@@ -990,10 +992,10 @@ namespace Gadgetron
                 #pragma omp for 
                 for ( ii=0; ii<num; ii++ )
                 {
-                    ArrayType img1D(RO, const_cast<T*>(img.begin()+ii*RO));
+                    Array1DType img1D(RO, const_cast<T*>(img.begin()+ii*RO));
                     pBH->setArray(img1D);
 
-                    ArrayType img_out1D(RO, img_out.begin()+ii*RO);
+                    Array1DType img_out1D(RO, img_out.begin()+ii*RO);
 
                     long long k, j;
                     for ( k=0; k<RO; k++ )
