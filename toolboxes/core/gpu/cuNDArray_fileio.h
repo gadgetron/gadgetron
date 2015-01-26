929842e77d53445c7e38901ab82d68ac16932735
David C Hansen <dch@cs.au.dk>
2015-01-26 08:19:24 +0100

    Fixed missing inclue in cuNDArray_fileio. Fixed a bug in the abs function of vector_t_utilities. Changed smart_ptr include to shared_ptr

--- toolboxes/core/gpu/cuNDArray_fileio.h
+++ toolboxes/core/gpu/cuNDArray_fileio.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "hoNDArray_fileio.h"
+
 namespace Gadgetron{
 template<class T> void write_nd_array(cuNDArray<T>* array, std::string s){
 	write_nd_array(array->to_host().get(),s.c_str());
