ce9e9e8f6a5ac97657ba6651d9f907c66f60909c
Hui Xue <hui.xue@nih.gov>
2015-04-13 15:00:19 -0400

    using hesv for rt grappa gpu matrix inversion

--- toolboxes/mri/pmri/gpu/htgrappa.cpp
+++ toolboxes/mri/pmri/gpu/htgrappa.cpp
@@ -10,12 +10,12 @@
 
 
 /*
-  This file is used to hide certain Armadillo calls from the nvcc compiler. If Armadillo functions need to 
-  be called in a *.cu file, it is preferably to wrap the calls in a function and place that function in 
+  This file is used to hide certain Armadillo calls from the nvcc compiler. If Armadillo functions need to
+  be called in a *.cu file, it is preferably to wrap the calls in a function and place that function in
   a *.cpp file so that Armadillo code will not be compiled by nvcc.
 
   Some error handling may be needed in these functions, but eventually SymmetricHermitianPositiveDefiniteLinearSystem_posv
-  will be renamed and made to throw exceptions and then it should be handled. 
+  will be renamed and made to throw exceptions and then it should be handled.
 
  */
 
@@ -25,17 +25,19 @@ namespace Gadgetron
 {
   template <class T> void ht_grappa_solve_spd_system(hoNDArray<T> *A, hoNDArray<T> *B) {
     /*
-      We are swithcing off OpenMP threading before this call to posv. There seems to be a bad interaction between openmp, cuda, and BLAS. 
-      So far this problem has only been observed from *.cu files (or in functions called from *.cu files) but the problem may be more general. 
+      We are swithcing off OpenMP threading before this call to posv. There seems to be a bad interaction between openmp, cuda, and BLAS.
+      So far this problem has only been observed from *.cu files (or in functions called from *.cu files) but the problem may be more general.
 
-      This is a temporary fix that we should keep an eye on. 
+      This is a temporary fix that we should keep an eye on.
      */
 #ifdef USE_OMP
     int num_threads = omp_get_num_threads();
     omp_set_num_threads(1);
 #endif //USE_OMP
 
-    posv(*A, *B);
+    // it is found that if signal is very very high, the posv can throw exceptions due to ill-conditioned matrix of A
+    // hesv does not require A to be a positive-definite matrix, but an n-by-n symmetric matrix
+    hesv(*A, *B);
 
 #ifdef USE_OMP
     omp_set_num_threads(num_threads);
@@ -44,5 +46,5 @@ namespace Gadgetron
   }
 
   template void ht_grappa_solve_spd_system< float_complext >(hoNDArray< float_complext > *A, hoNDArray< float_complext > *B);
-  
+
 }
