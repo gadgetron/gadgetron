5e0a23cebc472817691c73ffeec8faefd91a0c55
Hui Xue <hui.xue@nih.gov>
2015-07-20 22:30:57 -0400

    remove some functions from mri_core KL transform; only the basic functions are kept

--- toolboxes/mri_core/mri_core_KarhunenLoeve_Transform.h
+++ toolboxes/mri_core/mri_core_KarhunenLoeve_Transform.h
@@ -15,9 +15,9 @@ namespace Gadgetron
     /// KL transform
     /// ------------------------------------------------------------------------
     /// data: M*N data array
-    /// the KLT direction is along the N
+    /// the KLT direction is along the second dimension
     /// eigenVectors: N*N eigen vectors, every column is a eigen vector
-    /// eigenValues: N*1 eigen values, descending order
+    /// eigenValues: N*1 eigen values, ascending order
     template <typename T> EXPORTMRICORE void eigen_analysis(const hoNDArray<T>& data, hoNDArray<T>& eigenVectors, hoNDArray<T>& eigenValues);
 
     /// apply the eigen transform
@@ -26,49 +26,9 @@ namespace Gadgetron
     /// dataEigen: M*K eigen data matrix
     template <typename T> EXPORTMRICORE void apply_eigen_vector(const hoNDArray<T>& data, const hoNDArray<T>& eigenVectors, hoNDArray<T>& dataEigen);
 
-    /// number of kept eigen modes
-    /// all modes with eigen values greater than thres*max(eigenValues) are kept
-    template <typename T> EXPORTMRICORE void compute_number_of_eigen_modes_kept(const hoNDArray<T>& eigenValues, double thres, size_t& numOfModesKept);
-
-    /// prune the eigen vector matrixes to keep the last numOfModesKept columns
-    template <typename T> EXPORTMRICORE void prune_eigen_vector_matrix(const hoNDArray<T>& eigenVectors, size_t numOfModesKept, hoNDArray<T>& eigenVectorsPruned);
-
-    /// ------------------------------------------------------------------------
-    /// KL transform coil compression
-    /// ------------------------------------------------------------------------
     /// data: the first dimsion is dimension 0
     /// the KL transform is applied along the KLTDim dimension
     /// coeff: square eigen vector matrix
     /// KLTDim : the dimension to perform KL transform
     template <typename T> EXPORTMRICORE void compute_KLT_coeff(const hoNDArray<T>& data, hoNDArray<T>& coeff, hoNDArray<T>& eigenValues, size_t KLTDim);
-
-    /// coeff: CHA*numOfModesKept eigen vector matrix
-    /// eigenValues: CHA*1 eigen values
-    /// thres <0 or numOfModesKept==-1, keep all modes
-    /// if isChaLastDim==true, the CHA is the last dimension
-    /// data: [RO E1 CHA ...]
-    template <typename T> EXPORTMRICORE void compute_KLT_coil_compression_coeff_2D(const hoNDArray<T>& data, double thres, hoNDArray<T>& coeff, hoNDArray<T>& eigenValues);
-    template <typename T> EXPORTMRICORE void compute_KLT_coil_compression_coeff_2D(const hoNDArray<T>& data, size_t numOfModesKept, hoNDArray<T>& coeff, hoNDArray<T>& eigenValues);
-
-    /// data: [RO E1 E2 CHA ...]
-    template <typename T> EXPORTMRICORE void compute_KLT_coil_compression_coeff_3D(const hoNDArray<T>& data, double thres, hoNDArray<T>& coeff, hoNDArray<T>& eigenValues);
-    template <typename T> EXPORTMRICORE void compute_KLT_coil_compression_coeff_3D(const hoNDArray<T>& data, size_t numOfModesKept, hoNDArray<T>& coeff, hoNDArray<T>& eigenValues);
-
-    /// apply coil compression coefficients
-    /// data : [RO E1 srcCHA ...] for 2D or [RO E1 E2 srcCHA ...] for 3D
-    /// coeff: [srcCHA dstCHA]
-    /// dataEigen: [RO E1 dstCHA ...] or [RO E1 E2 dstCHA ...]
-    template <typename T> EXPORTMRICORE void appy_KLT_coil_compression_coeff_2D(const hoNDArray<T>& data, const hoNDArray<T>& coeff, hoNDArray<T>& dataEigen);
-    template <typename T> EXPORTMRICORE void appy_KLT_coil_compression_coeff_3D(const hoNDArray<T>& data, const hoNDArray<T>& coeff, hoNDArray<T>& dataEigen);
-
-    /// apply coil compresion coefficients 
-    /// data: [RO E1 srcCHA ... N] for 2D or [RO E1 E2 srcCHA ... N] for 3D
-    /// coeff has N elements; everyone is [srcCHA dstCHA] array
-    /// for every subarray in data, coeff[n] will be applied
-    template <typename T> EXPORTMRICORE void appy_KLT_coil_compression_coeff_2D(const hoNDArray<T>& data, const std::vector< hoNDArray<T> >& coeff, hoNDArray<T>& dataEigen);
-    template <typename T> EXPORTMRICORE void appy_KLT_coil_compression_coeff_3D(const hoNDArray<T>& data, const std::vector< hoNDArray<T> >& coeff, hoNDArray<T>& dataEigen);
-
-    /// compute KL transform and perform filtering
-    /// the KLT dimension is the last dimension
-    template <typename T> EXPORTMRICORE void compute_KLT_filter(const hoNDArray<T>& data, size_t numOfModesKept, hoNDArray<T>& dataKLF);
 }
