ab7101cb3cdb6c292dcbb52b38057f7c0b23fc6c
Hui Xue <hui.xue@nih.gov>
2015-09-15 15:16:24 -0400

    add export to PseudoReplicatorGadget

--- gadgets/mri_core/PseudoReplicatorGadget.h
+++ gadgets/mri_core/PseudoReplicatorGadget.h
@@ -8,9 +8,11 @@
 #pragma once
 #include "Gadget.h"
 #include "mri_core_data.h"
+#include "gadgetron_mricore_export.h"
+
 namespace Gadgetron {
 
-class PseudoReplicatorGadget : public Gadget1<IsmrmrdReconData>{
+class EXPORTGADGETSMRICORE PseudoReplicatorGadget : public Gadget1<IsmrmrdReconData>{
 public:
 	GADGET_PROPERTY(repetitions,int,"Number of pseudoreplicas to produce",10);
 	PseudoReplicatorGadget()  ;
