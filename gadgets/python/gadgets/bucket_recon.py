6679d98dc1b5f8fe6f8da16a694c7765a3e730c1
David C Hansen <dae.hansen@gmail.com>
2015-12-08 08:25:28 +0100

    Fixed a bug where bucket_recon was not placing the coils in the last dimension of the image

--- gadgets/python/gadgets/bucket_recon.py
+++ gadgets/python/gadgets/bucket_recon.py
@@ -21,6 +21,7 @@ class BucketRecon(Gadget):
 
         print np.shape(recondata[0].data.data)
         image = transform.transform_kspace_to_image(recondata[0].data.data,dim=(0,1,2))
+	image = np.reshape(image,(image.shape[0],image.shape[1],image.shape[2],image.shape[3]))
         #Create a new image header and transfer value
         acq = np.ravel(recondata[0].data.headers)[0]
         img_head = ismrmrd.ImageHeader()
