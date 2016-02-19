0d1086f6ab7740eb839e3d2a91c024564515518f
Michael S. Hansen <michael.hansen@nih.gov>
2016-02-18 20:20:52 -0500

    Added further garbage collection to docker scripts

--- docker/remove_all_but_latest.sh
+++ docker/remove_all_but_latest.sh
@@ -2,4 +2,8 @@
 
 image_name=$1
 
+#Remove images not tagged with latest
 docker rmi  $(docker images ${image_name} |grep -v "TAG"|grep -v "latest"|awk -v img=$image_name '{print img":"$2}')
+
+#Remove any "orphaned" images
+docker rmi  $(docker images |grep "<none>"|awk  '{print $3}'|uniq)
