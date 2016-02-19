6b897e88e66186981ede1fffea8b0f319cdd279c
Michael S. Hansen <michael.hansen@nih.gov>
2016-02-18 21:01:40 -0500

    Fixed small bug in docker garbage collection

--- docker/remove_all_but_latest.sh
+++ docker/remove_all_but_latest.sh
@@ -3,7 +3,14 @@
 image_name=$1
 
 #Remove images not tagged with latest
-docker rmi  $(docker images ${image_name} |grep -v "TAG"|grep -v "latest"|awk -v img=$image_name '{print img":"$2}')
+rem_im=$(docker images ${image_name} |grep -v "TAG"|grep -v "latest"|awk -v img=$image_name '{print img":"$2}')
+if [ -n "$rem_im" ]; then
+    docker rmi  $rem_im
+fi
 
 #Remove any "orphaned" images
-docker rmi  $(docker images |grep "<none>"|awk  '{print $3}'|uniq)
+rem_im=$(docker images |grep "<none>"|awk  '{print $3}'|uniq)
+if [ -n "$rem_im" ]; then
+    docker rmi  $rem_im
+fi
+
