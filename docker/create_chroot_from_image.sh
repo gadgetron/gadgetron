de4f61af7db1534199f454c418a54c602d60b215
Michael S. Hansen <michael.hansen@nih.gov>
2016-02-20 08:20:35 -0500

    Fixed script to create chroot from image so that it works on gtprep images too

--- docker/create_chroot_from_image.sh
+++ docker/create_chroot_from_image.sh
@@ -4,7 +4,7 @@ image=$1
 created=$(docker inspect --format='{{.Created}}' $image)
 timestamp="${created:0:4}${created:5:2}${created:8:2}-${created:11:2}${created:14:2}"
 gadgetron_hash=$(docker run --rm $image cut -c-8 /opt/code/gadgetron_sha1.txt)
-gtprep_hash=$(docker run --rm $image [ -f /opt/code/gtprep_sha1.txt ] && $(cut -c-8 /opt/code/gtprep_sha1.txt))
+gtprep_hash=$(docker run --rm $image bash -c  "[ -f /opt/code/gtprep_sha1.txt ] && cut -c-8 /opt/code/gtprep_sha1.txt")
 chroot_name="gadgetron-${timestamp}-$gadgetron_hash"
 if [ -n "$gtprep_hash" ]; then
     chroot_name="${chroot_name}-gtprep-${gtprep_hash}"
