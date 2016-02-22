0e7969b374ec86e0a30e7df5af150f02215dbedc
Hui Xue <hui.xue@nih.gov>
2016-02-22 14:12:22 -0500

    add optional gtprep sha1 for tag docker images

--- docker/tag_image_with_version.sh
+++ docker/tag_image_with_version.sh
@@ -3,4 +3,9 @@
 image_name=$1
 gadgetron_version=$(docker run --rm $image_name gadgetron_info | awk '/-- Version/ {print $4}')
 gadgetron_sha1=$(docker run --rm $image_name cut -c-8 /opt/code/gadgetron_sha1.txt)
-docker tag ${image_name} ${image_name}:${gadgetron_version}-${gadgetron_sha1}
+gtprep_sha1=$(docker run --rm $image_name bash -c  "[ -f /opt/code/gtprep_sha1.txt ] && cut -c-8 /opt/code/gtprep_sha1.txt")
+tag_value="${gadgetron_version}-${gadgetron_sha1}"
+if [ -n "$gtprep_sha1" ]; then
+    tag_value="${tag_value}-${gtprep_sha1}"
+fi
+docker tag ${image_name} ${image_name}:${tag_value}
