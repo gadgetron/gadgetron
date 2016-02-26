c3197bed424df975387ae3b394ec0839702e7687
Michael S. Hansen <michael.hansen@nih.gov>
2016-02-26 14:07:34 -0500

    Fixed small bug in create_chroot script

--- docker/create_chroot_from_image.sh
+++ docker/create_chroot_from_image.sh
@@ -17,7 +17,7 @@ chroot_base_name=$chroot_name
 chroot_name="${chroot_name}.tar.gz"
 echo "Creating chroot image with name: $chroot_name"
 
-echo "docker run -i --rm --volume=`pwd`:/opt/backup $image tar -cvpzf /opt/backup/$chroot_name --exclude=/opt/code --exclude=/opt/backup --one-file-system /"
+docker run -i --rm --volume=`pwd`:/opt/backup $image tar -cvpzf /opt/backup/$chroot_name --exclude=/opt/code --exclude=/opt/backup --one-file-system /
 
 if [ $# -gt 1 ]; then
     sleep 3
