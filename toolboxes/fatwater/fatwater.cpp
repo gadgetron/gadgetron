d1e63a9e27479e61c69d491d8fc56fdbb80a11ef
Diego Hernando <dhernando@wisc.edu>
2016-04-20 02:06:17 +0000

    Starting to add some graph stuff (still not nearly functional)

--- toolboxes/fatwater/fatwater.cpp
+++ toolboxes/fatwater/fatwater.cpp
@@ -7,7 +7,11 @@
 #include "hoNDArray_reductions.h"
 #include "hoArmadillo.h"
 
-#include "boost/graph/push_relabel_max_flow.hpp"
+#include <boost/config.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/read_dimacs.hpp>
+#include <boost/graph/graph_utility.hpp>
 
 
 #define GAMMABAR 42.576 // MHz/T
@@ -291,6 +295,64 @@ namespace Gadgetron {
 	}
 	
 	
+	
+	//arma::Mat< std::complex<float> > arma_phiMatrix = as_arma_matrix( phiMatrix ); 
+
+	
+	//Do graph cut iterations
+	using namespace boost;
+
+	typedef adjacency_list_traits<vecS, vecS, directedS> Traits;
+	typedef adjacency_list<listS, vecS, directedS, 
+			       property<vertex_name_t, std::string>,
+			       property<edge_capacity_t, long,
+					property<edge_residual_capacity_t, long,
+						 property<edge_reverse_t, Traits::edge_descriptor> > >
+			       > Graph;
+
+	Graph g;
+	
+
+	/*
+	property_map<Graph, edge_capacity_t>::type 
+	  capacity = get(edge_capacity, g);
+	property_map<Graph, edge_reverse_t>::type 
+	  rev = get(edge_reverse, g);
+	property_map<Graph, edge_residual_capacity_t>::type 
+	  residual_capacity = get(edge_residual_capacity, g);
+
+	Traits::vertex_descriptor s, t;
+	read_dimacs_max_flow(g, capacity, rev, s, t);
+
+	long flow;
+#if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
+	// Use non-named parameter version
+	property_map<Graph, vertex_index_t>::type 
+	  indexmap = get(vertex_index, g);
+	flow = push_relabel_max_flow(g, s, t, capacity, residual_capacity, rev, indexmap);
+#else
+	flow = push_relabel_max_flow(g, s, t);
+#endif
+
+	std::cout << "c  The total flow:" << std::endl;
+	std::cout << "s " << flow << std::endl << std::endl;
+
+	std::cout << "c flow values:" << std::endl;
+	graph_traits<Graph>::vertex_iterator u_iter, u_end;
+	graph_traits<Graph>::out_edge_iterator ei, e_end;
+	for (boost::tie(u_iter, u_end) = vertices(g); u_iter != u_end; ++u_iter)
+	  for (boost::tie(ei, e_end) = out_edges(*u_iter, g); ei != e_end; ++ei)
+	    if (capacity[*ei] > 0)
+	      std::cout << "f " << *u_iter << " " << target(*ei, g) << " " 
+			<< (capacity[*ei] - residual_capacity[*ei]) << std::endl;
+  
+	*/
+
+
+	
+
+	
+	//Do final calculations once the field map is done
 	hoMatrix< std::complex<float> > curWaterFat(2,N);
 	hoMatrix< std::complex<float> > AhA(2,2);
 	// Do fat-water separation with current field map and R2* estimates
@@ -333,14 +395,6 @@ namespace Gadgetron {
 	  }
 	}
 
-	
-	//arma::Mat< std::complex<float> > arma_phiMatrix = as_arma_matrix( phiMatrix ); 
-
-	
-	//Do graph cut iterations
-
-	
-	//Do final calculations once the field map is done
 	
 	
 	//Clean up as needed
