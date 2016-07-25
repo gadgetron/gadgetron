21d1b22671c6c2faa26628d2dbf464236da315de
Michael S. Hansen <michael.hansen@nih.gov>
2016-07-25 16:23:54 -0400

    Added Jupyter to docker images and fixed problems with building 14.04 base images (Python problems). Using pip to install python packages on all images to get the latest versions

---
+++ docker/build_base_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+base_dir=$(pwd)
+
+cd ${base_dir}/base/ubuntu_1404
+docker build -t gadgetron/ubuntu1404_base .
+
+cd ${base_dir}/base/ubuntu_1404_cuda55
+docker build -t gadgetron/ubuntu_1404_cuda55_base .
+
+cd ${base_dir}/base/ubuntu_1404_cuda75
+docker build -t gadgetron/ubuntu_1404_cuda75_base .
+
+cd ${base_dir}/base/ubuntu_1604
+docker build -t gadgetron/ubuntu1604_base .
+
+cd ${base_dir}/base/ubuntu_1604_cuda55
+docker build -t gadgetron/ubuntu_1604_cuda55_base .
+
+cd ${base_dir}/base/ubuntu_1604_cuda75
+docker build -t gadgetron/ubuntu_1604_cuda75_base .
+
