fb6da6488551a9d2d92414adc98a6d39500c6b60
Michael S. Hansen <michael.hansen@nih.gov>
2016-07-28 13:49:00 -0400

    Fixed type in scripts to generate base Docker images

--- docker/build_base_images.sh
+++ docker/build_base_images.sh
@@ -6,17 +6,17 @@ cd ${base_dir}/base/ubuntu_1404
 docker build -t gadgetron/ubuntu1404_base .
 
 cd ${base_dir}/base/ubuntu_1404_cuda55
-docker build -t gadgetron/ubuntu_1404_cuda55_base .
+docker build -t gadgetron/ubuntu1404_cuda55_base .
 
 cd ${base_dir}/base/ubuntu_1404_cuda75
-docker build -t gadgetron/ubuntu_1404_cuda75_base .
+docker build -t gadgetron/ubuntu1404_cuda75_base .
 
 cd ${base_dir}/base/ubuntu_1604
 docker build -t gadgetron/ubuntu1604_base .
 
 cd ${base_dir}/base/ubuntu_1604_cuda55
-docker build -t gadgetron/ubuntu_1604_cuda55_base .
+docker build -t gadgetron/ubuntu1604_cuda55_base .
 
 cd ${base_dir}/base/ubuntu_1604_cuda75
-docker build -t gadgetron/ubuntu_1604_cuda75_base .
+docker build -t gadgetron/ubuntu1604_cuda75_base .
 
