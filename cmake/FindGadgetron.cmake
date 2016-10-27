1f48572a2ce9448988c325b8339aba765fa82003
Hui Xue <hui.xue@nih.gov>
2016-10-27 16:02:10 -0400

    add finding gadgetron version in cmake/FindGadgetron.cmake

--- cmake/FindGadgetron.cmake
+++ cmake/FindGadgetron.cmake
@@ -6,7 +6,8 @@
 # GADGETRON_INCLUDE_DIR, where to finds Gadget.h
 # GADGETRON_HOME, Gadgetron Root Dir
 # GADGETRON_LIB_DIR, This is where all the installed gadgetron libraries live
-# GADGETRON_FOUND, if false, you cannot build anything that requires ACE
+# GADGETRON_FOUND, if false, you cannot build anything that requires Gadgetron
+# GADGETRON_VERSION_STRING, if Gadgetron is found, this verison string will be set
 
 # Keep a list of variable names that we need to pass on to
 # find_package_handle_standard_args().
@@ -38,3 +39,33 @@ find_package(CUDA)
 if (CUDA_FOUND)
   include_directories( ${CUDA_INCLUDE_DIRS} )
 endif ()
+
+# ------------------------------------------------------------------------
+#  Extract version information from "incluce/gadgetron/gadgetron_config.h"
+# ------------------------------------------------------------------------
+if(GADGETRON_FOUND)
+    set(GADGETRON_VERSION_MAJOR 0)
+    set(GADGETRON_VERSION_MINOR 0)
+    set(GADGETRON_VERSION_PATCH 0) 
+
+    if(EXISTS "${GADGETRON_INCLUDE_DIR}/gadgetron_config.h")
+
+        # Read and parse header file for version number
+        file(STRINGS "${GADGETRON_INCLUDE_DIR}/gadgetron_config.h" _gadgetron_HEADER_CONTENTS REGEX "#define GADGETRON_VERSION_MAJOR ")
+        string(REGEX REPLACE ".*#define GADGETRON_VERSION_MAJOR ([0-9]+).*" "\\1" GADGETRON_VERSION_MAJOR "${_gadgetron_HEADER_CONTENTS}")
+        unset(_gadgetron_HEADER_CONTENTS)
+        
+        file(STRINGS "${GADGETRON_INCLUDE_DIR}/gadgetron_config.h" _gadgetron_HEADER_CONTENTS REGEX "#define GADGETRON_VERSION_MINOR ")
+        string(REGEX REPLACE ".*#define GADGETRON_VERSION_MINOR ([0-9]+).*" "\\1" GADGETRON_VERSION_MINOR "${_gadgetron_HEADER_CONTENTS}")
+        unset(_gadgetron_HEADER_CONTENTS)
+        
+        file(STRINGS "${GADGETRON_INCLUDE_DIR}/gadgetron_config.h" _gadgetron_HEADER_CONTENTS REGEX "#define GADGETRON_VERSION_PATCH ")
+        string(REGEX REPLACE ".*#define GADGETRON_VERSION_PATCH ([0-9]+).*" "\\1" GADGETRON_VERSION_PATCH "${_gadgetron_HEADER_CONTENTS}")
+        unset(_gadgetron_HEADER_CONTENTS)
+        
+    endif (EXISTS "${GADGETRON_INCLUDE_DIR}/gadgetron_config.h")
+
+    set(GADGETRON_VERSION_STRING ${GADGETRON_VERSION_MAJOR}.${GADGETRON_VERSION_MINOR}.${GADGETRON_VERSION_PATCH})
+    set(GADGETRON_SOVERSION ${GADGETRON_VERSION_MAJOR}.${GADGETRON_VERSION_MINOR})
+    message("find Gadgetron version : ${GADGETRON_VERSION_STRING}")
+endif()
