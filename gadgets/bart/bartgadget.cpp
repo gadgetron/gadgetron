f47746dd4f45bece8f3b81b42571d2cbd94302cc
Hui Xue <hui.xue@nih.gov>
2017-09-23 17:56:13 -0400

    fix an extra string()

--- gadgets/bart/bartgadget.cpp
+++ gadgets/bart/bartgadget.cpp
@@ -132,7 +132,7 @@ namespace Gadgetron {
 #else
 		// in case an older version of boost is used in non-win system
 		// the system call is used
-		int res = chmod(CommandScript.string().c_str(), S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+		int res = chmod(CommandScript.c_str(), S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
 		if (res != 0)
 		{
 			GERROR("Error changing the permission of the command script.\n");
