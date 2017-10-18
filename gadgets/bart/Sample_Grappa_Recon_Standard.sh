3e580cfba4bd2ea9f7010f9445dd6722f0c610d6
Diakite <diakitem2@hongkong.nhlbi.nih.gov>
2017-10-18 16:55:36 -0400

    Standard script for Cine recon

---
+++ gadgets/bart/Sample_Grappa_Recon_Standard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+debug=false
+
+if "$debug"; then
+	set -x
+fi
+
+bart cc -S reference_data cc_mat
+# Compress coils to 12 virtual channels
+bart extract 4 0 11 cc_mat cc_mat_P
+
+bart fmac -C -s 8 reference_data cc_mat_P reference_data_cc
+bart transpose 3 4 reference_data_cc cc_reference_data
+
+bart fmac -C -s 8 input_data cc_mat_P input_data_cc
+bart transpose 3 4 input_data_cc cc_input_data
+
+bart ecalib -c0.65 -k6 -r24 -m2 -S cc_reference_data maps
+bart rsense -l1 -r0.001 -i50 cc_input_data maps ims
+
+if "$debug";then
+	set +x
+fi
