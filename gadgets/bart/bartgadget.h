d4f2f0f0a392f75d969cb08815f918407344d545
Fabian Diener <noreply>
2018-02-22 16:45:20 +0100

    Made line trimmed from both sides.
    Trimming from the right fixes problems with line ending.

--- gadgets/bart/bartgadget.h
+++ gadgets/bart/bartgadget.h
@@ -97,6 +97,8 @@ namespace Gadgetron {
 		std::string &getOutputFilename(const std::string &bartCommandLine);
 		void cleanup(std::string &createdFiles);
 		void ltrim(std::string &str);
+		void rtrim(std::string &str);
+		void trim(std::string &str);
 		void replace_default_parameters(std::string &str);
 	};
 
