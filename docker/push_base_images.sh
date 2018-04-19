739f710bfa3903dae73b5dedec856fc148cc5b67
xueh2 <hui.xue@nih.gov>
2018-04-19 10:51:22 -0400

    update push_base_images.sh

--- docker/push_base_images.sh
+++ docker/push_base_images.sh
@@ -8,4 +8,6 @@ docker push gadgetron/ubuntu1404_cuda75_base
 docker push gadgetron/ubuntu1604_base
 docker push gadgetron/ubuntu1604_cuda55_base
 docker push gadgetron/ubuntu1604_cuda75_base
-
+docker push gadgetron/ubuntu1604_cuda80_base
+docker push gadgetron/ubuntu1604_cuda80_cudnn6_base
+docker push gadgetron/ubuntu1604_cuda80_cudnn7_base
