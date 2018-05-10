1c78f7965707602149915f905617c428fbc570ba
Mahamadou Diakite <bijoumd@gmail.com>
2018-05-10 13:16:29 -0400

    Modified reconstruction script

--- gadgets/bart/Sample_Grappa_Recon_Standard.sh
+++ gadgets/bart/Sample_Grappa_Recon_Standard.sh
@@ -29,8 +29,8 @@ bart transpose 3 4 reference_data_cc cc_reference_data
 bart fmac -C -s 8 input_data cc_mat_P input_data_cc
 bart transpose 3 4 input_data_cc cc_input_data
 
-bart ecalib -c0.65 -k6 -r$reference_lines_PE1 -m2 -S cc_reference_data maps
-bart rsense -l1 -r0.001 -i50 cc_input_data maps ims
+bart ecalib -c0.7 -k7 -r$reference_lines_PE1 -m4 -S cc_reference_data maps
+bart pics -l1 -r0.1 -i150 cc_input_data maps ims
 
 if "$debug";then
 	set +x
