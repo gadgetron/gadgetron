fd1dbb8048654db244838b88d6ca8df7ed5186c2
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2018-06-18 13:12:51 +0200

    Fixed the frequency shift correction. Changed gradient descent to be more robust

--- toolboxes/fatwater/correct_frequency_shift.cpp
+++ toolboxes/fatwater/correct_frequency_shift.cpp
@@ -14,6 +14,7 @@ namespace Gadgetron{
 
         void correct_frequency_shift(hoNDArray<std::complex<float>> &species_images, const Parameters &parameters){
 
+
             uint16_t X = species_images.get_size(0);
             uint16_t Y = species_images.get_size(1);
             uint16_t Z = species_images.get_size(2);
@@ -22,12 +23,13 @@ namespace Gadgetron{
             uint16_t S = species_images.get_size(5);
             uint16_t LOC = species_images.get_size(6);
 
-            if (LOC > 1) throw std::invalid_argument("Phase shift does not support LOC dimension greater than 1");
 
 
             std::vector<size_t> sub_dimension = {X,Y,Z,CHA,N,1,1};
             auto data_ptr = species_images.get_data_ptr();
 
+
+            hoNDFFT<float>::instance()->fft(&species_images,0);
 
             for (int kspecies = 0; kspecies < parameters.species.size(); kspecies++){
 
@@ -40,32 +42,33 @@ namespace Gadgetron{
                               std::accumulate(
                         species.amplitude_frequency_pairs.begin(),species.amplitude_frequency_pairs.end(), 0.0f,
                                             [](auto v, auto pair) { return v + pair.first.real();});
+                mean_frequency_offset *= parameters.field_strength_T*parameters.gyromagnetic_ratio_Mhz;
 
                 if (std::abs(mean_frequency_offset) < 1.0 ) continue;
 
 
 
-                hoNDArray<std::complex<float>> phase_ramp(species_images.get_size(0));
+                hoNDArray<std::complex<float>> phase_ramp(X);
 
                 for (int i = 0; i < phase_ramp.get_number_of_elements(); i++){
                     phase_ramp[i] = std::exp(2if*PI*parameters.sample_time_us*1e-6f*float(i)*mean_frequency_offset);
                 }
 
-
-                auto view = hoNDArray<std::complex<float>>(sub_dimension,data_ptr);
-                hoNDFFT<float>::instance()->fft(&view,0);
-
-                view *= phase_ramp;
-
-                hoNDFFT<float>::instance()->ifft(&view,0);
-
-
-                data_ptr += view.get_number_of_elements();
+                //Kristoffer will have a field day with this.
+                for (int kL = 0; kL < LOC; kL++)
+                    for (int kN = 0; kN < N; kN++)
+                        for (int kCHA = 0; kCHA < CHA; kCHA++)
+                            for (int kZ = 0; kZ < Z; kZ++)
+                                for (int kY = 0; kY < Y; kY++)
+                                    for (int kX = 0; kX < Y; kX++)
+                                        species_images(kX,kY,kZ,kCHA,kN,kspecies,kL) *= phase_ramp[kX];
 
 
 
 
             }
+//
+            hoNDFFT<float>::instance()->ifft(&species_images,0);
 
 
 
