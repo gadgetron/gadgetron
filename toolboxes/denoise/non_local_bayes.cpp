6ef581e10a39591a8fab2271f592b3bd782fb7a1
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2018-06-22 13:47:47 +0200

    Updated nonlocal bayes

--- toolboxes/denoise/non_local_bayes.cpp
+++ toolboxes/denoise/non_local_bayes.cpp
@@ -7,6 +7,7 @@
 #include "vector_td_utilities.h"
 #include <Eigen/Dense>
 #include <GadgetronTimer.h>
+#include "hoArmadillo.h"
 
 
 namespace Gadgetron {
@@ -15,16 +16,16 @@ namespace Gadgetron {
 
         namespace {
 
-            template<class T, int D>
-            Eigen::Matrix<T, D * D, 1> get_patch(const hoNDArray<T> &image, int x, int y,
+            template<class T>
+            arma::Col<T> get_patch(const hoNDArray<T> &image, int x, int y,int patch_size,
                                                  const vector_td<int, 2> &image_dims) {
 
-                constexpr int N = D * D;
-                Eigen::Matrix<T, D * D, 1> window;
-                for (int ky = 0; ky < D; ky++) {
-                    for (int kx = 0; kx < D; kx++) {
-                        window[kx + ky * D] = image(((kx - D / 2) + x + image_dims[0]) % image_dims[0],
-                                                    ((ky - D / 2) + y + image_dims[1]) % image_dims[1]);
+                const int N = patch_size*patch_size;
+                arma::Col<T> window = arma::Col<T>(N);
+                for (int ky = 0; ky < patch_size; ky++) {
+                    for (int kx = 0; kx < patch_size; kx++) {
+                        window[kx + ky * patch_size] = image(((kx - patch_size / 2) + x + image_dims[0]) % image_dims[0],
+                                                    ((ky - patch_size / 2) + y + image_dims[1]) % image_dims[1]);
 
                     }
                 }
@@ -32,16 +33,23 @@ namespace Gadgetron {
             };
 
 
-            template<class T, int D>
-            hoNDArray<vector_td<T, D * D>> create_patches(const hoNDArray<T> &image) {
-                const vector_td<int, 2> image_dims = vector_td<int, 2>(
-                        from_std_vector<size_t, 2>(*image.get_dimensions()));
 
-                hoNDArray<vector_td<T, D * D>> result(image.get_dimensions());
+            template<class T> struct ImagePatch {
+                arma::Col<T> patch;
+                int center_x, center_y;
+            };
 
-                for (int ky = 0; ky < image.get_size(0); ky++) {
-                    for (int kx = 0; kx < image.get_size(0); kx++) {
-                        result(kx, ky) = get_patch(image, kx, ky, image_dims);
+
+            template<class T>
+            std::vector<ImagePatch<T>> create_patches(const hoNDArray<T> &image,int kx, int ky, int patch_size, int search_window,const vector_td<int,2>& image_dims) {
+
+                std::vector<ImagePatch<T>> result;
+//                result.reserve(search_window);
+
+                for (int dy = std::max(ky-search_window/2,0); dy < std::min(search_window/2+ky,image_dims[1]); dy++) {
+                    for (int dx = std::max(kx-search_window/2,0); dx < std::min(search_window/2+kx,image_dims[0]); dx++) {
+
+                    result.push_back(ImagePatch<T>{get_patch(image,dx,dy,patch_size,image_dims), dx, dy});
                     }
                 }
 
@@ -49,100 +57,159 @@ namespace Gadgetron {
 
             };
 
-//    template<class T, unsigned int D>
-//    std::vector<float>
-//    calculate_distances(const hoNDArray<T> &image, float noise_std, int search_radius,
-//                        const vector_td<int, 2> &image_dims,
-//                        int ky, int kx) {
-//        std::vector<float> distances(search_radius * search_radius);
-//
-//        auto current_patch = get_patch(image, kx, ky, image_dims);
-//        for (int dy = -search_radius; dy < search_radius; dy++) {
-//            for (int dx = -search_radius; dx < search_radius; dx++) {
-//
-//                distances[dx + dy * search_radius] = distance(current_patch,
-//                                                              get_patch(image, kx + dx, ky + dy, image_dims),
-//                                                              noise_std);
-//
-//            }
-//        }
-//        return distances;
-//    }
 
-            template<class T, unsigned int D>
-            Eigen::Matrix<T, D * D, 1>
-            get_mean_patch(const hoNDArray<T> &image, int search_radius, const vector_td<int, 2> &image_dims,
-                           int kx,
-                           int ky) {
+           template<class T> void add_patch(ImagePatch<T>& patch, hoNDArray<T>& image, hoNDArray<int>& count, int patch_size, const vector_td<int,2>& image_dims){
 
 
-                Eigen::Matrix<T, D * D, 1> mean_patch = Eigen::Matrix<T, D * D, 1>::Constant(T(0));
+               for (int ky = 0; ky < patch_size; ky++ ){
+                   auto output_ky =  (patch.center_y+ky-patch_size/2 + image_dims[0])%image_dims[0];
+                   for (int kx = 0; kx < patch_size; kx++ ){
+                       auto output_kx =  (patch.center_x+kx-patch_size/2 + image_dims[0])%image_dims[0];
+                       image(output_kx,output_ky) += patch.patch[kx+ky*patch_size];
+                       count(output_kx,output_ky)++;
+                   }
+               }
 
-                for (int dy = -search_radius / 2; dy < search_radius / 2; dy++) {
-                    for (int dx = -search_radius / 2; dx < search_radius / 2; dx++) {
-                        auto other_patch = get_patch<T, D>(image, kx + dx, ky + dy, image_dims);
-                        mean_patch += other_patch;
-                    }
+           };
+
+
+
+            template<class T>
+            float distance(const arma::Col<T>& patch1, const arma::Col<T>& patch2){
+                arma::Col<T> diff = patch1-patch2;
+
+                float result = 0;
+                for (auto d : diff) result += std::norm(d);
+
+                result /= patch1.size()*patch1.size();
+                return result;
+            };
+
+            template<class T>
+            arma::Col<T> get_mean_patch(const std::vector<ImagePatch<T>>& patches){
+
+                auto mean_patch = arma::Col<T>(patches.front().patch.size(),arma::fill::zeros);
+
+                for (auto& patch : patches){
+                    mean_patch += patch.patch;
                 }
-                mean_patch /= search_radius * search_radius;
+
+                mean_patch /= patches.size();
+
                 return mean_patch;
             }
 
-            template<class T, unsigned int D>
-            Eigen::Matrix<T, D * D, D * D>
-            get_covariance_matrix(const hoNDArray<T> &image, int search_radius, Eigen::Matrix<T, D * D, 1> mean_patch,
-                                  const vector_td<int, 2> &image_dims, int kx,
-                                  int ky) {
+            template<class T>   arma::Mat<T>
+            get_covariance_matrix(const std::vector<ImagePatch<T>>& patches, const arma::Col<T>& mean_patch) {
 
-                using CovMat = Eigen::Matrix<T, D * D, D * D>;
-                CovMat covariance_matrix = CovMat::Constant(T(0));
+                auto covariance_matrix = arma::Mat<T>(mean_patch.size(),mean_patch.size(),arma::fill::zeros);
 
-                for (int dy = -search_radius / 2; dy < search_radius / 2; dy++) {
-                    for (int dx = -search_radius / 2; dx < search_radius / 2; dx++) {
-                        auto other_patch = get_patch<T, D>(image, kx + dx, ky + dy, image_dims);
-                        covariance_matrix += (other_patch - mean_patch) * (other_patch - mean_patch).transpose();
-                    }
+                for (auto& patch : patches ){
+                    covariance_matrix += (patch.patch - mean_patch) * (patch.patch - mean_patch).t();
                 }
-                covariance_matrix /= search_radius * search_radius - 1;
+                covariance_matrix /= patches.size()-1;
+
                 return covariance_matrix;
             }
 
 
-            template<class T>
-            hoNDArray<T> non_local_bayes_single_image(const hoNDArray<T> &image, float noise_std, int search_radius) {
 
-                constexpr int D = 5;
+            template <class T> void
+            filter_patches(std::vector<ImagePatch<T>>& patches, int n_patches, const arma::Col<T> &reference_patch ) {
+
+                auto distances = std::vector<float>(patches.size());
+                transform(patches.begin(),patches.end(),distances.begin(),[&](auto patch){return distance(patch.patch,reference_patch);});
+
+                std::vector<size_t> patch_indices(patches.size());
+                iota(patch_indices.begin(),patch_indices.end(),0);
+
+                sort(patch_indices.begin(),patch_indices.end(),[&](auto v1, auto v2){ return distances[v1] < distances[v2];});
+
+
+                std::vector<ImagePatch<T>> best_patches(n_patches);
+
+                for (int i = 0; i < n_patches; i++){
+                                best_patches[i] = std::move(patches[patch_indices[i]]);
+                }
+
+                patches = std::move(best_patches);
+            }
+
+            template <class T>
+            arma::Col<T> bayesian_estimate(const arma::Col<T>& patch, const arma::Mat<T>& covariance, const arma::Mat<T>& inv_covariance,const arma::Col<T>& mean_patch, float noise_std){
+
+                return mean_patch + (covariance+noise_std*noise_std*arma::eye<arma::Mat<T>>(covariance.n_rows,covariance.n_cols))*
+                                    inv_covariance*(patch-mean_patch);
+
+            }
+
+
+            template<class T>
+            hoNDArray<T> non_local_bayes_single_image(const hoNDArray<T> &image, float noise_std, int search_window) {
+
+
+                if (image.get_number_of_dimensions() != 2)
+                    throw std::invalid_argument("non_local_bayes: image must be 2 dimensional");
+
+
+                constexpr int patch_size = 5;
+
+                const int n_patches = 50;
 
                 hoNDArray<T> result(image.get_dimensions());
                 result.fill(0);
 
+                hoNDArray<bool> mask(image.get_dimensions());
+                mask.fill(true);
+
+                hoNDArray<int> count(image.get_dimensions());
+                count.fill(0);
+
                 const float noise_std2 = noise_std * noise_std;
+//                float noise_std2 = 0;
                 const vector_td<int, 2> image_dims = vector_td<int, 2>(
                         from_std_vector<size_t, 2>(*image.get_dimensions()));
 
-//                auto patches = create_patches<T,D>(image);
 
 
 //#pragma omp parallel for collapse(2)
                 for (int ky = 0; ky < image.get_size(1); ky++) {
                     for (int kx = 0; kx < image.get_size(0); kx++) {
 
-                        auto mean_patch = get_mean_patch<T, D>(image, search_radius, image_dims, kx, ky);
-                        auto covariance_matrix = get_covariance_matrix<T, D>(image, search_radius, mean_patch,
-                                                                             image_dims,
-                                                                             kx,
-                                                                             ky);
+                        if (mask(kx,ky))
+                        {
+
+                            auto reference_patch = get_patch(image,kx,ky,patch_size,image_dims);
+                            auto patches = create_patches(image,kx,ky,patch_size,search_window,image_dims);
+
+                            filter_patches(patches, n_patches, reference_patch);
 
 
-                        auto window = get_patch<T, D>(image, kx, ky, image_dims);
-//                        window = mean_patch;
-                        window =
-                                 (covariance_matrix - noise_std2 * decltype(covariance_matrix)::Ones()) *
-                                 covariance_matrix.inverse() * (window - mean_patch);
+                            auto mean_patch = get_mean_patch(patches);
+                            auto covariance_matrix = get_covariance_matrix(patches,mean_patch);
 
-                        result(kx, ky) = window[D / 2 + (D / 2) * D];
+
+                            auto inv_cov = arma::Mat<T>(mean_patch.size(),mean_patch.size());
+                            if (arma::inv(inv_cov,covariance_matrix)){
+                                for (auto& patch : patches){
+                                    patch.patch = bayesian_estimate(patch.patch,covariance_matrix,inv_cov,mean_patch,noise_std);
+                                }
+                            }
+
+
+                            for (auto & patch : patches ){
+                                add_patch(patch,result,count,patch_size,image_dims);
+                                mask(patch.center_x,patch.center_y)  = false;
+                            }
+
+
+                        }
 
                     }
+                }
+
+                for (size_t i = 0; i < result.get_number_of_elements(); i++){
+                    result[i] /= count[i];
                 }
 
                 return result;
@@ -150,10 +217,11 @@ namespace Gadgetron {
             }
 
 
-            template<class T>
-            hoNDArray<T> non_local_bayes_T(const hoNDArray<T> &image, float noise_std, unsigned int search_radius) {
 
-                GadgetronTimer("Non local bayes");
+
+            template<class T>
+            hoNDArray<T> non_local_bayes_T(const hoNDArray<T> &image, float noise_std, unsigned int search_window) {
+
                 size_t n_images = image.get_number_of_elements() / (image.get_size(0) * image.get_size(1));
 
                 std::vector<size_t> image_dims = {image.get_size(0), image.get_size(1)};
@@ -165,7 +233,7 @@ namespace Gadgetron {
                     auto image_view = hoNDArray<T>(image_dims, image.get_data_ptr() + i * image_elements);
                     auto result_view = hoNDArray<T>(image_dims, result.get_data_ptr() + i * image_elements);
 
-                    result_view = non_local_bayes_single_image(image_view, noise_std, search_radius);
+                    result_view = non_local_bayes_single_image(image_view, noise_std, search_window);
 
 
                 }
@@ -174,13 +242,13 @@ namespace Gadgetron {
         }
 
 
-        hoNDArray<float> non_local_bayes(const hoNDArray<float> &image, float noise_std, unsigned int search_radius) {
-            return non_local_bayes_T(image, noise_std, search_radius);
+        hoNDArray<float> non_local_bayes(const hoNDArray<float> &image, float noise_std, unsigned int search_window) {
+            return non_local_bayes_T(image, noise_std, search_window);
         }
 
         hoNDArray<std::complex<float>>
-        non_local_bayes(const hoNDArray<std::complex<float>> &image, float noise_std, unsigned int search_radius) {
-            return non_local_bayes_T(image, noise_std, search_radius);
+        non_local_bayes(const hoNDArray<std::complex<float>> &image, float noise_std, unsigned int search_window) {
+            return non_local_bayes_T(image, noise_std, search_window);
         }
     }
 
