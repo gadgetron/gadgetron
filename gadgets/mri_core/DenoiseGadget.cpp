630a084060603f5eaa05e865f58bddc2ba4c92e6
David Christoffer Hansen <dchansen@users.noreply.github.com>
2018-06-26 15:17:09 +0200

    Denoise Gadget with non-local means and non-local Bayes (#634)
    * Initial push for denoising
    
    * added nonlocal Bayes and image array denoising
    
    * Rewrite of non local Bayes
    
    * Updated nonlocal bayes
    
    * Made vector_td norm and norm_squared return type real-valued, rather than complex
    
    * Fixed a bug in non_local_bayes
    
    * Fixed a bug and improved speed of non local Bayes
    
    * Finalised denoise gadget
    
    * Wrote some comments.
    
    * Implemented feedback

---
+++ gadgets/mri_core/DenoiseGadget.cpp
@@ -0,0 +1,75 @@
+//
+// Created by dchansen on 6/19/18.
+//
+
+#include <GadgetronTimer.h>
+#include "DenoiseGadget.h"
+#include "non_local_means.h"
+#include "non_local_bayes.h"
+
+namespace Gadgetron {
+    int DenoiseGadget::process(ACE_Message_Block *mb) {
+
+
+        if (auto m1 = AsContainerMessage<ISMRMRD::ImageHeader>(mb)) {
+            if (auto m2 = AsContainerMessage<hoNDArray<float>>(m1->cont())) {
+                return this->process(m1, m2);
+            } else if (auto m2 = AsContainerMessage<hoNDArray<std::complex<float>>>(m1->cont())) {
+                return this->process(m1, m2);
+            }
+        }
+
+
+        if (auto m1 = AsContainerMessage<IsmrmrdImageArray>(mb)) {
+            return this->process(m1);
+        }
+
+
+        if (pass_on_undesired_data_) {
+            return this->next()->putq(mb);
+        } else {
+            GERROR("Denoise Gadget received wrong input data type\n"); // Newline?
+            return -1;
+        }
+    }
+
+
+    template<class T>
+    int DenoiseGadget::process(GadgetContainerMessage<ISMRMRD::ImageHeader> *header_msg,
+                               GadgetContainerMessage<hoNDArray<T>> *image_msg) {
+
+        auto &input = *image_msg->getObjectPtr();
+        input = denoise_function(input);
+        this->next()->putq(header_msg);
+
+        return GADGET_OK;
+
+    }
+
+    int DenoiseGadget::process(GadgetContainerMessage<IsmrmrdImageArray> *image_array) {
+
+        auto &input = image_array->getObjectPtr()->data_;
+        input = denoise_function(input);
+        this->next()->putq(image_array);
+
+        return GADGET_OK;
+
+    }
+
+    template<class T>
+    hoNDArray<T> DenoiseGadget::denoise_function(const Gadgetron::hoNDArray<T> & input) {
+
+        if (denoiser == "non_local_bayes") {
+            return Denoise::non_local_bayes(input, image_std, search_radius);
+        } else if (denoiser == "non_local_means") {
+            return Denoise::non_local_means(input, image_std, search_radius);
+        } else {
+            throw std::invalid_argument(std::string("DenoiseGadget: Unknown denoiser type: ") + std::string(denoiser));
+        }
+
+    }
+
+
+    GADGET_FACTORY_DECLARE(DenoiseGadget)
+
+}
