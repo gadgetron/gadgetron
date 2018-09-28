9a9a41262cf1792676cd8eb9c5de5b4f0375c645
Hui Xue <hui.xue@nih.gov>
2018-09-28 19:53:56 -0400

    add cuda10.0 docker file for base

--- docker/build_base_images.sh
+++ docker/build_base_images.sh
@@ -5,20 +5,20 @@ base_dir=$(pwd)
 cd ${base_dir}/base/ubuntu_1604
 docker build --network=host -t gadgetron/ubuntu_1604_base .
 
-cd ${base_dir}/base/ubuntu_1604_cuda55
-docker build --network=host -t gadgetron/ubuntu_1604_cuda55_base .
+# cd ${base_dir}/base/ubuntu_1604_cuda55
+# docker build --network=host -t gadgetron/ubuntu_1604_cuda55_base .
 
-cd ${base_dir}/base/ubuntu_1604_cuda75
-docker build --network=host -t gadgetron/ubuntu_1604_cuda75_base .
+# cd ${base_dir}/base/ubuntu_1604_cuda75
+# docker build --network=host -t gadgetron/ubuntu_1604_cuda75_base .
 
-cd ${base_dir}/base/ubuntu_1604_cuda80
-docker build --network=host -t gadgetron/ubuntu_1604_cuda80_base .
+# cd ${base_dir}/base/ubuntu_1604_cuda80
+# docker build --network=host -t gadgetron/ubuntu_1604_cuda80_base .
 
-cd ${base_dir}/base/ubuntu_1604_cuda80_cudnn6
-docker build --network=host -t gadgetron/ubuntu_1604_cuda80_cudnn6_base .
+# cd ${base_dir}/base/ubuntu_1604_cuda80_cudnn6
+# docker build --network=host -t gadgetron/ubuntu_1604_cuda80_cudnn6_base .
 
-cd ${base_dir}/base/ubuntu_1604_cuda80_cudnn7
-docker build --network=host -t gadgetron/ubuntu_1604_cuda80_cudnn7_base .
+# cd ${base_dir}/base/ubuntu_1604_cuda80_cudnn7
+# docker build --network=host -t gadgetron/ubuntu_1604_cuda80_cudnn7_base .
 
 cd ${base_dir}/base/ubuntu_1604_cuda90_cudnn7
 docker build --network=host -t gadgetron/ubuntu_1604_cuda90_cudnn7_base .
@@ -34,3 +34,6 @@ docker build --network=host -t gadgetron/ubuntu_1804_cuda90_cudnn7_base .
 
 cd ${base_dir}/base/ubuntu_1804_cuda92_cudnn7
 docker build --network=host -t gadgetron/ubuntu_1804_cuda92_cudnn7_base .
+
+cd ${base_dir}/base/ubuntu_1804_cuda100_cudnn7
+docker build --network=host -t gadgetron/ubuntu_1804_cuda100_cudnn7_base .
