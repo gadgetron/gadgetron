ea26f22c265ceb74a0e988da30ba9f7258eb242f
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2018-11-07 12:27:41 +0100

    Updated FindCBLAS to accept openblas as a perfectly fine CBLAS implementation

--- cmake/FindCBLAS.cmake
+++ cmake/FindCBLAS.cmake
@@ -1,7 +1,7 @@
 find_path(CBLAS_INCLUDE_DIR cblas.h
-        HINTS /usr/include /usr/local/include)
+        HINTS /usr/include /usr/local/include /usr/include/openblas /usr/local/include/openblas)
 
-find_library(CBLAS_LIBRARY cblas libcblas
+find_library(CBLAS_LIBRARY cblas libcblas openblas libopenblas
         HINTS /usr/lib/ /usr/lib64)
 
 find_package_handle_standard_args(CBLAS DEFAULT_MSG CBLAS_LIBRARY CBLAS_INCLUDE_DIR)
