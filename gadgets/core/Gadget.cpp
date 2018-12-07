5384edf6bf6a90430034f6ee9d124d3923651748
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2018-12-07 11:58:02 +0100

    Race conditions

--- gadgets/core/Gadget.cpp
+++ gadgets/core/Gadget.cpp
@@ -3,14 +3,45 @@
 
 namespace Gadgetron {
     std::string Gadget::get_string_value(const char *name) {
-
         std::string str_val;
         GadgetPropertyBase *p = find_property(name);
         if (!p) {
             GERROR("Property %s\n", name);
-            throw std::runtime_error("Attempting to access non existent property on Gadget");
+            throw std::runtime_error(
+                    "Attempting to access non existent property on Gadget");
         }
         return std::string(p->string_value());
     }
 
-}
+    LegacyGadgetNode::LegacyGadgetNode(
+            std::unique_ptr<Gadget> &&gadget_ptr,
+            const ISMRMRD::IsmrmrdHeader &header,
+            const std::unordered_map<std::string, std::string> &props)
+            : gadget(std::move(gadget_ptr)) {
+        gadget->process_config(header);
+        for (auto &key_val : props) {
+            gadget->set_parameter(key_val.first.c_str(), key_val.second.c_str());
+        }
+    }
+
+    void LegacyGadgetNode::process(
+            std::shared_ptr<Core::InputChannel<Core::Message>> in,
+            std::shared_ptr<Core::OutputChannel> out) {
+        try {
+            //              GDEBUG("PenPen was here");
+            gadget->next(std::make_shared<ChannelAdaptor>(out));
+
+            //              for (auto message : *in ) {
+            while (true) {
+                auto message = in->pop();
+                //                  GDEBUG_STREAM("Message_ptr " << message.get() <<
+                //                  std::endl); GDEBUG_STREAM("Gadget_ptr " <<
+                //                  gadget.get() << std::endl);
+                gadget->process(message->to_container_message());
+            }
+        } catch (Core::ChannelClosedError err) {
+            gadget->close();
+            out->close();
+        }
+    }
+}  // namespace Gadgetron
--- gadgets/mri_core/ImageFinishGadget.cpp
+++ gadgets/mri_core/ImageFinishGadget.cpp
@@ -4,7 +4,10 @@ namespace Gadgetron{
 
     void ImageFinishGadget::process(std::shared_ptr<Core::InputChannel<Core::Message>> in, std::shared_ptr<Core::OutputChannel> out) {
 
-        for(auto message : *in ){
+//        for(auto message : *in ){
+        while(true) {
+            auto message = in->pop();
+            GDEBUG("ImageFinish got here");
             out->push_message(std::move(message));
         }
     }
