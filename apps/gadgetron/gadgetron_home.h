ff2c402a64ef1dc03f4874032ab9ccae8ec28b75
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-01-04 10:34:11 +0100

    Deleted unused files.

--- apps/gadgetron/gadgetron_home.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <boost/filesystem.hpp>
-
-namespace Gadgetron {
-    [[deprecated]] boost::filesystem::path get_gadgetron_home();
-}
--- core/gadgets/OversamplingGadget.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "OversamplingGadget.h"
-
-namespace Gadgetron::Core::Gadgets {
-
-    GADGETRON_GADGET_EXPORT(OversamplingGadget)
-}
