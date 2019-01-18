fb641a1178d8b01e561dc601405a4e8ddca01ab3
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-01-18 16:36:55 +0100

    End-of-day commit. Compiles and is less broken than usual.

--- gadgets/grappa_new/ImageAccumulator.cpp
+++ gadgets/grappa_new/ImageAccumulator.cpp
@@ -1,13 +1,43 @@
 #include "ImageAccumulator.h"
 
 #include <chrono>
+#include <ismrmrd/ismrmrd.h>
 
 #include "Node.h"
 
 #include "log.h"
 
 namespace {
+    using namespace Gadgetron;
     using namespace Gadgetron::Core;
+/*
+    void copy_acquisition_data_to_buffer(
+            const Acquisition &acquisition,
+            hoNDArray<std::complex<float>> &buffer
+    ) {
+        auto &header = std::get<ISMRMRD::AcquisitionHeader>(acquisition);
+        auto &data   = std::get<hoNDArray<std::complex<float>>>(acquisition);
+
+        auto current_slice      = header.idx.slice;
+//        auto current_line       = header.idx.kspace_encode_step_1 + line_offset_;
+        auto current_partition  = header.idx.kspace_encode_step_2;
+        auto samples            = header.number_of_samples;
+
+        std::complex<float> *b = image_data_[slice]->getObjectPtr()->get_data_ptr();
+        std::complex<float> *d = m2->getObjectPtr()->get_data_ptr();
+
+        size_t offset = 0;
+        //Copy the data for all the channels
+        for (int c = 0; c < acq_head->active_channels; c++) {
+            offset =
+                    c * image_dimensions_[0] * image_dimensions_[1] * image_dimensions_[2] +
+                    partition * image_dimensions_[0] * image_dimensions_[1] +
+                    line * image_dimensions_[0];
+
+            memcpy(b + offset, d + c * samples, sizeof(std::complex<float>) * samples);
+        }
+    }
+    */
 }
 
 namespace Gadgetron::Grappa {
@@ -21,11 +51,23 @@ namespace Gadgetron::Grappa {
 
         GINFO_STREAM("Hello, I'm the ImageAccumulator process function. I'm running!");
 
-        std::vector<hoNDArray<std::complex<float>>> image_data;
+        std::vector<hoNDArray<std::complex<float>>> image_buffers;
         std::vector<std::chrono::milliseconds> time_stamps;
 
         for (auto acquisition : in) {
-            GINFO_STREAM("ImageAccumulator handling an acquisition.")
+            auto &header = std::get<ISMRMRD::AcquisitionHeader>(acquisition);
+
+            auto slice = header.idx.slice;
+
+            GINFO_STREAM("SLICE! " << slice);
+
+            /*
+            copy_acquisition_data_to_buffer(acquisition, image_buffers[slice]);
+
+            if (header.isFlagSet(ISMRMRD::ISMRMRD_ACQ_LAST_IN_SLICE)) {
+                push_recon_job(acquisition, image_buffers[slice], out);
+            }
+             */
         }
 
         GINFO_STREAM("ImageAccumulator acquisition loop over - channel closed.")
--- gadgets/grappa_new/ImageAccumulator.h
+++ gadgets/grappa_new/ImageAccumulator.h
@@ -11,6 +11,17 @@ namespace Gadgetron::Grappa {
     public:
         ImageAccumulator(const Core::Context &, const std::unordered_map<std::string, std::string> &);
 
+        NODE_PROPERTY(image_series, int, "Image series number for output images", 0);
+        NODE_PROPERTY(target_coils, int, "Number of target coils for GRAPPA recon", 0);
+        NODE_PROPERTY(uncombined_channels, std::string, "Uncombined channels (as a comma separated list of channel indices", "");
+        NODE_PROPERTY(uncombined_channels_by_name, std::string, "Uncombined channels (as a comma separated list of channel names", "");
+
+        // TODO: Stop laughing and do something not a damn config flag.
+        NODE_PROPERTY(use_gpu, bool, "If true, recon will try to use GPU resources (when available)", false);
+
+        // TODO: Get rid of these pointless things.
+        NODE_PROPERTY(device_channels, int, "Number of device channels", 0);
+
         void process(Core::TypedInputChannel<Core::Acquisition> &in, Core::OutputChannel &out) override;
     };
 }
