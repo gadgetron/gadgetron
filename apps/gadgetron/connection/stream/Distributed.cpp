5a3f6854ccd7102a0ae755349879f66b4a0a610a
David C Hansen <dae.hansen@gmail.com>
2019-01-21 15:21:33 +0100

    More distributed

--- apps/gadgetron/connection/stream/Distributed.cpp
+++ apps/gadgetron/connection/stream/Distributed.cpp
@@ -1,1 +1,32 @@
+//
+// Created by dchansen on 1/16/19.
+//
+
 #include "Distributed.h"
+
+
+void Gadgetron::Server::Connection::Stream::Distributed::process(std::shared_ptr<Gadgetron::Core::Channel> input,
+                                                                 std::shared_ptr<Gadgetron::Core::Channel> output,
+                                                                 Gadgetron::Server::Connection::ErrorHandler &error_handler) {
+
+}
+
+Gadgetron::Server::Connection::Stream::Distributed::Distributed(const Config::Distributed &distributed_config,
+                                                                const Gadgetron::Core::Context &context,
+                                                                Gadgetron::Server::Connection::Loader &loader)
+        : stream_config(distributed_config.stream), loader(loader), context(context) {
+    distributor  = load_distributor(distributed_config.distributor);
+
+    readers = loader.load_readers(distributed_config);
+    writers = loader.load_writers(distributed_config);
+
+
+}
+
+std::unique_ptr<Gadgetron::Core::Distributed::Distributor>
+Gadgetron::Server::Connection::Stream::Distributed::load_distributor(
+        const Gadgetron::Server::Connection::Config::Distributor &conf) {
+    auto factory = loader.load_factory<Loader::generic_factory<Core::Distributed::Distributor>>(
+            "distributor_factory_export_", conf.classname, conf.dll);
+    return factory(context, conf.properties);
+}
--- apps/gadgetron/connection/stream/Distributed.h
+++ apps/gadgetron/connection/stream/Distributed.h
@@ -1,1 +1,37 @@
+//
+// Created by dchansen on 1/16/19.
+//
+
 #pragma once
+
+#include "distributed/Distributor.h"
+#include "Channel.h"
+#include "connection/stream/Processable.h"
+#include "Stream.h"
+
+namespace Gadgetron::Server::Connection::Stream {
+    class Distributed : public Processable {
+
+        Distributed(const Config::Distributed &, const Core::Context &, Loader &);
+
+
+    public:
+        void process(std::shared_ptr<Core::Channel> input, std::shared_ptr<Core::Channel> output,
+                     ErrorHandler &error_handler) override;
+
+    private:
+
+        std::unique_ptr<Core::Distributed::Distributor> load_distributor(const Config::Distributor&);
+
+        std::unique_ptr<Core::Distributed::Distributor> distributor;
+        const Config::Stream stream_config;
+        const Core::Context context;
+        std::map<uint16_t,std::unique_ptr<Core::Reader>> readers;
+        std::vector<std::unique_ptr<Core::Writer>> writers;
+        Gadgetron::Server::Connection::Loader& loader;
+
+    };
+}
+
+
+
