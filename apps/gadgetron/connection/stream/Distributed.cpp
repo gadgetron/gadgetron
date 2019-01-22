c1ff680db68dc6201ea6e695615d50c71a8b2885
David C Hansen <dae.hansen@gmail.com>
2019-01-22 11:53:30 +0100

    More distributed

--- apps/gadgetron/connection/stream/Distributed.cpp
+++ apps/gadgetron/connection/stream/Distributed.cpp
@@ -5,6 +5,12 @@
 #include "Distributed.h"
 
 
+namespace {
+    std::vector<Gadgetron::Server::Distributed::Address> get_workers() {
+        return {{"localhost", "9002"}};
+    }
+}
+
 void Gadgetron::Server::Connection::Stream::Distributed::process(std::shared_ptr<Gadgetron::Core::Channel> input,
                                                                  std::shared_ptr<Gadgetron::Core::Channel> output,
                                                                  Gadgetron::Server::Connection::ErrorHandler &error_handler) {
@@ -14,12 +20,13 @@ void Gadgetron::Server::Connection::Stream::Distributed::process(std::shared_ptr
 Gadgetron::Server::Connection::Stream::Distributed::Distributed(const Config::Distributed &distributed_config,
                                                                 const Gadgetron::Core::Context &context,
                                                                 Gadgetron::Server::Connection::Loader &loader)
-        : stream_config(distributed_config.stream), loader(loader), context(context) {
-    distributor  = load_distributor(distributed_config.distributor);
+        : loader(loader), context(context),
+          config{distributed_config.readers, distributed_config.writers, distributed_config.stream},
+          workers{get_workers()} {
+    distributor = load_distributor(distributed_config.distributor);
 
     readers = loader.load_readers(distributed_config);
     writers = loader.load_writers(distributed_config);
-
 
 }
 
@@ -29,4 +36,11 @@ Gadgetron::Server::Connection::Stream::Distributed::load_distributor(
     auto factory = loader.load_factory<Loader::generic_factory<Core::Distributed::Distributor>>(
             "distributor_factory_export_", conf.classname, conf.dll);
     return factory(context, conf.properties);
+}
+
+std::shared_ptr<Gadgetron::Server::Distributed::RemoteChannel>
+Gadgetron::Server::Connection::Stream::Distributed::create_remote_channel() {
+
+
+    return std::shared_ptr<Gadgetron::Server::Distributed::RemoteChannel>();
 }
--- apps/gadgetron/connection/stream/Distributed.h
+++ apps/gadgetron/connection/stream/Distributed.h
@@ -8,10 +8,12 @@
 #include "Channel.h"
 #include "connection/stream/Processable.h"
 #include "Stream.h"
+#include "connection/distributed/RemoteChannel.h"
 
 namespace Gadgetron::Server::Connection::Stream {
     class Distributed : public Processable {
 
+        using RemoteChannel = Gadgetron::Server::Distributed::RemoteChannel;
         Distributed(const Config::Distributed &, const Core::Context &, Loader &);
 
 
@@ -21,14 +23,17 @@ namespace Gadgetron::Server::Connection::Stream {
 
     private:
 
+        std::shared_ptr<RemoteChannel> create_remote_channel();
+
         std::unique_ptr<Core::Distributed::Distributor> load_distributor(const Config::Distributor&);
 
         std::unique_ptr<Core::Distributed::Distributor> distributor;
-        const Config::Stream stream_config;
+        const Config config;
         const Core::Context context;
         std::map<uint16_t,std::unique_ptr<Core::Reader>> readers;
         std::vector<std::unique_ptr<Core::Writer>> writers;
         Gadgetron::Server::Connection::Loader& loader;
+        const std::vector<Gadgetron::Server::Distributed::Address> workers;
 
     };
 }
