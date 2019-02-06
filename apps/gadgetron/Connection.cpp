25835a72bb3f2aef917c0b2c9e4fd57edeea07af
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2019-02-06 15:56:44 +0100

    Changed connection to fork() on linux and Mac, rather than simply start a thread

--- apps/gadgetron/Connection.cpp
+++ apps/gadgetron/Connection.cpp
@@ -5,13 +5,23 @@
 #include "Context.h"
 
 #include "connection/Core.h"
+#if !(_WIN32 )
+#include <unistd.h>
+#endif
 
 using namespace Gadgetron::Server::Connection;
 
 namespace Gadgetron::Server::Connection {
 
+#if _WIN32
     void handle(const Gadgetron::Core::Context::Paths &paths, std::unique_ptr<std::iostream> stream) {
         auto thread = std::thread(handle_connection, std::move(stream), paths);
         thread.detach();
     }
+#else
+    void handle(const Gadgetron::Core::Context::Paths &paths, std::unique_ptr<std::iostream> stream) {
+        auto pid = fork();
+        if (pid == 0) handle_connection(std::move(stream),paths);
+    }
+#endif
 }
