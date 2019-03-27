86cf25bd5e43903650d956b988effa075dd328a3
Hui Xue <hui.xue@nih.gov>
2019-03-26 23:34:42 -0400

    add more controls for coil map estimation parameters

--- gadgets/mri_core/GenericReconGadget.h
+++ gadgets/mri_core/GenericReconGadget.h
@@ -37,6 +37,11 @@ namespace Gadgetron {
         GADGET_PROPERTY_LIMITS(coil_map_algorithm, std::string, "Method for coil map estimation", "Inati",
             GadgetPropertyLimitsEnumeration, "Inati", "Inati_Iter");
 
+        GADGET_PROPERTY(coil_map_kernel_size_readout, size_t, "Coil map estimation, kernel size along read out", 7);
+        GADGET_PROPERTY(coil_map_kernel_size_phase, size_t, "Coil map estimation, kernel size along phase/slice encoding", 5);
+        GADGET_PROPERTY(coil_map_num_iter, size_t, "Coil map estimation, number of iterations", 10);
+        GADGET_PROPERTY(coil_map_thres_iter, double, "Coil map estimation, threshold to stop iteration", 1e-4);
+
     protected:
 
         // --------------------------------------------------
