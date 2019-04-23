1a11b6de05db63d1447f98729ed23c68319e842c
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2019-04-23 15:28:06 +0200

    Added ChannelAlgorithms

--- core/ChannelAlgorithms.h
+++ core/ChannelAlgorithms.h
@@ -3,4 +3,41 @@
 //
 
 #pragma once
+#include "Channel.h"
 
+namespace Gadgetron { namespace Core {
+
+    template <class CHANNEL, class PRED> class TakeWhileChannel {
+
+    public:
+        TakeWhileChannel(CHANNEL& channel, PRED pred) : channel{ channel }, pred{ pred } {}
+
+        auto pop() {
+            if (done)
+                throw ChannelClosed();
+            auto message = channel.pop();
+            done         = !pred(message);
+            return std::move(message);
+        }
+
+    private:
+        CHANNEL& channel;
+        PRED pred;
+        bool done = false;
+    };
+
+    template <class CHANNEL, class PRED>
+    inline ChannelIterator<TakeWhileChannel<CHANNEL, PRED>> begin(TakeWhileChannel<CHANNEL, PRED>& channel) {
+        return ChannelIterator<TakeWhileChannel<CHANNEL, PRED>>(&channel);
+    }
+
+    template <class CHANNEL, class PRED>
+    inline ChannelIterator<TakeWhileChannel<CHANNEL, PRED>> end(TakeWhileChannel<CHANNEL, PRED>& channel) {
+        return ChannelIterator<TakeWhileChannel<CHANNEL, PRED>>(&channel);
+    }
+
+    template <class CHANNEL, class PRED> auto take_while(CHANNEL& channel, PRED pred) {
+        return TakeWhileChannel<CHANNEL, PRED>(channel, pred);
+    }
+
+}}
