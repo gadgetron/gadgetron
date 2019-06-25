a6345604f30780fddd4c8675106ab0f12eb8f483
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-06-25 16:39:27 +0200

    Surface changes to the external python example; added some comments and prettied it up.

--- gadgets/python/examples/simple_recon.py
+++ gadgets/python/examples/simple_recon.py
@@ -3,162 +3,160 @@ import logging
 import ismrmrd
 
 import numpy as np
-
-from multimethod import multimethod, overload
+import numpy.fft as fft
 
 import gadgetron.external
 
-import pyfftw
-
-pyfftw.interfaces.cache.enable()
-pyfftw.config.PLANNER_EFFORT = 'FFTW_ESTIMATE'
-
-import pyfftw.interfaces.scipy_fftpack as fftpack
+# When running an external Python module, functions decorated with the 'gadgetron.external.module'
+# decorator (see the simple_recon function below) will be invoked. This decorated function serves
+# as the entry point for external Python modules.
 
 
-def cfftn(data,axes):
-    '''Centered fft'''
-    return fftpack.ifftshift(fftpack.fftn(fftpack.fftshift(data,axes=axes)))
+def cfftn(data, axes):
+    # Centered fast fourier transform, n-dimensional
+    return fft.ifftshift(fft.fftn(fft.fftshift(data, axes=axes)))
 
 
-def cifftn(data,axes):
-    '''Centered ifft'''
-    return fftpack.fftshift(fftpack.ifftn(fftpack.ifftshift(data,axes),axes=axes))
+def cifftn(data, axes):
+    # Centered inverse fast fourier transform, n-dimensional
+    return fft.fftshift(fft.ifftn(fft.ifftshift(data, axes), axes=axes))
 
 
-def calculate_prewhitening(noise, scale_factor=1.0):
-    '''Calculates the noise prewhitening matrix
-    :param noise: Input noise data (array or matrix), ``[coil, nsamples]``
-    :scale_factor: Applied on the noise covariance matrix. Used to
-                   adjust for effective noise bandwith and difference in
-                   sampling rate between noise calibration and actual measurement:
-                   scale_factor = (T_acq_dwell/T_noise_dwell)*NoiseReceiverBandwidthRatio
-    :returns w: Prewhitening matrix, ``[coil, coil]``, w*data is prewhitened
-    '''
-
-    noise_int = noise.reshape((noise.shape[0], noise.size//noise.shape[0]))
-    M = float(noise_int.shape[1])
-    dmtx = (1/(M-1))*np.asmatrix(noise_int)*np.asmatrix(noise_int).H
-    dmtx = np.linalg.inv(np.linalg.cholesky(dmtx))
-    dmtx = dmtx*np.sqrt(2)*np.sqrt(scale_factor)
-
-    return dmtx
-
-
-def apply_prewhitening(data,dmtx):
-    '''Apply the noise prewhitening matrix
-    :param noise: Input noise data (array or matrix), ``[coil, ...]``
-    :param dmtx: Input noise prewhitening matrix
-    :returns w_data: Prewhitened data, ``[coil, ...]``,
-    '''
-
-    s = data.shape
-    return np.asarray(np.asmatrix(dmtx)*np.asmatrix(data.reshape(data.shape[0],data.size//data.shape[0]))).reshape(s)
-
-
-def is_noise_adjustment(acq):
-    return acq.is_flag_set(ismrmrd.ACQ_IS_NOISE_MEASUREMENT) > 0
-
-
-def prepare_noise_adjustment(connection):
+def noise_adjustment(connection):
+    # The dataset might include noise measurements (mine does). We'll consume noise measurements, use them
+    # to prepare a noise adjustment matrix, and never pass them down the chain. They contain no image data,
+    # and will not be missed. We'll also perform noise adjustment on following acquisitions, when we have a
+    # noise matrix available.
 
     noise_matrix = None
 
-    def noise_adjust(acquisition : lambda x: not is_noise_adjustment(x) ):
-        if (noise_matrix is not None):
-            acquisition.data[:] = apply_prewhitening(acquisition.data,noise_matrix)[:]
-        return acquisition
+    def calculate_whitening_transformation(noise, scale_factor=1.0):
+        # The whitening transformation is beyond the scope of these comments. Read more:
+        # https://en.wikipedia.org/wiki/Whitening_transformation
 
-    for mid,acquisition in connection:
-        if is_noise_adjustment(acquisition):
-            noise_matrix = calculate_prewhitening(acquisition.data)
+        noise_int = noise.reshape((noise.shape[0], noise.size // noise.shape[0]))
+        M = float(noise_int.shape[1])
+        dmtx = (1/(M-1)) * np.asmatrix(noise_int) * np.asmatrix(noise_int).H
+        dmtx = np.linalg.inv(np.linalg.cholesky(dmtx))
+
+        return np.sqrt(2) * np.sqrt(scale_factor) * dmtx
+
+    def apply_whitening_transformation(data, dmtx):
+        s = data.shape
+        return np.asarray(np.asmatrix(dmtx) * np.asmatrix(data.reshape(s[0], data.size // s[0]))).reshape(s)
+
+    def noise_adjust(acq):
+        if noise_matrix is not None:
+            acq.data[:] = apply_whitening_transformation(acq.data, noise_matrix)[:]
+        return acq
+
+    for _, acquisition in connection:
+        if acquisition.is_flag_set(ismrmrd.ACQ_IS_NOISE_MEASUREMENT):
+            noise_matrix = calculate_whitening_transformation(acquisition.data)
         else:
             yield noise_adjust(acquisition)
 
 
-def remove_oversampling(iterable, header: ismrmrd.xsd.ismrmrdHeader):
+def remove_oversampling(iterable, header):
     # The dataset I'm working with was originally taken on a Siemens scanner, and features 2x oversampling
-    # along the first dimension. We're going to have a look at the header. If our encoded space doesn't
-    # match the recon space, we're going to crop the acquisitions.
+    # along the first image dimension. We're going to have a look at the header. If our encoded space
+    # doesn't match the recon space, we're going to crop the acquisition.
 
     encoding_space = header.encoding[0].encodedSpace.matrixSize
-    recon_space= header.encoding[0].reconSpace.matrixSize
+    recon_space = header.encoding[0].reconSpace.matrixSize
 
     if encoding_space.x == recon_space.x:
-        return iterable
+        yield from iterable
 
     x0 = (encoding_space.x - recon_space.x) // 2
     x1 = (encoding_space.x - recon_space.x) // 2 + recon_space.x
-    for acq in iterable:
-        xspace = cifftn(acq.data, axes=[1])
+
+    def crop_acquisition(acquisition):
+        xspace = cifftn(acquisition.data, axes=[1])
         xspace = xspace[:, x0:x1]
-        acq.resize(number_of_samples=xspace.shape[1], active_channels=xspace.shape[0])
-        acq.center_sample = recon_space.x // 2
-        acq.data[:] = cfftn(xspace, axes=[1])
-        yield acq
-
-
-def accumulate(iterable, header : ismrmrd.xsd.ismrmrdHeader):
-    print(header.acquisitionSystemInformation.coilLabel)
-    matrixsize = header.encoding[0].encodedSpace.matrixSize
-    buffer = None
-
-    is_first = lambda acq : acq.is_flag_set(ismrmrd.ACQ_FIRST_IN_SLICE) > 0
-    is_last = lambda acq : acq.is_flag_set(ismrmrd.ACQ_LAST_IN_SLICE) > 0
-
-    def put_acq(acq):
-        buffer[:, :, acq.idx.kspace_encode_step_1, acq.idx.kspace_encode_step_2] = acq.data
-
-    @overload
-    def handle_acquisition(acq ):
-        put_acq(acq)
-        return
-        yield
-
-    @overload
-    def handle_acquisition(acq : is_first):
-        nonlocal buffer
-        buffer = np.zeros((acq.active_channels,acq.data.shape[1],matrixsize.y,matrixsize.z),dtype=np.complex64)
-        put_acq(acq)
-        return
-        yield
-
-    @overload
-    def handle_acquisition(acq : is_last ):
-        nonlocal buffer
-        put_acq(acq)
-        yield buffer
+        acquisition.resize(number_of_samples=xspace.shape[1], active_channels=xspace.shape[0])
+        acquisition.center_sample = recon_space.x // 2
+        acquisition.data[:] = cfftn(xspace, axes=[1])
+        return acquisition
 
     for acq in iterable:
-        yield from handle_acquisition(acq)
+        yield crop_acquisition(acq)
 
 
-def recon(iterable):
-    for buffer in iterable:
-        yield cifftn(buffer, axes=[1, 2, 3])
+def accumulate_acquisitions(iterable, header):
+    # To form images, we need a full slice of data. We accumulate acquisitions until we reach the
+    # end of a slice. The acquisitions are then combined in a single buffer, which is then passed
+    # on. We also pass on a single acquisition. We don't need the data, but the header is used to
+    # initialize some of the image metadata (this is optional, but there's no reason to be cavalier
+    # about metadata).
+
+    acquisitions = []
+    matrix_size = header.encoding[0].encodedSpace.matrixSize
+
+    def buffer_from_acquisitions(acqs):
+        logging.debug(f"Assembling buffer from {len(acqs)} acquisitions.")
+
+        buffer = np.zeros(
+            (acqs[0].data.shape[0],
+             acqs[0].data.shape[1],
+             matrix_size.y,
+             matrix_size.z),
+            dtype=np.complex64
+        )
+
+        for acq in acqs:
+            buffer[:, :, acq.idx.kspace_encode_step_1, acq.idx.kspace_encode_step_2] = acq.data
+
+        return buffer
+
+    for acquisition in iterable:
+        acquisitions.append(acquisition)
+        if acquisition.is_flag_set(ismrmrd.ACQ_LAST_IN_SLICE):
+            yield acquisition, buffer_from_acquisitions(acquisitions)
+            acquisitions = []
 
 
-def combine_coils(iterable):
-    for buffer in iterable:
-        rms = np.sqrt(np.sum(np.abs(buffer)**2, axis=0))
-        yield rms
+def reconstruct_images(iterable):
+    # Reconstruction is an inverse fft. We pass on the reference acquisition unchanged; we'll need
+    # it to initialize the image header later.
+
+    for acq, buffer in iterable:
+        yield acq, cifftn(buffer, axes=[1, 2, 3])
 
 
-def make_image(iterable):
-    for buffer in iterable:
-        yield ismrmrd.image.Image.from_array(buffer)
+def combine_channels(iterable):
+    # Buffer contains complex images, one for each channel. We combine these into a single image
+    # through a sum of squares along the channels (axis 0).
+
+    for acq, buffer in iterable:
+        yield acq, np.sqrt(np.sum(np.square(np.abs(buffer)), axis=0))
+
+
+def create_ismrmrd_images(iterable):
+    # Buffer contains the finished image. We wrap it in an ISMRMRD data structure, so the connection
+    # can send it back to Gadgetron. We provide a reference acquisition to properly initialize the
+    # image header with delicious metadata; feel the good karma.
+
+    for acquisition, buffer in iterable:
+        yield ismrmrd.image.Image.from_array(buffer, acquisition=acquisition)
 
 
 @ gadgetron.external.module
 def simple_recon(connection):
     logging.info("Python reconstruction running.")
 
-    iterable = prepare_noise_adjustment(connection)
+    # Connections are iterable - iterating them can be done once, and will yield the data sent from Gadgetron.
+    # In this example, we use a nested sequence of generators (https://wiki.python.org/moin/Generators) each
+    # responsible for part of the reconstruction. In this manner, we construct a succession of generators, each
+    # one step closer to the final product. Iterating the final iterator thus produces output-ready images.
+
+    iterable = noise_adjustment(connection)
     iterable = remove_oversampling(iterable, connection.header)
-    iterable = accumulate(iterable, connection.header)
-    iterable = recon(iterable)
-    iterable = combine_coils(iterable)
-    iterable = make_image(iterable)
+    iterable = accumulate_acquisitions(iterable, connection.header)
+    iterable = reconstruct_images(iterable)
+    iterable = combine_channels(iterable)
+    iterable = create_ismrmrd_images(iterable)
 
     for image in iterable:
+        logging.debug("Sending image back to client.")
         connection.send(image)
