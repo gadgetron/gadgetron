e223b1e74690708c2458b02aee89653388ce1b0a
Hui Xue <hui.xue@nih.gov>
2019-07-11 14:22:36 -0400

    further refine cmr strain analysis and test

--- test/cmr_strain_test.cpp
+++ test/cmr_strain_test.cpp
@@ -13,38 +13,38 @@ using testing::Types;
 template<typename Real>
 class cmr_strain_test : public ::testing::Test
 {
-    protected:
-        virtual void SetUp()
+protected:
+    virtual void SetUp()
+    {
+        GDEBUG_STREAM("=============================================================================================");
+        char* ut_dir = ::getenv("GT_UNITTEST_DIRECTORY");
+        if (ut_dir != NULL)
         {
-            GDEBUG_STREAM("=============================================================================================");
-            char* ut_dir = ::getenv("GT_UNITTEST_DIRECTORY");
-            if(ut_dir!=NULL)
-            {
-                gt_ut_folder_ = std::string(ut_dir);
-            }
-            else
-                gt_ut_folder_.clear();
+            gt_ut_folder_ = std::string(ut_dir);
+        }
+        else
+            gt_ut_folder_.clear();
 
-            GDEBUG_STREAM("=============================================================================================");
+        GDEBUG_STREAM("=============================================================================================");
 
-            if(!gt_ut_folder_.empty())
-            {
-                GDEBUG_STREAM("Unit Test for Gadgetron hoNFFT");
-                gt_ut_data_folder_ = gt_ut_folder_;
-                gt_ut_res_folder_ = gt_ut_folder_ + "/result/";
-                GDEBUG_STREAM("gt_ut_data_folder_ is " << gt_ut_data_folder_);
-                GDEBUG_STREAM("gt_ut_res_folder_ is " << gt_ut_res_folder_);
-            }
-
-            this->timer_.set_timing_in_destruction(false);
+        if (!gt_ut_folder_.empty())
+        {
+            GDEBUG_STREAM("Unit Test for Gadgetron hoNFFT");
+            gt_ut_data_folder_ = gt_ut_folder_;
+            gt_ut_res_folder_ = gt_ut_folder_ + "/result/";
+            GDEBUG_STREAM("gt_ut_data_folder_ is " << gt_ut_data_folder_);
+            GDEBUG_STREAM("gt_ut_res_folder_ is " << gt_ut_res_folder_);
         }
 
-        std::string gt_ut_folder_;
-        std::string gt_ut_data_folder_;
-        std::string gt_ut_res_folder_;
+        this->timer_.set_timing_in_destruction(false);
+    }
 
-        ImageIOAnalyze gt_io_;
-        GadgetronTimer timer_;
+    std::string gt_ut_folder_;
+    std::string gt_ut_data_folder_;
+    std::string gt_ut_res_folder_;
+
+    ImageIOAnalyze gt_io_;
+    GadgetronTimer timer_;
 };
 
 typedef Types<float> realImplementations;
@@ -65,22 +65,22 @@ TYPED_TEST(cmr_strain_test, Cine)
     this->gt_io_.import_array(dx, this->gt_ut_data_folder_ + "/RetroCine/dx_6");
     dx.print(std::cout);
 
-	hoNDArray< T > dy;
-	this->gt_io_.import_array(dy, this->gt_ut_data_folder_ + "/RetroCine/dy_6");
-	dy.print(std::cout);
+    hoNDArray< T > dy;
+    this->gt_io_.import_array(dy, this->gt_ut_data_folder_ + "/RetroCine/dy_6");
+    dy.print(std::cout);
 
-	hoNDArray< T > mask;
-	this->gt_io_.import_array(mask, this->gt_ut_data_folder_ + "/RetroCine/mask_6");
-	T norm_mask = Gadgetron::nrm2(mask);
+    hoNDArray< T > mask;
+    this->gt_io_.import_array(mask, this->gt_ut_data_folder_ + "/RetroCine/mask_6");
+    T norm_mask = Gadgetron::nrm2(mask);
 
 
-	hoNDArray<T> radial, circ, thetas;
-	bool compare_mask = true;
- 	Gadgetron::compute_strain(dx, dy, mask, compare_mask, radial, circ, thetas);
+    hoNDArray<T> radial, circ, thetas;
+    bool compare_mask = true;
+    Gadgetron::compute_strain(dx, dy, mask, compare_mask, radial, circ, thetas);
 
     this->gt_io_.export_array(radial, this->gt_ut_res_folder_ + "/Strain/radial_mask_6");
     this->gt_io_.export_array(circ, this->gt_ut_res_folder_ + "/Strain/circ_mask_6");
-	this->gt_io_.export_array(thetas, this->gt_ut_res_folder_ + "/Strain/theta_6");
+    this->gt_io_.export_array(thetas, this->gt_ut_res_folder_ + "/Strain/theta_6");
 
     // compare agains ground truth
     hoNDArray<T> ref;
@@ -88,18 +88,18 @@ TYPED_TEST(cmr_strain_test, Cine)
     T norm_ref;
 
     this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/RetroCine/rad_strain_mask_6");
-	std::cout << Gadgetron::nrm2(radial) << std::endl;
-	std::cout << Gadgetron::nrm2(ref) << std::endl;
+    std::cout << Gadgetron::nrm2(radial) << std::endl;
+    std::cout << Gadgetron::nrm2(ref) << std::endl;
     Gadgetron::subtract(ref, radial, diff);
     T v = Gadgetron::nrm2(diff);
     norm_ref = Gadgetron::nrm2(ref);
-    EXPECT_LE(v/norm_ref, 0.002);
+    EXPECT_LE(v / norm_ref, 0.002);
 
-	this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/RetroCine/circ_strain_mask_6");
-	std::cout << Gadgetron::nrm2(circ) << std::endl;
-	std::cout << Gadgetron::nrm2(ref) << std::endl;
-	Gadgetron::subtract(ref, circ, diff);
-	T q = Gadgetron::nrm2(diff);
-	norm_ref = Gadgetron::nrm2(ref);
-	EXPECT_LE(q/norm_ref, 0.002);
+    this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/RetroCine/circ_strain_mask_6");
+    std::cout << Gadgetron::nrm2(circ) << std::endl;
+    std::cout << Gadgetron::nrm2(ref) << std::endl;
+    Gadgetron::subtract(ref, circ, diff);
+    T q = Gadgetron::nrm2(diff);
+    norm_ref = Gadgetron::nrm2(ref);
+    EXPECT_LE(q / norm_ref, 0.002);
 }
