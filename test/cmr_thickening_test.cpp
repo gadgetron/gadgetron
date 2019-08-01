0a5adf349e2900df6f05ad36bddd18ceeae066c0
angelagao28 <angelagao28@gmail.com>
2019-08-01 14:24:16 -0400

    update_thickening_to_accomodate_shifting_centroid

--- test/cmr_thickening_test.cpp
+++ test/cmr_thickening_test.cpp
@@ -86,7 +86,7 @@ TYPED_TEST(cmr_thickening_test, 229201050_229201055_362_20190718_retro_cine)
 	endo_mask.print(std::cout);
 
 	hoNDArray<T> rad_strain, edge_endo, edge_epi;
-	size_t ref_phase = 1;
+	size_t ref_phase = 0;
 	Gadgetron::compute_thickening(endo_mask, epi_mask, ref_phase, edge_endo, edge_epi, rad_strain);
 
 	this->gt_io_.export_array(rad_strain, this->gt_ut_res_folder_ + "/Thickening/retro_cine_strain");
@@ -99,26 +99,25 @@ TYPED_TEST(cmr_thickening_test, 229201050_229201055_362_20190718_retro_cine)
 	T norm_ref;
 
 	this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/Masks/retro_cine_strain");
-	std::cout << Gadgetron::nrm2(rad_strain) << std::endl;
-	std::cout << Gadgetron::nrm2(ref) << std::endl;
 	Gadgetron::subtract(ref, rad_strain, diff);
 	T v = Gadgetron::nrm2(diff);
 	norm_ref = Gadgetron::nrm2(ref);
+	std::cout << v << std::endl;
+	std::cout << norm_ref << std::endl;
+	std::cout << v / norm_ref << std::endl;
 	EXPECT_LE(v / norm_ref, 0.1);
 
 	this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/Masks/retro_cine_edge_endo");
-	std::cout << Gadgetron::nrm2(edge_endo) << std::endl;
-	std::cout << Gadgetron::nrm2(ref) << std::endl;
 	Gadgetron::subtract(ref, edge_endo, diff);
 	T q = Gadgetron::nrm2(diff);
 	norm_ref = Gadgetron::nrm2(ref);
+	std::cout << q / norm_ref << std::endl;
 	EXPECT_LE(q / norm_ref, 0.15);
 
 	this->gt_io_.import_array(ref, this->gt_ut_data_folder_ + "/Masks/retro_cine_edge_epi");
-	std::cout << Gadgetron::nrm2(edge_epi) << std::endl;
-	std::cout << Gadgetron::nrm2(ref) << std::endl;
 	Gadgetron::subtract(ref, edge_epi, diff);
 	T s = Gadgetron::nrm2(diff);
 	norm_ref = Gadgetron::nrm2(ref);
+	std::cout << s / norm_ref << std::endl;
 	EXPECT_LE(s / norm_ref, 0.15);
 }
--- toolboxes/cmr/cmr_radial_thickening.cpp
+++ toolboxes/cmr/cmr_radial_thickening.cpp
@@ -1,7 +1,7 @@
 /** \file   cmr_radial_thickening.h
-	\brief  Implement functionalities to handle cardiac time stamps
+	\brief  Implement radial thickening method of computing cardiac strain
 	\author Angela Gao
-	\date   July 9, 2019
+	\date   July 29, 2019
 */
 
 #include "cmr_radial_thickening.h"
@@ -49,7 +49,7 @@ namespace Gadgetron {
 
 			typedef hoNDArray<T> ArrayType;
 
-			hoNDArray<T> thetas(RO, E1, PHS), centroidE(PHS), centroidR(PHS); 
+			hoNDArray<T> thetas(RO, E1, PHS), centroidE(PHS), centroidR(PHS);
 			Gadgetron::clear(centroidE);
 			Gadgetron::clear(centroidR);
 
@@ -125,71 +125,91 @@ namespace Gadgetron {
 						double theta = atan(y / (x + FLT_EPSILON)) + M_PI * (x < 0) + M_PI * 2 * (x >= 0) * (y < 0);
 						thetas(ro, e1, p) = theta;
 
-						double x_in = ro + 0.5 * cos(theta);
-						double x_out = ro - 0.5 * cos(theta);
-						double y_in = e1 - 0.5 * sin(theta);
-						double y_out = e1 + 0.5 * sin(theta);
+						double x_in = ro + 1.0 * cos(theta);
+						double x_out = ro - 1.0 * cos(theta);
+						double y_in = e1 - 1.0 * sin(theta);
+						double y_out = e1 + 1.0 * sin(theta);
 
 						double epi_in = interp_epi(x_in, y_in);
 						double epi_out = interp_epi(x_out, y_out);
 						double endo_in = interp_endo(x_in, y_in);
 						double endo_out = interp_endo(x_out, y_out);
 
-						if (abs(endo_out - endo_in) > 0.35)
+						if (abs(endo_out - endo_in) > 0.8)
 						{
 							edge_endo(ro, e1, p) = 1;
 						}
-						if (abs(epi_out - epi_in) > 0.35)
+						if (abs(epi_out - epi_in) > 0.8)
 						{
 							edge_epi(ro, e1, p) = 1;
 						}
 					}
 				}
 			}
+
 			std::cout << Gadgetron::nrm2(edge_epi) << std::endl;
 			std::cout << Gadgetron::nrm2(edge_endo) << std::endl;
-			int epi_e_ref, epi_r_ref, p;
+			int e, r, p;
 
-//#pragma omp parallel for default(none) private(p, epi_e_ref, epi_r_ref) shared(PHS, RO, E1, rad_strain, mask, centroidE, centroidR, thetas)
+//#pragma omp parallel for default(none) private(p, e, r) shared(PHS, RO, E1, rad_strain, centroidE, centroidR, thetas, samples, edge_endo, edge_epi)
 
-			for (epi_e_ref = 0; epi_e_ref < E1; epi_e_ref++)
+			for (e = 0; e < E1; e++)
 			{
-				for (epi_r_ref = 0; epi_r_ref < RO; epi_r_ref++)
+				for (r = 0; r < RO; r++)
 				{
-					if (edge_epi(epi_r_ref, epi_e_ref, ref_phase) == 1)
+					if (edge_epi(r, e, ref_phase) == 1)
 					{
 						double cr = centroidR(ref_phase);
 						double ce = centroidE(ref_phase);
-						double theta_pt = thetas(epi_r_ref, epi_e_ref, ref_phase);
-						double check_e = epi_e_ref - samples / 8 * sin(theta_pt);
-						double check_r = epi_r_ref + samples / 8 * cos(theta_pt);
+						double theta_pt = thetas(r, e, ref_phase);
+						double check_e_ref = e - samples / 8 * sin(theta_pt);
+						double check_r_ref = r + samples / 8 * cos(theta_pt);
 						
-						hoNDArray<T> test_r(samples), test_e(samples);
-						double r_stepsize = (check_r - centroidR(ref_phase))/samples;
-						double e_stepsize = (check_e - centroidE(ref_phase))/samples;
+						hoNDArray<T> test_r_ref(samples), test_e_ref(samples);
+						double r_stepsize_ref = (check_r_ref - centroidR(ref_phase))/samples;
+						double e_stepsize_ref = (check_e_ref - centroidE(ref_phase))/samples;
 						for (int s = 0; s < samples; s++)
 						{
-							test_r(s) = centroidR(ref_phase) + r_stepsize*s;
-							test_e(s) = centroidE(ref_phase) + e_stepsize*s;
+							test_r_ref(s) = centroidR(ref_phase) + r_stepsize_ref*s;
+							test_e_ref(s) = centroidE(ref_phase) + e_stepsize_ref*s;
 						}
 
 						ArrayType edge_endo_ref(RO, E1, const_cast<T*>(&edge_endo(0, 0, ref_phase)));
+						ArrayType edge_epi_ref(RO, E1, const_cast<T*>(&edge_epi(0, 0, ref_phase)));
 
 						hoNDBoundaryHandlerBorderValue<ArrayType> bv_edge_endo(edge_endo_ref);
-						hoNDInterpolatorLinear<ArrayType> interp_ref(edge_endo_ref, bv_edge_endo);
+						hoNDInterpolatorLinear<ArrayType> interp_endo_ref(edge_endo_ref, bv_edge_endo);
+
+						hoNDBoundaryHandlerBorderValue<ArrayType> bv_edge_epi(edge_epi_ref);
+						hoNDInterpolatorLinear<ArrayType> interp_epi_ref(edge_epi_ref, bv_edge_epi);
 						
-						hoNDArray<T> endo_ref_line(samples);
+						hoNDArray<T> endo_ref_line(samples), epi_ref_line(samples);
 						for (int s = 0; s < samples; s++)
 						{
-							endo_ref_line(s) = interp_ref(test_r(s), test_e(s));
+							endo_ref_line(s) = interp_endo_ref(test_r_ref(s), test_e_ref(s));
+							epi_ref_line(s) = interp_epi_ref(test_r_ref(s), test_e_ref(s));
 						}
 						int endo_ind_ref = Gadgetron::amax(endo_ref_line);
-						double endo_e_ref = (test_e(endo_ind_ref));
-						double endo_r_ref = (test_r(endo_ind_ref));
+						int epi_ind_ref = Gadgetron::amax(epi_ref_line);
+
+						double endo_e_ref = (test_e_ref(endo_ind_ref));
+						double endo_r_ref = (test_r_ref(endo_ind_ref));
+						double epi_e_ref = (test_e_ref(epi_ind_ref));
+						double epi_r_ref = (test_r_ref(epi_ind_ref));
 						double myo_dist_ref = std::sqrt(std::pow(epi_e_ref - endo_e_ref, 2) + std::pow(epi_r_ref - endo_r_ref, 2));
 
 						for (p = 0; p < PHS; p++)
 						{
+							double check_e = centroidE(p) - centroidE(ref_phase) + check_e_ref;
+							double check_r = centroidR(p) - centroidR(ref_phase) + check_r_ref;
+							hoNDArray<T> test_r(samples), test_e(samples);
+							double r_stepsize = (check_r - centroidR(p)) / samples;
+							double e_stepsize = (check_e - centroidE(p)) / samples;
+							for (int s = 0; s < samples; s++)
+							{
+								test_r(s) = centroidR(p) + r_stepsize * s;
+								test_e(s) = centroidE(p) + e_stepsize * s;
+							}
 
 							ArrayType edge_endo_2D(RO, E1, const_cast<T*>(&edge_endo(0, 0, p)));
 							ArrayType edge_epi_2D(RO, E1, const_cast<T*>(&edge_epi(0, 0, p)));
@@ -215,7 +235,7 @@ namespace Gadgetron {
 							double epi_r = test_r(epi_ind);
 							double myo_dist = sqrt(std::pow(epi_e - endo_e, 2) + std::pow(epi_r - endo_r, 2));
 
-							rad_strain(epi_r_ref, epi_e_ref, p) = (myo_dist - myo_dist_ref) / myo_dist_ref;
+							rad_strain(r, e, p) = (myo_dist - myo_dist_ref) / myo_dist_ref;
 						}
 					}
 				}
