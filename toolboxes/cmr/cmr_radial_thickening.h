256168e397e2b7ef1817ab9b8d5960fbbaf3227a
Hui Xue <hui.xue@nih.gov>
2019-08-01 09:48:13 -0400

    finish the strain test cases

--- toolboxes/cmr/cmr_radial_thickening.h
+++ toolboxes/cmr/cmr_radial_thickening.h
@@ -1,6 +1,6 @@
 /** \file   cmr_radial_thickening.h
-	\brief  Implement functionalities to handle cardiac time stamps
-	\author Angela Gao
+    \brief  Implement functionalities to handle cardiac time stamps
+    \author Angela Gao
 */
 
 #pragma once
@@ -13,9 +13,8 @@
 
 namespace Gadgetron {
 
-	/// compute radial strain map over all phases with reference to ref_phase
-	/// endo_mask, epi_mask: [RO, E1, N], 2D+T array of masks over all phases
-	/// ref phase: size_t that is the peak phase
-	template <typename T> EXPORTCMR void compute_thickening(const hoNDArray<T>& endo_mask, const hoNDArray<T>& epi_mask, const size_t ref_phase, hoNDArray<T>& edge_endo, hoNDArray<T>& edge_epi, hoNDArray<T>& rad_strain);
-	//template <typename T> EXPORTCMR void linspace(const T start, const T end, const int num, hoNDArray<T>& res);
+    /// compute radial strain map over all phases with reference to ref_phase
+    /// endo_mask, epi_mask: [RO, E1, N], 2D+T array of masks over all phases
+    /// ref phase: size_t that is the peak phase
+    template <typename T> EXPORTCMR void compute_thickening(const hoNDArray<T>& endo_mask, const hoNDArray<T>& epi_mask, const size_t ref_phase, hoNDArray<T>& edge_endo, hoNDArray<T>& edge_epi, hoNDArray<T>& rad_strain);
 }
