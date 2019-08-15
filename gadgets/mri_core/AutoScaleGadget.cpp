ae82dcb818301946a2d5459cbdea51f107385578
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2019-08-15 15:40:56 +0200

    Fixed the AutoScaleGadget

--- gadgets/mri_core/AutoScaleGadget.cpp
+++ gadgets/mri_core/AutoScaleGadget.cpp
@@ -11,7 +11,7 @@ namespace Gadgetron {
         if (header.image_type != ISMRMRD::ISMRMRD_IMTYPE_MAGNITUDE)
             return image;
 
-        auto scale = max_value / percentile_approx(data, 0.99f);
+        auto scale = max_value / percentile(data, 0.99f);
         data *= scale;
 
         return image;
