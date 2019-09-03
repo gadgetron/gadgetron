a6bcded5ef117df2c77a9646c5629d34c9002e55
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2019-09-03 13:59:49 +0200

    Overhauled FFT class. FFTshift and friends now uses significantly less memory and is about 2x faster.

--- toolboxes/mri_core/mri_core_girf_correction.cpp
+++ toolboxes/mri_core/mri_core_girf_correction.cpp
@@ -59,8 +59,8 @@ namespace Gadgetron {
                 }
 
 
-                hoNDFFT<float>::instance()->timeswitch(&rotated, 0);
                 hoNDFFT<float>::instance()->fft1(rotated);
+                hoNDFFT<float>::instance()->fftshift1D(rotated);
 //            auto padded_kernel = std::move(*pad<std::complex<float>,1>(uint64d1(nlines_new), &girf_kernel));
 
 //            rotated *= padded_kernel;
@@ -77,9 +77,8 @@ namespace Gadgetron {
 
 //                rotated = *pad<std::complex<float>, 1>(uint64d1(full_domain), &rotated);
 
+                hoNDFFT<float>::instance()->ifftshift1D(rotated);
                 hoNDFFT<float>::instance()->ifft1(rotated);
-
-                hoNDFFT<float>::instance()->timeswitch(&rotated, 0);
 
 
 //            hoNDFFT<float>::instance()->ifft(&rotated,0);
