1a15c4d3b0043287642179741fdefb409270bf97
David Christoffer Hansen <davidchansen@gradientsoftware.net>
2019-09-26 15:09:05 +0200

    More work

--- core/readers/AcquisitionBucketReader.cpp
+++ core/readers/AcquisitionBucketReader.cpp
@@ -6,7 +6,6 @@
 using namespace Gadgetron;
 using namespace Gadgetron::Core;
 
-
 namespace {
 
     struct bundle_meta {
@@ -31,10 +30,36 @@ namespace {
     };
 
     struct bucket_meta {
-        bundle_meta   data, reference;
-        stats_meta    data_stats, reference_stats;
+        bundle_meta data, reference;
+        stats_meta data_stats, reference_stats;
         waveform_meta waveforms;
     };
+
+    std::vector<Core::Acquisition> read_acquisitions(std::istream& stream, bundle_meta sizes) {
+        auto acqs = std::vector<Core::Acquisition>(sizes.count);
+        for (auto& acq : acqs) {
+            auto& head = std::get<ISMRMRD::AcquisitionHeader>(acq);
+            head       = IO::read<ISMRMRD::AcquisitionHeader>(stream);
+            auto& data = std::get<hoNDArray<std::complex<float>>>(acq);
+            data       = hoNDArray<std::complex<float>>(head.number_of_samples, head.active_channels);
+            if (head.trajectory_dimensions > 0) {
+                auto& traj = std::get<Core::optional<hoNDArray<float>>>(acq);
+                traj       = hoNDArray<float>(head.trajectory_dimensions, head.number_of_samples);
+            }
+        }
+
+
+        for (auto& acq : acqs) {
+            auto& traj = std::get<Core::optional<hoNDArray<float>>>(acq);
+            if (traj)
+                stream.read(reinterpret_cast<char*>(traj->data()), traj->get_number_of_bytes());
+        }
+        for (auto& acq : acqs) {
+            auto& data = std::get<hoNDArray<std::complex<float>>>(acq);
+            stream.read(reinterpret_cast<char*>(data.data()), data.get_number_of_bytes());
+        }
+
+    }
 
 }
 
@@ -42,10 +67,7 @@ namespace Gadgetron::Core::Readers {
 
     Message AcquisitionBucketReader::read(std::istream& stream) {
 
-
         auto meta = Core::IO::read<bucket_meta>(stream);
-
-
 
         return Message();
     }
