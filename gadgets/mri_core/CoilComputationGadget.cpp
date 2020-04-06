375bd5cae6dc324f491eab0999cf290fdc6cb283
johannesmayer <johannes.mayer@ptb.de>
2020-04-06 16:24:28 +0000

    Added coilcomputation gadget

---
+++ gadgets/mri_core/CoilComputationGadget.cpp
@@ -0,0 +1,55 @@
+#include "CoilComputationGadget.h"
+#include "gadgetron/hoNDArray_elemwise.h"
+#include "gadgetron/hoNDImage_util.h"
+
+#include "gadgetron/mri_core_coil_map_estimation.h"
+/*
+*       CoilComputationGadget.cpp
+*       Author: Johannes Mayer
+*/
+
+
+
+namespace Gadgetron
+{
+    CoilComputationGadget::CoilComputationGadget()
+    {
+    }
+
+    CoilComputationGadget::~CoilComputationGadget()
+    {
+    }
+
+
+    int CoilComputationGadget::process(GadgetContainerMessage<ISMRMRD::ImageHeader>* m1, GadgetContainerMessage<ArrayType>* m2)
+    {
+        ArrayType* input_array = m2->getObjectPtr();
+
+        std::vector<size_t> dims;
+        input_array->get_dimensions(dims);
+
+        if (input_array->get_number_of_dimensions() == 4)
+        {
+            ArrayType csm;
+
+//            coil_map_Inati<ValueType>(*input_array, csm, ks_.value(), kz_.value(), power_.value());
+            coil_map_Inati<ValueType>(*input_array, csm);
+            *m2->getObjectPtr() = csm;
+        }
+        else
+        {
+            GERROR_STREAM("CoilComputationGadget, no 4D data was passed ... ");
+            return GADGET_FAIL;
+        }
+
+        if (this->next()->putq(m1) < 0)
+        {
+            GERROR_STREAM("CoilComputationGadget, failed to pass images to next gadget ... ");
+            return GADGET_FAIL;
+        }
+
+        return GADGET_OK;
+    }
+
+    GADGET_FACTORY_DECLARE(CoilComputationGadget)
+}
