012679f2da4eaf88d2ac4446129f8fb2082d248c
Aymeric Chataigner <chataigner@certis-therapeutics.com>
2020-04-10 09:27:21 +0200

    cmake: find_package calls: find and store version of FFTW3, PugiXML and Sphinx
    Therefore we can check the version of these dependencies in the
    CMakeLists.txt.

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -3,6 +3,14 @@ find_program(SPHINX_EXECUTABLE
              NAMES sphinx-build
              DOC "Path to sphinx-build executable")
 
+#Look for the version of sphinx
+execute_process(COMMAND ${SPHINX_EXECUTABLE} --version
+                OUTPUT_VARIABLE _sphinx_version_output_string
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(FIND ${_sphinx_version_output_string} " " _last_space_position REVERSE)
+math(EXPR _version_substring_begin "${_last_space_position} + 1")
+string(SUBSTRING ${_sphinx_version_output_string} ${_version_substring_begin} -1 SPHINX_VERSION)
+
 include(FindPackageHandleStandardArgs)
 
 #Handle standard arguments to find_package like REQUIRED and QUIET
