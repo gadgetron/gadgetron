8edd1bc6b24f78fd227be5db75bcb2b2cf55b57e
javeda2 <javeda2@nih.gov>
2020-10-26 16:58:47 +0000

    Fixed bug in importing cuSDC.h

--- toolboxes/mri/sdc/SDC.h
+++ toolboxes/mri/sdc/SDC.h
@@ -6,7 +6,7 @@
 
 #include "mri_sdc_export.h"
 
-#include "gadgetron/cuGriddingConvolution.h"
+//#include "gadgetron/cuGriddingConvolution.h"
 namespace Gadgetron
 {
     /**
@@ -28,26 +28,26 @@ namespace Gadgetron
         REAL os_factor = 2.1,
         unsigned int num_iterations = 10);
 
-        /**
-     * \brief Estimate density compensation weights for an arbitrary trajectory.
-     * 
-     * \tparam ARRAY Array type.
-     * \tparam REAL Floating-point type.
-     * \tparam D Number of dimensions.
-     * \param traj Flattened trajectory array of size [Ns, ...], where Ns is the number of samples in a single frame.
-     * \param matrix_size Size of reconstructed image.
-     * \param os_factor Oversampling factor
-     * \param num_iterations Number of iterations.
-     * \param convtype is ConvolutionType::STANDARD ConvolutionType::ATOMIC
-     * \return std::shared_ptr<ARRAY<REAL>> Density compensation weights, same size as trajectory.
-     */
-    template<class REAL, unsigned int D>
-    EXPORTSDC std::shared_ptr<cuNDArray<REAL>> estimate_dcw(
-        const cuNDArray<vector_td<REAL, D>>& traj,
-        const vector_td<size_t, D>& matrix_size,
-        REAL os_factor = 2.1,
-        unsigned int num_iterations = 10,
-        ConvolutionType convtype = ConvolutionType::STANDARD);
+    //     /**
+    //  * \brief Estimate density compensation weights for an arbitrary trajectory.
+    //  * 
+    //  * \tparam ARRAY Array type.
+    //  * \tparam REAL Floating-point type.
+    //  * \tparam D Number of dimensions.
+    //  * \param traj Flattened trajectory array of size [Ns, ...], where Ns is the number of samples in a single frame.
+    //  * \param matrix_size Size of reconstructed image.
+    //  * \param os_factor Oversampling factor
+    //  * \param num_iterations Number of iterations.
+    //  * \param convtype is ConvolutionType::STANDARD ConvolutionType::ATOMIC
+    //  * \return std::shared_ptr<ARRAY<REAL>> Density compensation weights, same size as trajectory.
+    //  */
+    // template<class REAL, unsigned int D>
+    // EXPORTSDC std::shared_ptr<cuNDArray<REAL>> estimate_dcw(
+    //     const cuNDArray<vector_td<REAL, D>>& traj,
+    //     const vector_td<size_t, D>& matrix_size,
+    //     REAL os_factor = 2.1,
+    //     unsigned int num_iterations = 10,
+    //     ConvolutionType convtype = ConvolutionType::STANDARD);
 
 
 
@@ -72,27 +72,27 @@ namespace Gadgetron
         REAL os_factor = 2.1,
         unsigned int num_iterations = 10);
 
-/**
-     * \brief Estimate density compensation weights for an arbitrary trajectory.
-     * 
-     * \tparam ARRAY Array type.
-     * \tparam REAL Floating-point type.
-     * \tparam D Number of dimensions.
-     * \param traj Flattened trajectory array of size [Ns, ...], where Ns is the number of samples in a single frame.
-     * \param initial_dcw Initial estimate for density compensation weights.
-     * \param matrix_size Size of reconstructed image.
-     * \param os_factor Oversampling factor
-     * \param num_iterations Number of iterations.
-     * \param convtype is ConvolutionType::STANDARD ConvolutionType::ATOMIC
-     * \return std::shared_ptr<ARRAY<REAL>> Density compensation weights, same size as trajectory.
-     */
-    template < class REAL, unsigned int D>
-    EXPORTSDC std::shared_ptr<Gadgetron::cuNDArray<REAL>> estimate_dcw(
-        const Gadgetron::cuNDArray<vector_td<REAL, D>>& traj,
-        const Gadgetron::cuNDArray<REAL>& initial_dcw,
-        const vector_td<size_t, D>& matrix_size,
-        REAL os_factor = 2.1,
-        unsigned int num_iterations = 10,
-        ConvolutionType convtype = ConvolutionType::STANDARD);
+// /**
+//      * \brief Estimate density compensation weights for an arbitrary trajectory.
+//      * 
+//      * \tparam ARRAY Array type.
+//      * \tparam REAL Floating-point type.
+//      * \tparam D Number of dimensions.
+//      * \param traj Flattened trajectory array of size [Ns, ...], where Ns is the number of samples in a single frame.
+//      * \param initial_dcw Initial estimate for density compensation weights.
+//      * \param matrix_size Size of reconstructed image.
+//      * \param os_factor Oversampling factor
+//      * \param num_iterations Number of iterations.
+//      * \param convtype is ConvolutionType::STANDARD ConvolutionType::ATOMIC
+//      * \return std::shared_ptr<ARRAY<REAL>> Density compensation weights, same size as trajectory.
+//      */
+//     template < class REAL, unsigned int D>
+//     EXPORTSDC std::shared_ptr<Gadgetron::cuNDArray<REAL>> estimate_dcw(
+//         const Gadgetron::cuNDArray<vector_td<REAL, D>>& traj,
+//         const Gadgetron::cuNDArray<REAL>& initial_dcw,
+//         const vector_td<size_t, D>& matrix_size,
+//         REAL os_factor = 2.1,
+//         unsigned int num_iterations = 10,
+//         ConvolutionType convtype = ConvolutionType::STANDARD);
 
 } // namespace Gadgetron
