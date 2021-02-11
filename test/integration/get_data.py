8b6f88b9de7e9995f9e1b3f4cc8f7494cf4c4a10
David Christoffer Hansen <dchansen@users.noreply.github.com>
2021-02-11 11:42:18 +0100

    Fixed error reporting in get_data.py

--- test/integration/get_data.py
+++ test/integration/get_data.py
@@ -15,18 +15,20 @@ import urllib.request
 
 from concurrent.futures import ThreadPoolExecutor
 
-def is_valid(file, digest):
-
-    if not os.path.isfile(file):
-        return False
-
+def calc_mdf5(file):
     md5 = hashlib.new('md5')
 
     with open(file, 'rb') as f:
         for chunk in iter(lambda: f.read(65536), b''):
             md5.update(chunk)
+    return md5.hexdigest()
 
-    return digest == md5.hexdigest()
+def is_valid(file, digest):
+
+    if not os.path.isfile(file):
+        return False
+
+    return digest == calc_mdf5(file) 
 
 def urlretrieve(url, filename, retries=3):
     if retries <= 0:
@@ -76,12 +78,13 @@ def main():
         urlretrieve(url,destination)
 
         if not is_valid(destination, entry['md5']):
-            raise(RuntimeError("Downloaded file {} failed validation.".format(destination)))
+            raise(RuntimeError("Downloaded file {} failed validation. Expected MD5 {}. Actual MD5 {}".format(destination,entry['md5'],calc_mdf5(destination))))
 
         print("File saved as: {}".format(destination))
-            
+
     with ThreadPoolExecutor() as executor:
-        list(executor.map(download_entry,entries))
+       for result in executor.map(download_entry,entries): #Required song and dance to get the Threadpoolexecutor to return exceptions
+           pass
 
 if __name__ == '__main__':
     main()
