18cd82355096a6ab5d04379153d33c069599bafe
Hui Xue <hui.xue@nih.gov>
2021-03-17 19:31:34 +0000

    fix find mkl linkage

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -96,10 +96,9 @@ if (MKL_FOUND)
         endif ()
 
     if (WIN32)
-
-        set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_intel_thread_dll mkl_core_dll libiomp5md)
+        set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_sequential_dll mkl_intel_thread_dll mkl_core_dll libiomp5md)
    else ()
-        set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_core iomp5)
+        set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_sequential mkl_core mkl_intel_thread iomp5)
    endif ()
 endif ()
 
