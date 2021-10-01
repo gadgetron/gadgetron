0e4435ee51a8224039673bd39983178c99f800ed
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2021-10-01 10:11:33 +0200

    Adding a docstring to ImageIndexGadget.

--- gadgets/mri_core/ImageIndexGadget.h
+++ gadgets/mri_core/ImageIndexGadget.h
@@ -5,6 +5,12 @@
 
 namespace Gadgetron {
 
+    /**
+     * ImageIndexGadget sets the image_index fields for images.
+     *
+     * ImageIndexGadget will keep track of individual image series, and number images in each series
+     * sequentially.
+     */
     class ImageIndexGadget : public Core::ChannelGadget<Core::AnyImage> {
       public:
         ImageIndexGadget(const Core::Context &, const Core::GadgetProperties &);
