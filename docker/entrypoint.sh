ebb4efed3d656734bbe3a367ea0ff793b17754ee
Michael Hansen <mihansen@microsoft.com>
2022-03-23 13:44:04 -0700

    Remove supervisord (#1078)
    * Removed Supervisor
    
    * Added tini to allow CTRL+C exit
    
    * Adjust image integration test script

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# enable conda for this shell
+# shellcheck disable=SC1091
+. /opt/conda/etc/profile.d/conda.sh
+
+# activate the environment
+conda activate gadgetron
+
+exec gadgetron "$@"
