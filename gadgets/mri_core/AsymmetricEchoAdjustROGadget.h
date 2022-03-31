701acbc59dbc77f3b1b942a40fcf861939db520c
Andrew Dupuis <andrew.dupuis@case.edu>
2022-03-31 19:11:38 +0000

    Move AsymmetricEchoAdjustROGadget->ChannelGadget

--- gadgets/mri_core/AsymmetricEchoAdjustROGadget.h
+++ gadgets/mri_core/AsymmetricEchoAdjustROGadget.h
@@ -1,32 +1,23 @@
-
 #pragma once
 
 #include "Gadget.h"
 #include "hoNDArray.h"
-#include "ismrmrd/ismrmrd.h"
-#include "gadgetron_mricore_export.h"
-
-namespace Gadgetron
-{
+#include "GadgetMRIHeaders.h"
+#include "Node.h"
+#include "Types.h"
 
 /// for incoming readout
 /// if not the noise scan and the partial fourier along readout is detected
 /// the readout data will be realigned with center of echo at the centre of incoming 1D array
-class EXPORTGADGETSMRICORE AsymmetricEchoAdjustROGadget : public Gadgetron::Gadget2<ISMRMRD::AcquisitionHeader, hoNDArray< std::complex<float> > >
-{
-public:
-
-    GADGET_DECLARE(AsymmetricEchoAdjustROGadget);
-
-    AsymmetricEchoAdjustROGadget();
-
-protected:
-
-    virtual int process_config(ACE_Message_Block* mb);
-    virtual int process(Gadgetron::GadgetContainerMessage<ISMRMRD::AcquisitionHeader>* m1,
-        Gadgetron::GadgetContainerMessage< Gadgetron::hoNDArray< std::complex<float> > >* m2);
-
-    std::vector<unsigned int> maxRO_;
-};
-
+namespace Gadgetron{
+  class AsymmetricEchoAdjustROGadget : public Core::ChannelGadget<Core::Acquisition> 
+    {
+      public:
+        using Core::ChannelGadget<Core::Acquisition>::ChannelGadget;
+        AsymmetricEchoAdjustROGadget(const Core::Context& context, const Core::GadgetProperties& props);
+        ~AsymmetricEchoAdjustROGadget() override = default;
+        void process(Core::InputChannel<Core::Acquisition>& input, Core::OutputChannel& output) override;
+      protected:
+        std::vector<unsigned int> maxRO_;
+    };
 }
