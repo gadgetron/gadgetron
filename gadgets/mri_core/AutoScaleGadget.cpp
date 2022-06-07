99ebdb65f377ee271f309caebffeb3be659136e0
Andrew Dupuis <andrew.dupuis@case.edu>
2022-06-07 05:17:25 -0400

    Fixing tabbing/spacing in AutoScaleGadget.cpp

--- gadgets/mri_core/AutoScaleGadget.cpp
+++ gadgets/mri_core/AutoScaleGadget.cpp
@@ -4,26 +4,21 @@ using namespace Gadgetron;
 using namespace Gadgetron::Core;
 
 namespace {
-    template<class T>
-    Image<T> autoscale(Image<T> &image, float max_value, unsigned int histogram_bins) {
-
+	template<class T>
+	Image<T> autoscale(Image<T> &image, float max_value, unsigned int histogram_bins) {
 		auto &header = std::get<ISMRMRD::ImageHeader>(image);
-        auto &data = std::get<hoNDArray<T>>(image);
-		auto &meta = std::get<optional<ISMRMRD::MetaContainer>>(image);
-		
+		auto &data = std::get<hoNDArray<T>>(image);
+		auto &meta = std::get<optional<ISMRMRD::MetaContainer>>(image);	
 		if (header.image_type == ISMRMRD::ISMRMRD_IMTYPE_MAGNITUDE) { //Only scale magnitude images for now
 			auto max = *std::max_element(data.begin(), data.end());
 			auto current_scale_ = 1.0;
 			auto histogram_ = std::vector<size_t>(histogram_bins);
-
 			if (histogram_.size() != histogram_bins) {
 				histogram_ = std::vector<size_t>(histogram_bins);
 			}
-
 			for (size_t i = 0; i < histogram_bins; i++) {
 				histogram_[i] = 0;
 			}
-
 			for (auto& d : data) {
 				size_t bin = static_cast<size_t>(std::floor((d/max)*histogram_bins));
 				if (bin >= histogram_bins) {
@@ -31,7 +26,6 @@ namespace {
 				}
 				histogram_[bin]++;
 			}
-			
 			//Find 99th percentile
 			long long cumsum = 0;
 			size_t counter = 0;
@@ -41,13 +35,12 @@ namespace {
 			max = (counter+1)*(max/histogram_bins);
 			GDEBUG("Max: %f\n",max);
 			current_scale_ = max_value/max;
-
 			for (auto& d : data){
 				d *= current_scale_;
 			}
 		}
-        return Image<T>(header,data,meta);
-    }
+		return Image<T>(header,data,meta);
+	}
 
     template<class T>
     Image<std::complex<T>> autoscale(Image<std::complex<T>> &image, float max_value, unsigned int histogram_bins) {
@@ -57,8 +50,8 @@ namespace {
 }
 
 namespace Gadgetron {
-    AnyImage AutoScaleGadget::process_function(AnyImage image) const {
-        return visit([&](auto &image) -> AnyImage { return autoscale(image, max_value, histogram_bins); }, image);
-    }
-    GADGETRON_GADGET_EXPORT(AutoScaleGadget);
+	AnyImage AutoScaleGadget::process_function(AnyImage image) const {
+		return visit([&](auto &image) -> AnyImage { return autoscale(image, max_value, histogram_bins); }, image);
+	}
+	GADGETRON_GADGET_EXPORT(AutoScaleGadget);
 }
