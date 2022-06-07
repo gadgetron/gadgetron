9ba486dded654501dfb00bddca86dae94961f1cf
Andrew Dupuis <andrew.dupuis@case.edu>
2022-06-07 05:51:53 -0400

    Improve variable naming and fix header output

--- gadgets/mri_core/CombineGadget.cpp
+++ gadgets/mri_core/CombineGadget.cpp
@@ -13,15 +13,16 @@ namespace {
     Image<std::complex<T>> combine(Image<std::complex<T>>& image) {
         auto& header = std::get<ISMRMRD::ImageHeader>(image);
         auto& data = std::get<hoNDArray<std::complex<T>>>(image);
-
+        auto& meta = std::get<optional<ISMRMRD::MetaContainer>>(image);
+        
         // Get the dimensions
         size_t nx = data.get_size(0);
         size_t ny = data.get_size(1);
         size_t nz = data.get_size(2);
         size_t nc = data.get_size(3);
 
-        // Create a new message with an hoNDArray for the combined image
-        hoNDArray<std::complex<T>> m3 = hoNDArray<std::complex<T>>();
+        // Create a new hoNDArray for the combined image data
+        hoNDArray<std::complex<T>> combinedData = hoNDArray<std::complex<T>>();
 
         std::vector<size_t> dimensions(3);
         dimensions[0] = nx;
@@ -29,13 +30,13 @@ namespace {
         dimensions[2] = nz;
 
         try {
-            m3.create(dimensions);
+            combinedData.create(dimensions);
         } catch (std::runtime_error& err) {
             GEXCEPTION(err, "CombineGadget, failed to allocate new array\n");
         }
 
-        std::complex<T>* d1 = data.get_data_ptr();
-        std::complex<T>* d2 = m3.get_data_ptr();
+        std::complex<T>* source = data.get_data_ptr();
+        std::complex<T>* combined = combinedData.get_data_ptr();
 
         size_t img_block = nx * ny * nz;
 
@@ -46,18 +47,18 @@ namespace {
                     float phase = 0;
                     size_t offset = z * ny * nx + y * nx + x;
                     for (size_t c = 0; c < nc; c++) {
-                        float mag_tmp = norm(d1[offset + c * img_block]);
-                        phase += mag_tmp * arg(d1[offset + c * img_block]);
+                        float mag_tmp = norm(source[offset + c * img_block]);
+                        phase += mag_tmp * arg(source[offset + c * img_block]);
                         mag += mag_tmp;
                     }
-                    d2[offset] = std::polar(std::sqrt(mag), phase / mag);
+                    combined[offset] = std::polar(std::sqrt(mag), phase / mag);
                 }
             }
         }
 
         // Modify header to match the size and change the type to real
         header.channels = 1;
-        return Image<std::complex<T>>(std::get<ISMRMRD::ImageHeader>(image), std::move(m3), std::get<optional<ISMRMRD::MetaContainer>>(image));
+        return Image<std::complex<T>>(header, combinedData, meta);
     }
 } // namespace
 
