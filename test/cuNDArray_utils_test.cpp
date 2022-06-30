2fa9e195ab8803b82e68997d03f2fbaeb1425607
ahsan <ahsan.javed@nih.gov>
2022-06-30 18:37:37 +0000

    Added unit tests for cuNFFT Atomic and Standard

--- test/cuNDArray_utils_test.cpp
+++ test/cuNDArray_utils_test.cpp
@@ -2,12 +2,21 @@
 #include "cuNDArray_reductions.h"
 #include "cuNDArray_blas.h"
 #include "cuNDArray_elemwise.h"
+#include "NFFTOperator.h"
+#include "cuNFFT.h"
 #include "complext.h"
 
 #include <gtest/gtest.h>
 #include <complex>
 #include <vector>
 #include "vector_td_utilities.h"
+
+// Std includes
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include "GadgetronTimer.h"
+#include "hoArmadillo.h"
 
 using namespace Gadgetron;
 using testing::Types;
@@ -32,10 +41,60 @@ protected:
     dims = std::vector<size_t>(vdims,vdims+sizeof(vdims)/sizeof(size_t));
     Array = cuNDArray<T>(&dims);
     Array2 = cuNDArray<T>(&dims);
+
+    // Prep for NUFFT_TEST
+    RO = 2500;
+    INT = 1100;
+    CHA = 8;
+    xsize_ = 256;
+    ysize_ = 256;
+    kernel_width_ = 3;
+    oversampling_factor_ = 2.1;
+
+
+    std::vector<size_t> data_dims = {RO, INT,CHA};
+    fake_data=cuNDArray<float_complext>(data_dims); 
+    data_dims.pop_back();
+    fake_dcw=cuNDArray<float>(data_dims); 
+    
+    hoNDArray<floatd2> fake_traj_ho(data_dims);
+    vector_td<float, 2> init_val;
+    init_val[0]= 0.1f;
+    init_val[1]= 0.1f;
+    fake_traj_ho.fill(init_val);
+
+    cuNDArray<floatd2> fake_traj = cuNDArray<floatd2>(fake_traj_ho);
+    fill(&fake_dcw,1.0f);
+    fill(&fake_data,complext(1.0f,0.0f));
+
+    
+    unsigned int warp_size = cudaDeviceManager::Instance()->warp_size();
+
+    image_dims_.push_back(xsize_);
+    image_dims_.push_back(ysize_);
+
+    image_dims_os_ = uint64d2(((static_cast<size_t>(std::ceil(image_dims_[0] * oversampling_factor_)) + warp_size - 1) / warp_size) * warp_size,
+                              ((static_cast<size_t>(std::ceil(image_dims_[1] * oversampling_factor_)) + warp_size - 1) / warp_size) * warp_size); // No oversampling is needed in the z-direction for SOS
+
+    recon_dims = {this->image_dims_[0], this->image_dims_[1], CHA};
+
+     std::vector<size_t> flat_dims = {fake_traj.get_number_of_elements()};
+     flat_traj=cuNDArray<floatd2>(flat_dims, fake_traj.get_data_ptr());
   }
   std::vector<size_t> dims;
   cuNDArray<T> Array;
   cuNDArray<T> Array2;
+  
+  cuNDArray<float_complext> fake_data;
+  cuNDArray<float>   fake_dcw;
+  cuNDArray<floatd2> flat_traj;
+  size_t RO, INT, Kz, CHA, xsize_, ysize_; 
+  float kernel_width_, oversampling_factor_;
+  uint64d2 image_dims_os_;
+  std::vector<size_t> image_dims_;
+  boost::shared_ptr<cuNFFT_plan<float, 2>> nfft_plan_;
+  std::vector<size_t> recon_dims;
+  
 };
 
 typedef Types<float, double> realImplementations;
@@ -220,6 +279,30 @@ TYPED_TEST(cuNDArray_utils_TestCplx,padTest){
   EXPECT_EQ(out.get_number_of_elements(),this->Array.get_number_of_elements()*scale);
   EXPECT_NEAR(real(mean(&out))*scale,real(mean(&this->Array)), 0.001);
   EXPECT_NEAR(imag(mean(&out))*scale,imag(mean(&this->Array)), 0.001);
+}
+
+TYPED_TEST(cuNDArray_utils_TestCplx,cuNFFT_ATOMIC){
+
+    this->nfft_plan_ = NFFT<cuNDArray, float, 2>::make_plan(from_std_vector<size_t, 2>(this->image_dims_), this->image_dims_os_, this->kernel_width_, ConvolutionType::ATOMIC);
+    this->nfft_plan_->preprocess(this->flat_traj, NFFT_prep_mode::NC2C);
+
+     auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
+
+     this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
+
+}
+
+TYPED_TEST(cuNDArray_utils_TestCplx,cuNFFT_STANDARD){
+  
+
+     this->nfft_plan_ = NFFT<cuNDArray, float, 2>::make_plan(from_std_vector<size_t, 2>(this->image_dims_), this->image_dims_os_, this->kernel_width_, ConvolutionType::STANDARD);
+     this->nfft_plan_->preprocess(this->flat_traj, NFFT_prep_mode::NC2C);
+
+     
+     auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
+ 
+     this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
+ 
 }
 
 
