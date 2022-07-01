1a87ef8d56bbb61b0310035ea1aaafe866f993ee
ahsan <ahsan.javed@nih.gov>
2022-07-01 15:09:17 +0000

    working test cuNFFT

--- test/cuNDArray_utils_test.cpp
+++ test/cuNDArray_utils_test.cpp
@@ -281,29 +281,30 @@ TYPED_TEST(cuNDArray_utils_TestCplx,padTest){
 }
 
 TYPED_TEST(cuNDArray_utils_TestCplx,cuNFFT_ATOMIC){
-        std::vector<size_t> flat_dims = {this->fake_traj.get_number_of_elements()};
-     cuNDArray<floatd2> flat_traj(flat_dims, this->fake_traj.get_data_ptr());
+        
+    std::vector<size_t> flat_dims = {this->fake_traj.get_number_of_elements()};
+    cuNDArray<floatd2> flat_traj(flat_dims, this->fake_traj.get_data_ptr());
     this->nfft_plan_ = NFFT<cuNDArray, float, 2>::make_plan(from_std_vector<size_t, 2>(this->image_dims_), this->image_dims_os_, this->kernel_width_, ConvolutionType::ATOMIC);
     this->nfft_plan_->preprocess(flat_traj, NFFT_prep_mode::NC2C);
 
-     auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
+    auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
 
-     this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
+    this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
 
 }
 
 TYPED_TEST(cuNDArray_utils_TestCplx,cuNFFT_STANDARD){
   
     std::vector<size_t> flat_dims = {this->fake_traj.get_number_of_elements()};
-     cuNDArray<floatd2> flat_traj(flat_dims, this->fake_traj.get_data_ptr());
-     this->nfft_plan_ = NFFT<cuNDArray, float, 2>::make_plan(from_std_vector<size_t, 2>(this->image_dims_), this->image_dims_os_, this->kernel_width_, ConvolutionType::STANDARD);
-     this->nfft_plan_->preprocess(flat_traj, NFFT_prep_mode::NC2C);
+    cuNDArray<floatd2> flat_traj(flat_dims, this->fake_traj.get_data_ptr());
+    this->nfft_plan_ = NFFT<cuNDArray, float, 2>::make_plan(from_std_vector<size_t, 2>(this->image_dims_), this->image_dims_os_, this->kernel_width_, ConvolutionType::STANDARD);
+    this->nfft_plan_->preprocess(flat_traj, NFFT_prep_mode::NC2C);
 
-     
-     auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
- 
-     this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
- 
+    
+    auto temp = boost::make_shared<cuNDArray<float_complext>>(this->recon_dims);
+
+    this->nfft_plan_->compute(&this->fake_data, *temp, &this->fake_dcw, NFFT_comp_mode::BACKWARDS_NC2C);
+
 }
 
 
