43af9b42691ae9a8704698830ddc07861541e35e
ahsan <ahsan.javed@nih.gov>
2022-07-01 16:55:35 +0000

    Added a cuNFFT test file

--- test/cuNDArray_utils_test.cpp
+++ test/cuNDArray_utils_test.cpp
@@ -41,58 +41,11 @@ protected:
     dims = std::vector<size_t>(vdims,vdims+sizeof(vdims)/sizeof(size_t));
     Array = cuNDArray<T>(&dims);
     Array2 = cuNDArray<T>(&dims);
-
-    // Prep for NUFFT_TEST
-    RO = 2500;
-    INT = 1100;
-    CHA = 8;
-    xsize_ = 256;
-    ysize_ = 256;
-    kernel_width_ = 3;
-    oversampling_factor_ = 2.1;
-
-
-    std::vector<size_t> data_dims = {RO, INT,CHA};
-    fake_data=cuNDArray<float_complext>(data_dims); 
-    data_dims.pop_back();
-    fake_dcw=cuNDArray<float>(data_dims); 
-    
-    hoNDArray<floatd2> fake_traj_ho(data_dims);
-    vector_td<float, 2> init_val;
-    init_val[0]= 0.1f;
-    init_val[1]= 0.1f;
-    fake_traj_ho.fill(init_val);
-
-    fake_traj = cuNDArray<floatd2>(fake_traj_ho);
-    fill(&fake_dcw,1.0f);
-    fill(&fake_data,complext(1.0f,0.0f));
-
-    
-    unsigned int warp_size = cudaDeviceManager::Instance()->warp_size();
-
-    image_dims_.push_back(xsize_);
-    image_dims_.push_back(ysize_);
-
-    image_dims_os_ = uint64d2(((static_cast<size_t>(std::ceil(image_dims_[0] * oversampling_factor_)) + warp_size - 1) / warp_size) * warp_size,
-                              ((static_cast<size_t>(std::ceil(image_dims_[1] * oversampling_factor_)) + warp_size - 1) / warp_size) * warp_size); // No oversampling is needed in the z-direction for SOS
-
-    recon_dims = {this->image_dims_[0], this->image_dims_[1], CHA};
-
    
   }
   std::vector<size_t> dims;
   cuNDArray<T> Array;
   cuNDArray<T> Array2;
-  
-  cuNDArray<float_complext> fake_data;
-  cuNDArray<float>   fake_dcw;
-  cuNDArray<floatd2> fake_traj;
-  size_t RO, INT, Kz, CHA, xsize_, ysize_; 
-  float kernel_width_, oversampling_factor_;
-  uint64d2 image_dims_os_;
-  std::vector<size_t> image_dims_;
-  boost::shared_ptr<cuNFFT_plan<float, 2>> nfft_plan_;
-  std::vector<size_t> recon_dims;
   
 };
 
