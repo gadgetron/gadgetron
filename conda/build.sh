89f95e0ee84b1c0d4504de507633aebfaa4a2b5e
Kyle Greenslade <108955485+v-kylegre@users.noreply.github.com>
2022-07-22 14:22:29 -0600

    Package Gadgetron in Gadgetron (#1125)
    * Package Gadgetron in Gadgetron
    
    Historically the Gadgetron conda package was built as part of:
    
    https://github.com/gadgetron/gadgetron-conda-recipes
    
    However this build process took a very long time and coupled everything.
    
    This PR brings over the packaging of Gadgetron from gadgetron-conda-recipes
    into the Gadgetron repo.
    
    The package will always be built, but published on succesful CI builds.
    The version is determined automagically from the root CMakeLists.txt which
    gives us a single source of truth for versioning.
    
    In addition support for validating the package was achieved by adding the
    Gadgetron integration tests to the packaging step via run_test.sh. This
    will catch things such as missing runtime dependencies in the package meta.
    
    Included in the changes is a bug revision uprev to signify a new package
    being built, and a timeout increase to get_data.py (it was insufficient).

---
+++ conda/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+cd conda && python3 validate_versions.py
+
+PKG_DIR="${SRC_DIR}/build_pkg"
+mkdir -p "${PKG_DIR}"
+cd "${PKG_DIR}" || exit 1
+
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+ninja && ninja install
+
+TEST_DIR="${PREFIX}/share/gadgetron/test/"
+mkdir -p "${TEST_DIR}"
+rsync -a --exclude 'data' "${SRC_DIR}/test/integration" "${TEST_DIR}"
