08b20cc9698db91e9e7f84b50848f0701fdd5c6c
Andrew Dupuis <andrew.dupuis@case.edu>
2022-08-04 10:27:01 -1000

    Convert AcquisitionPassthroughGadget to ChannelGadget (#1139)
    * Convert AcquisitionPassthroughGadget to ChannelGadget
    
    * Fix excess copy-paste error

--- gadgets/debugging/AcquisitionPassthroughGadget.cpp
+++ gadgets/debugging/AcquisitionPassthroughGadget.cpp
@@ -1,20 +1,19 @@
 #include "AcquisitionPassthroughGadget.h"
 
 namespace Gadgetron{
-int AcquisitionPassthroughGadget
-::process(GadgetContainerMessage<ISMRMRD::AcquisitionHeader>* m1,
-	  GadgetContainerMessage< hoNDArray< std::complex<float> > >* m2)
-{
-  //It is enough to put the first one, since they are linked
-  if (this->next()->putq(m1) == -1) {
-    m1->release();
-    GERROR("AcquisitionPassthroughGadget::process, passing data on to next gadget");
-    return -1;
+  void Gadgetron::AcquisitionPassthroughGadget::process(Core::InputChannel<Core::Acquisition>& in, Core::OutputChannel& out) {
+      for (auto acquisition : in) {
+        // Get the header, image data, and trajectory for this acquisition
+		    auto &header = std::get<ISMRMRD::AcquisitionHeader>(acquisition);
+        auto &data = std::get<hoNDArray<std::complex<float>>>(acquisition);
+        auto &trajectory = std::get<Core::optional<hoNDArray<float>>>(acquisition);
+
+        // Do nothing
+        GDEBUG("Passing acquisition through.");
+
+        // Output the acquisition
+        out.push(Core::Acquisition(std::move(header), std::move(data), std::move(trajectory)));
+      }  
   }
-
-  return 0;
+  GADGETRON_GADGET_EXPORT(AcquisitionPassthroughGadget)
 }
-GADGET_FACTORY_DECLARE(AcquisitionPassthroughGadget)
-}
-
-
