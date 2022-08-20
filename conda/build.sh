8b474491abbcbfca1e10150c27fecf9163f526be
V. R <for_usenet@yahoo.com>
2022-08-20 13:15:27 -0400

    Setting conda package build for macOS, e.g. making sure build uses clang from Conda, instead of macOS, not building with CUDA, etc.

--- conda/build.sh
+++ conda/build.sh
@@ -7,7 +7,11 @@ PKG_DIR="${SRC_DIR}/build_pkg"
 mkdir -p "${PKG_DIR}"
 cd "${PKG_DIR}" || exit 1
 
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+if [[ $(uname) =~ Darwin ]]; then
+    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CONDA_PREFIX}/bin/clang -DCMAKE_CXX_COMPILER=${CONDA_PREFIX}/bin/clang++ -DUSE_MKL=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+else
+    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+fi
 ninja && ninja install
 
 TEST_DIR="${PREFIX}/share/gadgetron/test/"
