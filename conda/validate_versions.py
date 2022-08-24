5a7d101f4c0fcaba33c1c0a88c8041a5d6aa96cc
V. R <for_usenet@yahoo.com>
2022-08-23 21:10:42 -0400

    Update validate_versions.py to account for platform flags in Jinja templating.  Also extend to check macOS environment file.

--- conda/validate_versions.py
+++ conda/validate_versions.py
@@ -1,6 +1,7 @@
 import os
 import re
 import yaml
+import sys
 
 from dataclasses import dataclass
 from typing import Union
@@ -53,11 +54,25 @@ def get_meta_dependencies(path_to_meta: str):
     meta_dependencies = {}
     errors = []
 
+    if 'darwin' in sys.platform.lower():
+        excluded_platforms = ['linux64']
+    else:
+        excluded_platforms = ['osx']
+
     with open(path_to_meta) as file:
         for i in range(7):
             _ = file.readline() # Seek past the version directive as its not valid YML.
 
-        file_contents = yaml.safe_load(file)
+        with open ('temp_meta.yml', 'w') as f:
+            for line in file:
+                if any (excluded_platform in line for excluded_platform in excluded_platforms):
+                   pass  # don't include lines with excluded platform flags
+                else:
+                   f.write("%s" % line)
+
+        with open('temp_meta.yml', 'r') as filtered_file:
+            file_contents = yaml.safe_load(filtered_file)
+
         deps = file_contents['requirements']['build']
         deps.extend(file_contents['requirements']['run'])
 
@@ -69,6 +84,8 @@ def get_meta_dependencies(path_to_meta: str):
                     errors.append(f'{pkg_dependency.name} defined twice in meta.yml with varying versioning requirements.')
             meta_dependencies.update({ pkg_dependency.name: pkg_dependency })
 
+    os.remove('temp_meta.yml')
+
     if errors:
         raise RuntimeError(str(errors))
     return meta_dependencies
@@ -77,7 +94,11 @@ def get_meta_dependencies(path_to_meta: str):
 def main():
     current_dir = os.path.dirname(__file__)
 
-    environment_path = os.path.abspath(os.path.join(current_dir, '../environment.yml'))
+    if 'darwin' in sys.platform.lower():
+       environment_path = os.path.abspath(os.path.join(current_dir, '../environment-macos.yml'))
+    else:
+       environment_path = os.path.abspath(os.path.join(current_dir, '../environment.yml'))
+
     meta_path = os.path.join(current_dir, 'meta.yaml')
 
     environment_deps = get_environment_dependencies(environment_path)
@@ -86,7 +107,7 @@ def main():
     errors = []
     for meta_pkg_name, meta_pkg_dep in meta_deps.items():
         if not environment_deps.get(meta_pkg_name):
-            raise RuntimeError(f'{meta_pkg_name} defined in meta.yaml not found in environment.yml')
+            raise RuntimeError(f'{meta_pkg_name} defined in meta.yaml not found in {environment_path}')
         else:
             env_dep = environment_deps[meta_pkg_name]
 
@@ -94,7 +115,7 @@ def main():
             if meta_pkg_dep != env_dep:
                 # If not equivalent we validate that packages defined in meta.yaml are newer than packages in environment.yml
                 if version.parse(meta_pkg_dep.version.removesuffix('.*')) < version.parse(env_dep.version.removesuffix('.*')):
-                    errors.append(f'{env_dep!s} defined in environment.yml is newer than {meta_pkg_dep!s} defined in meta.yaml')
+                    errors.append(f'{env_dep!s} defined in {environment_path} is newer than {meta_pkg_dep!s} defined in meta.yaml')
 
     if errors:
         raise RuntimeError(f'Dependency errors occured:\n{errors!s}')
