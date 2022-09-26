8629d1c0f1b481ce29630ce059eacf4d86f5211c
V. R <for_usenet@yahoo.com>
2022-09-26 09:03:52 -0400

    Add integration testing back into macOS build.

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -5,9 +5,5 @@ export LANG=C
 
 cd "${PREFIX}/share/gadgetron/test/integration" || exit 1
 
-if [[ $(uname) =~ Darwin ]]; then
-   echo "Tests for macOS/Darwin TBD"
-else
-   python get_data.py
-   python run_tests.py --ignore-requirements python,cuda cases/*
-fi
+python get_data.py
+python run_tests.py --ignore-requirements python,cuda cases/*
