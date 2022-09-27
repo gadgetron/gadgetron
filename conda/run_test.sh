5ecc2e1dc6c5126ebe3563a3b1ff4bd3eb9d9550
V. R <for_usenet@yahoo.com>
2022-09-27 10:21:01 -0400

    Add code to check hardware and RAM configuration on GitHub's and Azure's runners.

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -6,6 +6,10 @@ export LANG=C
 cd "${PREFIX}/share/gadgetron/test/integration" || exit 1
 
 if [[ $(uname) =~ Darwin ]]; then
+   echo "Reported hardware and RAM for macOS:"
+   system_profiler SPHardwareDataType
+   system_profiler SPMemoryDataType
+   sysctl hw.memsize
    echo "Tests for macOS/Darwin TBD"
 else
    python get_data.py
