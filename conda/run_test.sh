594ab1fd9b82872c780c6d7ff7d3b457cf6a20dc
V. R <for_usenet@yahoo.com>
2022-11-11 14:10:17 -0500

    Modify conda build to detect when building on a macOS host, and configure the build accordingly.

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -6,4 +6,15 @@ export LANG=C
 cd "${PREFIX}/share/gadgetron/test/integration" || exit 1
 
 python get_data.py
-python run_tests.py --ignore-requirements python,cuda cases/*
+
+if [[ $(uname) =~ Darwin ]]; then
+   echo "Reported hardware and RAM for macOS:"
+   system_profiler SPHardwareDataType
+   system_profiler SPMemoryDataType
+   sysctl hw.memsize
+   sw_vers
+   echo "Tests for macOS/Darwin TBD"
+   # python run_tests.py cases/*
+else
+   python run_tests.py --ignore-requirements python,cuda cases/*
+fi
