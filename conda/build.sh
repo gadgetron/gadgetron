471bf0a752dc077f21879718c87d9e415a01cf60
V. R <for_usenet@yahoo.com>
2022-12-12 20:51:31 -0500

    Tweak conda package build to also include building packages on and for macOS.

--- conda/build.sh
+++ conda/build.sh
@@ -7,8 +7,31 @@ PKG_DIR="${SRC_DIR}/build_pkg"
 mkdir -p "${PKG_DIR}"
 cd "${PKG_DIR}" || exit 1
 
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+if [[ $(uname) =~ Darwin ]]; then
+   echo "Reported hardware and RAM for macOS:"
+   uname -a
+   system_profiler SPHardwareDataType
+   # system_profiler SPMemoryDataType
+   sysctl hw.memsize
+   sw_vers
+   # identify Python causing trouble in cloud environment when building in Python support into macOS build
+   which python
+   python --version
+   # also determine which cmake is being called
+   which cmake
+   cmake --version
+   export MACOSX_DEPLOYMENT_TARGET=10.15
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CONDA_PREFIX}/bin/clang -DCMAKE_CXX_COMPILER=${CONDA_PREFIX}/bin/clang++ -DBUILD_PYTHON_SUPPORT=OFF -DUSE_MKL=ON -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+else
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+fi
+
 ninja && ninja install
+
+if [[ $(uname) =~ Darwin ]]; then
+   ./apps/gadgetron/test/server_tests &
+   ./test/test_all
+fi
 
 TEST_DIR="${PREFIX}/share/gadgetron/test/"
 mkdir -p "${TEST_DIR}"
--- conda/run_test.sh
+++ conda/run_test.sh
@@ -6,4 +6,13 @@ export LANG=C
 cd "${PREFIX}/share/gadgetron/test/integration" || exit 1
 
 python get_data.py
-python run_tests.py --ignore-requirements python,cuda cases/*
+
+# confirm reported gadgetron configuration
+gadgetron --info
+
+if [[ $(uname) =~ Darwin ]]; then
+   # echo "Tests for macOS/Darwin TBD"
+   python run_tests.py cases/*
+else
+   python run_tests.py --ignore-requirements python,cuda cases/*
+fi
