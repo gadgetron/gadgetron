53126085df8c5894c562d9038b14ad6bada9cc4e
V. R <for_usenet@yahoo.com>
2022-12-29 12:25:15 -0500

    Maybe run tests *AFTER* build process ...

--- conda/build.sh
+++ conda/build.sh
@@ -24,12 +24,14 @@ if [[ $(uname) =~ Darwin ]]; then
    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CONDA_PREFIX}/bin/clang -DCMAKE_CXX_COMPILER=${CONDA_PREFIX}/bin/clang++ -DBUILD_PYTHON_SUPPORT=OFF -DUSE_MKL=ON -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
 else
    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
-
-   echo "Run storage server tests at end of build, as this testing binary is not installed."
-   ./apps/gadgetron/test/server_tests &
 fi
 
 ninja && ninja install
+
+if [[ $(uname) =~ Darwin ]]; then
+   echo "Run storage server tests at end of build, as this testing binary is not installed."
+   ./apps/gadgetron/test/server_tests &
+fi
 
 TEST_DIR="${PREFIX}/share/gadgetron/test/"
 mkdir -p "${TEST_DIR}"
