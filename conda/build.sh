c1b707cd0fd737f02a9f27c1a0d1e3b071676423
Michael Hansen <mihansen@microsoft.com>
2023-06-29 09:07:12 -0700

    New ISMRMRD version + hosted pools (#1225)
    * Switched to hosted pool
    * Updated ISMRMRD versions

--- conda/build.sh
+++ conda/build.sh
@@ -7,7 +7,7 @@ PKG_DIR="${SRC_DIR}/build_pkg"
 mkdir -p "${PKG_DIR}"
 cd "${PKG_DIR}" || exit 1
 
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}" -DPython3_EXECUTABLE="${PREFIX}/bin/python3"
 ninja && ninja install
 
 TEST_DIR="${PREFIX}/share/gadgetron/test/"
