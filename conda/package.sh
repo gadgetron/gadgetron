36a107376096940d97ef7362e9e3a264cb7a8642
ahsanjaved <ahsan.javed@nih.gov>
2023-10-13 14:29:23 +0000

    updating conda to fix bugs

--- conda/package.sh
+++ conda/package.sh
@@ -27,4 +27,6 @@ channels=(
 channel_directives=$(printf -- "-c %s " "${channels[@]}")
 
 mkdir -p "$output_path"
+
+bash -c "conda config --set solver libmamba"
 bash -c "conda build --no-anaconda-upload --output-folder $output_path $channel_directives $(dirname "$0")"
