f4be3f71fad0506b465c3c002ee31d1c5dec2b69
Ahsan Javed <ahsanjav@usc.edu>
2023-10-16 13:15:57 -0400

    Pr dcw update (#1236)
    * updated density comp
    
    * updates cmake
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * updating solver to mamba
    
    * Update package.sh
    
    * unnecessary print statements
    
    * vds bug fix for gt
    
    * azure was failing with driver mismatch
    
    * gt tests wont pass
    
    ---------
    
    Co-authored-by: ahsan <ahsan.javed@nih.gov>

--- conda/package.sh
+++ conda/package.sh
@@ -27,6 +27,5 @@ channels=(
 channel_directives=$(printf -- "-c %s " "${channels[@]}")
 
 mkdir -p "$output_path"
-
 bash -c "conda config --set solver libmamba"
 bash -c "conda build --no-anaconda-upload --output-folder $output_path $channel_directives $(dirname "$0")"
