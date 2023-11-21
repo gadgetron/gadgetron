c249127b8879f4e60947b9613e0d33e6f561c3b5
Fox Cutter <v-foxcutter@microsoft.com>
2023-11-21 22:21:05 +0000

    Pytest install in devcontainer

--- .devcontainer/bootstrap-vscode.sh
+++ .devcontainer/bootstrap-vscode.sh
@@ -8,6 +8,8 @@ else
     cp -r "${thisFolder}/.vscode" "${thisFolder}/../"
 fi
 
+pip install pytest
+
 mkdir -p /home/vscode/.local/share/CMakeTools && \
     echo '[{"name":"GCC-CONDA","compilers":{"C":"/opt/conda/envs/gadgetron/bin/x86_64-conda_cos6-linux-gnu-gcc","CXX":"/opt/conda/envs/gadgetron/bin/x86_64-conda_cos6-linux-gnu-g++"}}]' > /home/vscode/.local/share/CMakeTools/cmake-tools-kits.json && \
     chown vscode:conda /home/vscode/.local/share/CMakeTools/cmake-tools-kits.json
