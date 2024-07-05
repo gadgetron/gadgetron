41c6474c26c8cb104145f627d0549661a44a6970
Hui Xue <hui.xue@nih.gov>
2024-07-05 06:04:59 -0700

    add file lock for dump gadget (#1259)
    * add file lock for dump gadget
    
    * use boost file lock
    
    * fix boost lock usage in ismrmrd dump
    
    ---------
    
    Co-authored-by: Hui Xue <xueh@microsoft.com>

--- gadgets/mri_core/IsmrmrdDumpGadget.h
+++ gadgets/mri_core/IsmrmrdDumpGadget.h
@@ -9,13 +9,11 @@
 #include <ismrmrd/xml.h>
 #include <ismrmrd/waveform.h>
 #include <set>
-
 #include <complex>
 
-
-
 namespace Gadgetron {
-       class IsmrmrdDumpGadget : public Core::ChannelGadget<Core::variant<Core::Acquisition, Core::Waveform>>
+
+    class IsmrmrdDumpGadget : public Core::ChannelGadget<Core::variant<Core::Acquisition, Core::Waveform>>
     {
     public:
       
@@ -42,18 +40,13 @@ namespace Gadgetron {
         // TODO: remove this option
         NODE_PROPERTY(pass_waveform_downstream, bool, "If true, waveform data is passed downstream", true);
 
-
         void process(Core::InputChannel<Core::variant<Core::Acquisition,Core::Waveform>>& input, Core::OutputChannel& output) override;
-
 
     private:
 
         const bool save_ismrmrd_data_;
 
-        ISMRMRD::Dataset create_ismrmrd_dataset() const;
-        bool  is_ip_on_blacklist() const ; 
+        std::string create_ismrmrd_dataset_name() const;
+        bool is_ip_on_blacklist() const ; 
     };
-
-
-
 }
