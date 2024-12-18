fb98ff7e952c51bffe08f7511c6a4a0a606cbba9
V. R. <for_usenet@yahoo.com>
2024-12-18 17:17:08 -0500

    Migrate personal fork to pingvin, and trying to match macOS build there.

--- conda/build.sh
+++ conda/build.sh
@@ -4,14 +4,6 @@ set -euo pipefail
 mkdir -p build-conda
 cd build-conda
 
-cmake -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCUDA_COMPUTE_CAPABILITY=ALL \
-    -DUSE_MKL=ON \
-    -DUSE_CUDA=ON \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    ../
-
 if [[ $(uname) =~ Darwin ]]; then
    echo "Reported hardware and RAM for macOS:"
    uname -a
@@ -19,9 +11,16 @@ if [[ $(uname) =~ Darwin ]]; then
    # system_profiler SPMemoryDataType
    sysctl hw.memsize
    sw_vers
-   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_C_COMPILER=${PREFIX}/bin/clang -DCMAKE_CXX_COMPILER=${PREFIX}/bin/clang++ -DPython3_EXECUTABLE=${PREFIX}/bin/python3 -DBUILD_PYTHON_SUPPORT=ON -DUSE_MKL=ON -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}"
+   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_C_COMPILER=${CONDA_PREFIX}/bin/clang -DCMAKE_CXX_COMPILER=${CONDA_PREFIX}/bin/clang++ -DPython3_EXECUTABLE=${CONDA_PREFIX}/bin/python3 -DBUILD_PYTHON_SUPPORT=ON -DUSE_MKL=ON -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
 else
-   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCUDA_COMPUTE_CAPABILITY=ALL -DUSE_MKL=ON -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${SRC_DIR}" -DPython3_EXECUTABLE="${PREFIX}/bin/python3"
+   cmake -GNinja \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DCUDA_COMPUTE_CAPABILITY=ALL \
+       -DUSE_MKL=ON \
+       -DUSE_CUDA=ON \
+       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+       -DPython3_EXECUTABLE="${CONDA_PREFIX}/bin/python3" \
+       ..
 fi
 
 ninja install
