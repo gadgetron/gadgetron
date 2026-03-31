71729f451c1e036b8b1a9186ec0469262b772c4f
Hui Xue <hui.xue@nih.gov>
2026-03-31 04:03:00 +0000

    fix KLT

--- toolboxes/klt/cpu/hoNDKLT.cpp
+++ toolboxes/klt/cpu/hoNDKLT.cpp
@@ -68,52 +68,63 @@ void hoNDKLT<T>::compute_eigen_vector(const hoNDArray<T>& data, bool remove_mean
     Gadgetron::clear(V_);
     Gadgetron::clear(E_);
 
-        size_t m, n;
+    size_t m, n;
 
-        // compute and subtract mean from data
-        hoNDArray<T> data2DNoMean;
+    // compute and subtract mean from data
+    hoNDArray<T> data2DNoMean;
 
-        arma::Mat<T> Am;
-        if (remove_mean)
-        {
-            hoNDArray<T> dataMean(1, N);
-            Gadgetron::sum_over_dimension(data2D, dataMean, 0);
+    arma::Mat<T> Am;
+    if (remove_mean)
+    {
+        hoNDArray<T> dataMean(1, N);
+        Gadgetron::sum_over_dimension(data2D, dataMean, 0);
 
-            Gadgetron::scal((T)(1.0 / M), dataMean);
+        Gadgetron::scal((T)(1.0 / M), dataMean);
 
-            data2DNoMean.create(M, N);
-
-            for (n = 0; n < N; n++)
-            {
-                for (m = 0; m < M; m++)
-                {
-                    data2DNoMean(m, n) = data2D(m, n) - dataMean(0, n);
-                }
-            }
-
-            Am = as_arma_matrix(data2DNoMean);
-        }
-        else
-        {
-            Am = as_arma_matrix(data2D);
-        }
-
-        // call svd
-        arma::Mat<T> Vm = as_arma_matrix(V_);
-        arma::Mat<T> Um;
-        arma::Col<value_type> Sv;
-
-		
-
-        arma::svd_econ(Um, Sv, Vm, Am, 'r');
-		
+        data2DNoMean.create(M, N);
 
         for (n = 0; n < N; n++)
         {
-            value_type v = Sv(n);
-            E_(n) = v * v; // the E is eigen value, the square of singular value
+            for (m = 0; m < M; m++)
+            {
+                data2DNoMean(m, n) = data2D(m, n) - dataMean(0, n);
+            }
         }
 
+        Am = as_arma_matrix(data2DNoMean);
+    }
+    else
+    {
+        Am = as_arma_matrix(data2D);
+    }
+
+    // call eigen
+    arma::Mat<T> Vm = as_arma_matrix(V_);
+
+    // arma::Mat<T> B = Am.t()*Am;
+    // arma::Col<T> Sv;
+    // arma::eig_gen(Sv, Vm, B);
+
+    // for (n = 0; n < N; n++)
+    // {
+    //     E_(n) = Sv(n);
+    // }
+
+    arma::Mat<T> Um;
+    arma::Col<value_type> Sv;
+    if ((M>128) || (N>128))
+        arma::svd_econ(Um, Sv, Vm, Am, 'r');
+    else
+    {
+        GDEBUG_STREAM("Run standard SVD");
+        arma::svd(Um, Sv, Vm, Am, "std");
+    }
+
+    for (n = 0; n < N; n++)
+    {
+        value_type v = Sv(n);
+        E_(n) = v * v; // the E is eigen value, the square of singular value
+    }
 }
 
 template<typename T>
